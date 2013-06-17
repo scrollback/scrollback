e306844532c68be14d11231d2a03e49d994fc9b7
Sankha Narayan Guria <sankha93@gmail.com>
2013-06-17 15:11:01 +0530

    rooms URL scheme

--- gate/http/http.js
+++ gate/http/http.js
@@ -1,8 +1,10 @@
 var express = require("./express.js"),
 	socket = require("./socket.js"),
+	page=require("./page.js");
 	app = express.init();
 
 exports.send = socket.send;
 exports.init = function() {
 	socket.init(app.server);
+	page.init(app);
 };
--- gate/http/page.js
+++ gate/http/page.js
@@ -1,15 +1,10 @@
-var config = require('./config.js');
+var config = require('../../config.js');
 var fs = require("fs");
-var wh = require("./warehouse.js");
-
-var res = require("./lib/respond.js"),
-    log = require("./lib/logger.js").tag('AA/AP'),
-    success = res.success, failure = res.failure;
 
 exports.init = function(app) {
     app.get('*', function(req, res, next) {
         var rooms = [];
-        var i, subURLs = req.path.split("/+/");
+        var i, subURLs = req.path.substring(1).split("/+/");
         for (i = 0; i < subURLs.length; i++) {
             var obj = {};
             var sections = subURLs[i].split("/");
