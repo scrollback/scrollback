b67901af48a6203de38d7200f8efff32f50af48c
Sankha Narayan Guria <sankha93@gmail.com>
2013-06-17 15:38:06 +0530

    fixed error messages

--- gate/http/page.js
+++ gate/http/page.js
@@ -4,13 +4,15 @@ var fs = require("fs");
 exports.init = function(app) {
     app.get('*', function(req, res, next) {
         var rooms = [];
+        // we do a substring because the leading / causes an "" as the first
+        // element in the array
         var i, subURLs = req.path.substring(1).split("/+/");
         for (i = 0; i < subURLs.length; i++) {
             var obj = {};
             var sections = subURLs[i].split("/");
             obj.room = sections[0];
             obj.timestamp = sections[2];
-            obj.tags = sections[1].split("+");
+            obj.tags = sections[1] != undefined ? sections[1].split("+") : [];
             rooms.push(obj);
         }
         var str = JSON.stringify(rooms);
