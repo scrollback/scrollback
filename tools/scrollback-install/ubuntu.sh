b305e28e2c5dc8a50d7642e29aa792446d62f57a
Kamal <kkjoshi.it@gmail.com>
2013-09-09 15:54:50 +0530

    Auto-install script for ubuntu

---
+++ tools/scrollback-install/ubuntu.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+while true; do
+    read -p "Do you wish to install MySQL? " yn
+    case $yn in
+        [Yy]* ) sudo apt-get install mysql-server mysql-client; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+while true; do
+    read -p "Do you wish to install git? " yn
+    case $yn in
+        [Yy]* ) sudo apt-get install git; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you wish to install Nodejs? " yn
+    case $yn in
+        [Yy]* ) sudo apt-get install nodejs; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+read -p "Enter Github username to install scrollback from (default is scrollback): " GHUSER
+
+#echo "$GHUSER" = "$v" ;
+
+if [ ! "$GHUSER" ];then
+   GHUSER="scrollback";
+fi
+            
+echo $GHUSER;
+
+cd ~
+git clone git@github.com:scrollback/$GHUSER
+cd scrollback/tools
+./install.sh
