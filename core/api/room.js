69b56f8b385bc39d84177913a8cf05ea76d76717
harish kumar v <harry.softer@gmail.com>
2013-09-10 17:34:31 +0530

    Adding the room api for the auth plugin.

--- core/api/room.js
+++ core/api/room.js
@@ -1,25 +1,39 @@
 var pool = require("../data.js");
+var log = require("../../lib/logger.js");
 
 module.exports = function(data, callback) {
 	if (typeof data === "object") {
 		pool.get(function(err, db) {
 			if(err) return callback(err);
 			
-			db.query("INSERT INTO `rooms` SET `id`=?, `type`=?, `name`=?, `description`=?, `picture`=?, "+
-			"`profile`=?, `createdOn`=NOW(), `owner`=?, `params`=?", [data.id, data.type || "room",
-			data.name || "", data.description || "", data.picture || "", data.profile || "",
-			data.owner|| data.id, data.params|| ""], function(err, data) {
-				if(err)  return callback(err);
+			db.query("INSERT INTO `rooms` values(?,?,?,?,?,?,NOW(),?,?) ON DUPLICATE KEY UPDATE "+
+				"`id`=values(`id`),`type`=values(`type`),`type`=values(`type`),`description`=values(`description`)"+
+				",`picture`=values(`picture`), `profile`=values(`profile`), `createdOn`=NOW(), `owner`=values(`owner`),"+
+				"`params`=values(`params`)", [data.id, data.type || "user", data.name || "", data.description || "",
+				data.picture || "", data.profile || "", data.owner|| data.id,data.params|| ""],
+			function(err, room) {
+
+				if(err)  {
+					return callback(err,data);
+				}
+				if(data.originalId!= data.id)
+					db.query("delete from `rooms` where `id`=?",[data.originalId]);
 				
+				if (data.accounts.length>0) {
+					insertAccounts(data,function(err,data){
+						if (callback) callback(err,data);
+					});	
+				}
+				else{
+					callback(null,data);
+				}
 				db.end();
-				console.log(err,data);
-				if (callback) callback(err,data);
 			});
+			
 		});
 	}
 	else {
 		pool.get(function(err, db) {
-			console.log(data);
 			db.query("SELECT * FROM `rooms` WHERE `id`=? ", [data], function(err, room){
 				if(err) return callback(err);
 				db.query("SELECT * FROM `accounts` WHERE `room`=?", [data], function(err, accounts) {
@@ -34,3 +48,36 @@ module.exports = function(data, callback) {
 	}
 	
 };
+
+// accounts is an array of strings.
+function insertAccounts(data,callback){
+	var account, accountsQuery=" INSERT INTO `accounts` VALUES ",
+		accountValues=" (?,?,?,?) ",params=[];
+	pool.get(function(err, db) {
+		if (data.accounts.length>0) {
+			data.accounts.forEach(function(element) {
+				var id = element, room = data.id, gateway;
+				gateway = element.split(":")[0];
+				
+				accountsQuery += accountValues;
+				params.push(id);
+				params.push(room);
+				params.push(gateway);
+				params.push("");
+			});
+			
+			db.query("delete from accounts where `room`=?",data.originalId,function(err,data){
+				if (err) callback(err,data);
+
+				db.query(accountsQuery,params,function(err,account){
+					db.end();
+					callback(err,data);
+				});
+
+			});
+			
+			return;
+		}
+	});
+	
+}
--- plugins/auth/auth.js
+++ plugins/auth/auth.js
@@ -5,16 +5,36 @@ module.exports = function(core) {
 	core.on('message', function(message, callback) {
 		var assertion = message.browserid;
 		delete message.browserid;
+		
 		if(message.type !== 'nick') return callback();
+		
+		if (!assertion && message.user) {
+			core.room(message.user.id,function(err,room) {
+				if (room.length!=0 && message.user.originalId!=message.user.id) {
+					return callback(new Error("DUP_NICK/ Nick already exist."));
+				}else{
+					return core.room(message.user,function(err,room) {
+						if (callback) {
+							callback(err,room);	
+						}
+						return;
+					});	
+				}
+			});
+			
+		}
+		
 		if(!assertion) {
 			// If there is no authentication info, make sure the new nick is a guest.
 			if(!/^guest-/.test(message.ref)) message.ref = 'guest-' + message.ref;
 			return callback();
 		}
+		
 		request.post("https://verifier.login.persona.org/verify", { form: {
 			assertion: assertion,
-			audience: 'http://'+config.http.host
+			audience: 'http://'+config.http.host+":"+config.http.port
 		}}, function(err, res, body) {
+			var accountId;
 			if(err) return callback(new Error("AUTH_FAIL_NETWORK/" + err.message));
 			try {
 				body = JSON.parse(body);
@@ -24,9 +44,13 @@ module.exports = function(core) {
 			if(body.status !== 'okay') {
 				return callback(new Error("AUTH_FAIL/" + body.reason));
 			}
-			core.rooms({accounts: ["mailto:" + body.email]}, function(err, data) {
+			accountId = "mailto:" + body.email;
+			core.rooms({accounts: [accountId]}, function(err, data) {
 				if(err) return callback(new Error("AUTH_FAIL_DATABASE/" + err.message));
-				if(data.length === 0) return callback(new Error("AUTH_UNREGISTERED"));
+				if(data.length === 0) {
+					message.user = {accounts: [accountId]};
+					return callback(new Error("AUTH_UNREGISTERED"));
+				}
 				message.user = data[0];
 				message.ref = data[0].id;
 				return callback();
