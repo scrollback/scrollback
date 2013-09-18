1854e134f1e46022cc745e50da3898dd1b53494d
harish kumar v <harry.softer@gmail.com>
2013-09-18 22:33:47 +0530

    Adding the rateLimiter.

--- plugins/ratelimit/ratelimit.js
+++ plugins/ratelimit/ratelimit.js
@@ -1,11 +1,46 @@
-		var limiter = new RateLimiter(config.http.limit, config.http.time, true);
-			limiter.removeTokens(1, function(err, remaining) {
-				var room;
-				
-				if (remaining < 0) {
-					log("Error: API Limit exceeded.");
-					socket.emit('error', 'API Limit exceeded.');
-					return;
-				}
-				log("API Limit remaining:", remaining);
-				
+var users={};
+
+
+module.exports = function(core) {
+	init();
+	core.on('message', function(message, callback) {
+		var limiter;
+		if (!users[message.from])
+			users[message.from]=new RateLimiter(config.http.limit, config.http.time);
+		limiter = users[message.from];
+		
+		if (message.type=="nick") {
+			if (users[message.from]) {
+				users[message.ref] = users[message.from];
+				// this statement means that the auth plugin has to be called before this plugin.
+				// make be even ignore this when the its a auth request.
+				delete users[message.from];
+			}
+		}
+		
+		limiter.removeTokens(1, function(err, remaining) {
+			var room;
+			if (remaining < 0) {
+				log("Error: API Limit exceeded.");
+				callback(new Error("API Limit exceeded"));
+			}
+			callback();
+		});
+	});
+};
+
+RateLimiter =function(a,b,c){
+	return {
+		limit:a,
+		time:b,
+		removeTokens: function(count,callback){
+			if(this.limit>0){
+				this.limit-=count;
+				setTimeout(function(object,count){
+					object.limit+=count;
+				},this.time,this,count);
+			}
+			callback(null, this.limit);
+		}
+    };
+};
