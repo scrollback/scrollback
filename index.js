397d5f0160eda071d6e1775a0ad80c8bacc80dff
Kumar Anirudha <anirudhastark@yahoo.com>
2013-11-07 13:06:11 +0530

    iframe to index

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ process.nextTick(function(){
 	if(config.core.uid) process.setuid(config.core.uid);
 });
 process.title = config.core.name;
+
+//put in id as "newFrame" and the "src" of the irccloud in the src and we are good to go to be put in anywhere in the page a user wants to.
+document.write('<iframe id="newFrame" src="http://scrollback.io" width="350" height="500" marginwidth="0" marginheight="0" frameborder="0" style="border: #000000 1px solid;"></iframe>')
