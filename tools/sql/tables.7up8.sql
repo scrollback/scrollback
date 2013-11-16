0e68c02046b39107cc586d4e5f9614ec2aac6b14
harish kumar v <harry.softer@gmail.com>
2013-11-16 18:39:10 +0530

    Added the indexes to the tables.

--- tools/sql/tables.7up8.sql
+++ tools/sql/tables.7up8.sql
@@ -1,7 +1,17 @@
 /*Number of min from UTC*/
-alter table accounts add timezone int default 0;/* 0 UTC*/
+ALTER TABLE `scrollback`.`accounts` ADD COLUMN `timezone` int default 0, ADD INDEX `timezone` (`timezone`);
+
 ALTER TABLE `rooms` ADD `temp_time` datetime ;
 UPDATE scrollback.rooms SET temp_time = createdOn;
 ALTER TABLE scrollback.rooms MODIFY COLUMN createdOn bigint;
 UPDATE scrollback.rooms SET createdOn  = UNIX_TIMESTAMP(temp_time);
 ALTER TABLE rooms drop COLUMN temp_time ;
+
+ALTER TABLE `scrollback`.`text_messages` add index `time` (`time`), add index `fromtime` (`from`,`time`), add index `tolabeltime` (`to`,`labels`,`time`);
+ALTER TABLE `scrollback`.`nick_messages` add index `fromtime` (`from`,`time`), add index `reftime` (`ref`,`time`);
+ALTER TABLE `scrollback`.`away_messages` add index `fromtime` (`from`,`time`), add index `fromtotime` (`from`,`to`,`time`);
+ALTER TABLE `scrollback`.`back_messages` add index `fromtime` (`from`,`time`), add index `fromtotime` (`from`,`to`,`time`);
+ALTER TABLE `scrollback`.`join_messages` add index `fromtime` (`from`,`time`), add index `fromtotime` (`from`,`to`,`time`);
+ALTER TABLE `scrollback`.`part_messages` add index `fromtime` (`from`,`time`), add index `fromtotime` (`from`,`to`,`time`);
+
+ALTER TABLE `scrollback`.`rooms` add index `owner` (`owner`);
