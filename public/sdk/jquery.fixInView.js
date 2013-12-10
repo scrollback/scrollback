c899b1f09c55f3f2f351df36007983c317053300
aravind <aravindet@gmail.com>
2013-12-10 11:35:26 +0530

    Jquery Fixinview plugin improvements   - emits 'reposition' events   - nudgeInView() method.

--- public/sdk/jquery.fixInView.js
+++ public/sdk/jquery.fixInView.js
@@ -1,11 +1,19 @@
-jQuery.fn.fixInView = function() {
+jQuery.fn.fixInView = function(container) {
 	var lastTop = 0,
 		columns = this,
 		$ = jQuery;
-		
-	function reposition() {
-		var top = $(window).scrollTop(),
-			height = $(window).height(),
+
+	container = container || $(window);
+	
+	container.data('fixedInColumns', columns);
+	
+	columns.each(function () {
+		$(this).data('fixedInContainer', container);
+	});
+	
+	function reposition(event, silent) {
+		var top = container.scrollTop(),
+			height = container.height(),
 			up = lastTop > top;
 		
 		if(lastTop == top) return;
@@ -14,13 +22,12 @@ jQuery.fn.fixInView = function() {
 			var column = $(this),
 				cHeight = column.outerHeight(),
 				cTop = column.offset().top,
-				small = cHeight < height;
+				small = cHeight < height,
+				status = column.data('fixedInStatus'),
+				cSilent = !!silent;
 			
-			if(
-				(!small && up || small && !up) && 
-				column.data('fixedInView') == 'bottom'
-			) {
-				column.data('fixedInView', 'none');
+			if((!small && up || small && !up) && status == 'bottom') {
+				column.data('fixedInStatus', 'none');
 				column.css({
 					position: 'absolute',
 					top: lastTop + height - cHeight,
@@ -28,11 +35,8 @@ jQuery.fn.fixInView = function() {
 				});
 			}
 			
-			else if(
-				(!small && !up || small && up) && 
-				column.data('fixedInView') == 'top'
-			) {
-				column.data('fixedInView', 'none');
+			else if((!small && !up || small && up) && status == 'top') {
+				column.data('fixedInStatus', 'none');
 				column.css({
 					position: 'absolute',
 					top: lastTop,
@@ -44,7 +48,7 @@ jQuery.fn.fixInView = function() {
 				(!small && up && cTop > top) ||
 				(small && !up && cTop < top)
 			) {
-				column.data('fixedInView', 'top');
+				column.data('fixedInStatus', 'top');
 				column.css({
 					position: 'fixed',
 					top: 0,
@@ -56,23 +60,73 @@ jQuery.fn.fixInView = function() {
 				(!small && !up && cTop + cHeight < top + height) ||
 				(small && up && cTop + cHeight > top + height)
 			) {
-				column.data('fixedInView', 'bottom');
+				column.data('fixedInStatus', 'bottom');
 				column.css({
 					position: 'fixed',
 					top: 'auto',
 					bottom: 0
 				});
-			};
+			}
+			
+			else if(!cSilent && (status == 'top' || status == 'bottom')) {
+				cSilent = true;
+			}
+			
+			if(!cSilent) {
+				// cTop = column.offset().top;
+				column.trigger({
+					type: 'reposition',
+					by: top - lastTop,
+					above: -cTop + top,
+					range: height, 
+					below: cHeight - height + cTop - top,
+					height: cHeight
+				});
+			}
 		});
 		
 		lastTop = top;
 	}
 	
-	$(window).scroll(reposition);
-	$(window).resize(function() {
-		lastTop = $(window).scrollTop() + 1;
-		reposition();
-		lastTop = $(window).scrollTop() - 1;
-		reposition();					
+	container.scroll(reposition);
+	$(window).resize(function(e) {
+		lastTop = container.scrollTop() + 1;
+		reposition(e, true);
+		lastTop = container.scrollTop() - 1;
+		reposition(e, true);
 	});
+};
+
+jQuery.fn.nudgeInView = function (adjustment) {
+	var container = this.data('fixedInContainer'),
+		column = this;
+
+	if(!container) {
+		console.log('Cannot nudgeInView: Isnt fixedInView yet', this);
+		return;
+	}
+	
+	var top = container.scrollTop(),
+		height = container.height(),
+		cHeight = column.outerHeight(),
+		cTop = column.offset().top,
+		small = cHeight < height;
+	
+	if(column.data('fixedInStatus') != 'none') {
+		column.data('fixedInStatus', 'none');
+		column.css({
+			position: 'absolute',
+			top: cTop,
+			bottom: 'auto'
+		});
+	}
+	
+	column.css({top: cTop + adjustment});
+	if(cTop + adjustment < 0) {
+		adjustment = -cTop - adjustment;
+		container.data('fixedInColumns').each(function() {
+			$(this).nudgeInView(adjustment);
+		});
+		container.scrollTop(container.scrollTop() + adjustment);
+	};
 };
