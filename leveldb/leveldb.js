cf8400539440ce82e89f2717e4b7a391427adfb7
aravind <aravindet@gmail.com>
2014-01-17 20:48:12 +0530

    Added file structure

--- leveldb/leveldb.js
+++ leveldb/leveldb.js
@@ -1,12 +1,13 @@
-var objectlevel = require("objectlevel"),
+var db = require("objectlevel")(),
 	config = require("../config.js");
 
 objectlevel.connect(config.leveldb.path);
+var types = require("./types.js")(objectlevel);
 
-var texts = require("./schemas/text.js")(objectlevel);
+var texts = require("./schemas/text.js")(types);
 
 module.exports = function(core) {
 	core.on('message', texts.put, 'storage');
 //	core.on('messages', texts.get, 'storage');
-	
+	core.on('join', 
 };
