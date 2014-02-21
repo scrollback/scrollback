38698ee2a66ee328c574e6d161b5a62e6a68c4d4
aravind <aravindet@gmail.com>
2014-02-21 23:01:37 +0530

    Revert "Synthetic scrolling"
    This reverts commit f28cbec7604bce63f31e20018b1b68142f9a3968.

--- public/sdk/jquery-fixInView.js
+++ public/sdk/jquery-fixInView.js
@@ -12,7 +12,7 @@
 	$.fn.fixInView = function() {
 		this.each(function () {
 			var el = $(this),
-				column = { element: el, anchorBottom: false };
+				column = { element: el, bottom: false };
 			columns.push(column);
 			el.css({position: 'fixed'});
 			el.data("column", column);
@@ -21,50 +21,52 @@
 	
 	function read() {
 		viewHeight = $window.height();
-		columns.forEach(readColumn);
-	}
-
-	function readColumn(column) {
-		column.top = column.element.offset().top;
-		column.height = column.element.outerHeight();
-		column.bottom = viewHeight - (column.top + column.height);
-	//	if(column.element.attr('id') == 'body') console.log('Read', column.top, column.bottom, column.anchorBottom);
+		
+		columns.forEach(function (column) {
+			column.top = column.element.offset().top;
+			column.height = column.element.outerHeight();
+		});
 	}
 	
 	function moveView(movement) {
 		columns.forEach(function (column) {
 			var t = column.top;
 			moveColumn(column, -movement);
-			if(column.top != t) setTimeout(function() { trigger(column, column.top - t); }, 100);
+			if(column.top != t) trigger(column, column.top - t);
 		});
 	}
 	
 	function moveColumn(column, movement) {
-		column.top += movement; column.bottom -= movement;
-		if(column.top > 0) { column.top = 0; column.bottom = viewHeight - column.height; }
-		else if(column.bottom > 0) { column.bottom = 0; column.top = viewHeight - column.height; }
+		if(column.small) {
+			column.top = 0;
+			return;
+		}
+		column.top = column.top + movement;
+		if(column.top > 0) { column.top = 0; }
+		if(column.top + column.height < viewHeight) {
+			column.top = viewHeight - column.height;
+		}
 	}
 		
 	function trigger(column, movement) {
+		// console.log('trigger', column.element.attr('id'), -column.top);
 		column.element.trigger({
 			type: 'reposition',
 			by: -movement,
 			above: -column.top,
 			range: viewHeight,
-			below: -column.bottom,
+			below: column.top + column.height - (viewHeight),
 			height: column.height
 		});
 	}
 	
 	function write() {
-		columns.forEach(writeColumn);
-	}
-
-	function writeColumn(column) {
-	//	if(column.element.attr('id') == 'body') console.log('Write', column.top, column.bottom, column.anchorBottom);
-		column.element.css(
-			column.anchorBottom? {top: 'auto', bottom: column.bottom}: {top: column.top, bottom: 'auto'}
-		);
+		columns.forEach(function (column) {
+			column.element.css(column.bottom?
+				{top: column.top, bottom: 'auto'}:
+				{top: 'auto', bottom: viewHeight - column.top - column.height}
+			);
+		});
 	}
 	
 	function scroll(movement) {
@@ -73,7 +75,7 @@
 		write();
 	}
 	
-	$window.on('wheel', function(e) {
+	$(window).on('wheel', function(e) {
 		var y, lhs, lh;
 		e = e.originalEvent;
 		if(e.deltaMode === 0) y = e.deltaY;
@@ -90,19 +92,7 @@
 		if(y) scroll(y);
 	});
 	
-	$window.on('keydown', function(e) {
-			var lhs = $body.css('lineHeight'),
-				lh = parseFloat(lhs);
-			if(lhs.substr(-2) != 'px') {
-				lh = (isNaN(lh)? 1.5: lh) * parseFloat($body.css('fontSize'));
-			}
-		if(e.which == 38) scroll(-3*lh);
-		else if(e.which == 40) scroll(3*lh);
-		else if(e.which == 33) scroll(-viewHeight);
-		else if(e.which == 34) scroll(viewHeight);
-	});
-
-	$window.resize(function() { scroll(0); });
+	$(window).resize(function() { scroll(0); });
 	
 	$.fn.nudgeInView = function(movement) {
 		read();
@@ -111,17 +101,13 @@
 	};
 	
 	$.fn.anchorBottom = function() {
-		var column = $(this).data('column');
-		readColumn(column);
-		column.anchorBottom = true;
-		writeColumn(column);
+		console.log(this.attr('id'), 'set to bottom');
+		$(this).data('column').bottom = true;
 	};
 	
 	$.fn.anchorTop = function() {
-		var column = $(this).data('column');
-		readColumn(column);
-		column.anchorBottom = false;
-		writeColumn(column);
+		console.log(this.attr('id'), 'set to bottom');
+		$(this).data('column').bottom = false;
 	};
 
 }(jQuery));
