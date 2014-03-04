d64d8714472f67aa4d22869f16844fcf0bb40c05
aravind <aravindet@gmail.com>
2014-03-04 17:22:49 +0530

    Initial changes

--- http/socket.js
+++ http/socket.js
@@ -25,20 +25,16 @@ Boston, MA 02111-1307 USA.
 
 /* global require, module, exports, console, setTimeout */
 
-var sockjs = require("sockjs"),core,
-	cookie = require("cookie"),
+var sockjs = require("sockjs"), core,
+	api = require("./api.js"),
 	log = require("../lib/logger.js"),
-	config = require("../config.js"),
-	EventEmitter = require("events").EventEmitter,
-	session = require("./session.js"),
-	guid = require("../lib/guid.js"),
-	crypto = require("crypto"),
-	names = require("../lib/names.js");
+	config = require("../config.js");
 
 
-var rConns = {}, users = {};
-var pid = guid(8);
+var rConns = {}, uConns = {};
 var sock = sockjs.createServer();
+
+function respons () {}
 
 sock.on('connection', function (socket) {
 	var conn = { socket: socket };
@@ -47,81 +43,55 @@ sock.on('connection', function (socket) {
 		try { d = JSON.parse(d); log ("Socket received ", d); }
 		catch(e) { log("ERROR: Non-JSON data", d); return; }
 		
+		if(d.type == 'init' && d.session) conn.session = d.data.session;
+		else if(conn.session) { d.session = conn.session; }
+		else { conn.session = d.session = 'http:' + generate.guid(32); }
 		
-		switch(d.type) {
-			case 'init': init(d.data, conn); break;
-			case 'message': message(d.data, conn); break;
-			case 'messages': messages(d.data, conn); break;
-			case 'room': room(d.data, conn); break;
-			case 'rooms': rooms(d.data, conn); break;
-			case 'getUsers': getUsers(d.data, conn); break;
-			case 'getRooms': getRooms(d.data, conn); break;
-			case 'edit': edit(d.data, conn); break;
-		}
+		if(d.type == 'back' && !verifyBack(d)) return;
+		if(d.type == 'awwy' && !verifyAway(d)) return;
+		
+		api.process(d, function(err, data) {
+			conn.send(err? {type: 'error', ref: message: err.message}: data);
+		});
 	});
 	
-	conn.send = function(type, data) {
-		socket.write(JSON.stringify({type: type, data: data}));
+	conn.send = function(data) {
+		socket.write(JSON.stringify(data));
 	};
-	socket.on('close', function() { close(conn); });
+	socket.on('close', function() { handleClose(conn); });
 });
 
-exports.init = function(server, coreObject) {
-    core = coreObject;
+module.exports = function(server, c) {
+    core = c;
+	api(core);
+	core.on('init', emit);
+	core.on('away', emit);
+	core.on('back', emit);
+	core.on('join', emit);
+	core.on('part', emit);
+	core.on('room', emit);
+	core.on('user', emit);
+	core.on('admit', emit);
+	core.on('expel', emit);
+	core.on('edit', emit);
+	core.on('text', emit);
     sock.installHandlers(server, {prefix: '/socket'});
 };
 
-function init(data, conn) {
-	var user, sid = data.sid, nick = data.nick, i;
+function emit(action) {
+	if(action.type == 'init' || action.type == 'user') {
+		sConns[action.session].forEach(dispatch);
+	} else {
+		rConns[action.session].forEach(dispatch);
+	}
 	
-	session.get({ sid:sid, suggestedNick:data.nick }, function(err, sess) {
-		var rooms = sess.user.rooms;
-		var i;
-		conn.sid = sid;
-		conn.rooms = [];
+	function dispatch(conn) { conn.send(action); }
+};
 
-		if (sess.user.id.indexOf("guest-")===0 && data.nick)	sess.user.id="guest-"+data.nick;
-		if(sess.pid !== pid) {
-			sess.pid = pid;
-			for(i in rooms) {
-				if(rooms.hasOwnProperty(i)) rooms[i] = 0;
-			}
-		}
-		var query=[];
-		if (sess.user.id.indexOf('guest-')!==0) {
-			query.user=sess.user.id;
-		}
-		core.emit("members", query,function(err,d) {
-			var m={};
-			if (d) {
-				for (i=0;i<d.length;i++) {
-					m[d[i].room]=true;
-				}
-			}
-			sess.user.membership = Object.keys(m);//Room added to user object
-			
-			conn.send('init', {
-				sid: sess.cookie.value,
-				user: sess.user,
-				clientTime: data.clientTime,
-				serverTime: new Date().getTime(),
-			});
+function handleClose(conn) {
+	if(!conn.session) return;
 
-			//Temp for now.
-			core.emit("init", {
-				from: sess.user.id,
-				session: "web:"+conn.socket.remoteAddress+":"+conn.sid,
-				time: new Date().getTime()
-			});
-			session.set(conn.sid, sess);
-		});
-	});
-}
-
-function close(conn) {
-	if(!conn.sid) return;
-
-	session.get({sid: conn.sid}, function(err, sess) {
+	core.emit('getSession', {ref: conn.session}, function(err, sess) {
 		if(err) {
 			log("Couldn't find session to close.");
 			return;
@@ -132,20 +102,20 @@ function close(conn) {
 			log("Closed connection, removing", user.id, room);
 			userAway(user, room, conn);
 		});
-		session.set(conn.sid, sess);
+		session.set(conn.session, sess);
 	});
 }
 
-function userAway(user, room, conn) {
+function verifyAway(user, room, conn) {
 	if(rConns[room]) rConns[room].splice(rConns[room].indexOf(conn), 1);
 	conn.rooms.splice(conn.rooms.indexOf(room), 1);
 	setTimeout(function() {
-		session.get({sid: conn.sid}, function(err, sess) {
+		session.get({session: conn.session}, function(err, sess) {
 			var user = sess.user;
 			if (typeof user.rooms[room] !== "undefined" && user.rooms[room]<=1) {
 				delete user.rooms[room];
 				core.emit("message", { type: 'away', from: user.id, to: room,
-					time: new Date().getTime(), session:"web:"+conn.socket.remoteAddress+":"+ conn.sid,origin : {gateway : "web", location : "", ip :  conn.socket.remoteAddress}}, function(err, m) {
+					time: new Date().getTime(), session:"web:"+conn.socket.remoteAddress+":"+ conn.session,origin : {gateway : "web", location : "", ip :  conn.socket.remoteAddress}}, function(err, m) {
 						log(err, m);
 					});
 				if(!Object.keys(user.rooms).length) {
@@ -156,13 +126,13 @@ function userAway(user, room, conn) {
 				user.rooms[room]--;
 				log("User still has some active windows or away already sent.",user);
 			}
-			session.set(conn.sid, sess);
+			session.set(conn.session, sess);
 		});
 	}, 30*1000);
 	return false; // never send an away message immediately. Wait.
 }
 
-function userBack(user, room, conn) {
+function verifyBack(user, room, conn) {
 
 	if(rConns[room]) rConns[room].push(conn);
 	else rConns[room] = [conn];
@@ -192,10 +162,10 @@ function messages (query, conn) {
 
 
 function edit(action, conn) {
-	session.get({sid: conn.sid}, function(err, sess) {
+	session.get({session: conn.session}, function(err, sess) {
 		var user = sess.user;
 		action.from = user.id;
-		action.session = "web:"+conn.socket.remoteAddress+":"+conn.sid;
+		action.session = "web:"+conn.socket.remoteAddress+":"+conn.session;
 		core.emit("edit",action, function(err, data){
 		});
 	});
@@ -203,14 +173,14 @@ function edit(action, conn) {
 }
 function message (m, conn) {
 	
-	if(!conn.sid) return;
-	session.get({sid: conn.sid}, function(err, sess) {
+	if(!conn.session) return;
+	session.get({session: conn.session}, function(err, sess) {
 		var user = sess.user, tryingNick, roomName;
 		roomName = m.to;
 		
 		m.from = user.id;
 		m.time = new Date().getTime();
-		m.session = "web:"+conn.socket.remoteAddress+":"+ conn.sid;
+		m.session = "web:"+conn.socket.remoteAddress+":"+ conn.session;
 
 		if (m.origin) m.origin.ip = conn.socket.remoteAddress;
 		else{
@@ -225,29 +195,29 @@ function message (m, conn) {
 		if(m.type == 'join'){
 			//check for user login as well
 			sess.user.membership.push(roomName);
-			session.set(conn.sid, sess);
+			session.set(conn.session, sess);
 		}
 		if(m.type == 'part'){
 			//check for user login as well
 			sess.user.membership.splice(sess.user.membership.indexOf(roomName),1);
-			session.set(conn.sid, sess);
+			session.set(conn.session, sess);
 		}
 		
 		if (m.type == 'back') {
 			if(!userBack(user, m.to, conn)) {
-				session.set(conn.sid, sess);
+				session.set(conn.session, sess);
 				return; 
 			}
-			session.set(conn.sid, sess);
+			session.set(conn.session, sess);
 			// it returns false if the back message for this user is already sent
 		} else if (m.type == 'away') {
 			if(!userAway(user, m.to, conn)) {
-				session.set(conn.sid, sess);
+				session.set(conn.session, sess);
 				return; 
 			}
 			// it returns false if the away message for this user is not to be sent yet
 		} else if(m.type == 'nick') {
-			//validating nick name on server side 
+			//validating nick name on server sessione 
 			if(m.ref && m.ref !== "guest-" && !validateNick(m.ref.substring(6))) {
 				return conn.send('error', {id:m.id , message: "INVALID_NAME"});
 			}
@@ -287,9 +257,9 @@ function message (m, conn) {
 						sess.user.picture = "//s.gravatar.com/avatar/" + crypto.createHash('md5').update(sess.user.id).digest('hex') + "/?d=identicon&s=48";
 						sess.user.accounts = [];
 						sess.user.membership = [];
-						session.set(conn.sid, sess);
+						session.set(conn.session, sess);
 						conn.send('init', {
-							sid: sess.cookie.value,
+							session: sess.cookie.value,
 							user: sess.user
 						});
 						if(/^guest-/.test(m.ref)){
@@ -312,7 +282,7 @@ function message (m, conn) {
 					} else if(!err){
 						user.id = m.ref;
 					}
-					session.set(conn.sid, sess);
+					session.set(conn.session, sess);
 					var query=[];
 					if (sess.user.id.indexOf('guest-')!==0) {
 						query.user=sess.user.id;
@@ -327,11 +297,11 @@ function message (m, conn) {
 						sess.user.membership = Object.keys(m);
 						if(!err){
 							conn.send('init', {
-								sid: sess.cookie.value,
+								session: sess.cookie.value,
 								user: sess.user
 							});
 						}
-						session.set(conn.sid, sess);
+						session.set(conn.session, sess);
 					});
 					if(m.ref) {
 						users[m.ref] = users[user.from] || {};
@@ -368,8 +338,8 @@ function message (m, conn) {
 
 function room (r, conn) {
 	var user;
-	session.get({sid: conn.sid}, function(err, sess) {
-		if(!conn.sid) return;
+	session.get({session: conn.session}, function(err, sess) {
+		if(!conn.session) return;
 		if(typeof r === 'object') {
 			user = sess.user;
 			r.owner = user.id;
@@ -390,7 +360,7 @@ function room (r, conn) {
 				conn.send('room', data);
 			}
 		});
-		session.set(conn.sid, sess);
+		session.set(conn.session, sess);
 	});
 }
 
@@ -445,32 +415,75 @@ function validateNick(nick){
 	return (nick.match(/^[a-z][a-z0-9\_\-\(\)]{2,32}$/i)?nick!='img'&&nick!='css'&&nick!='sdk':false);
 }
 
-// ----- Outgoing send ----
+//// ----- Outgoing send ----
+//
+//exports.send = function (message, rooms) {
+//	message.text = message.text || "";
+//	log("Socket sending", message, "to", rooms);
+//	
+//	rooms.map(function(room) {
+//		var location, to = message.to;
+//		if(message.origin) {
+//			location= message.origin;
+//			delete message.origin;
+//		}
+//		//if(message.type == "text") core.occupants(message.to, function(err, data){console.log(err, data);});
+//		if(rConns[room]) rConns[room].map(function(conn) {
+//			message.to = room;
+//			conn.send('message', message);
+//		});
+//		if(location) message.origin = location;
+//		message.to = to;
+//	});
+//};
+//
+//
 
-exports.send = function (message, rooms) {
-	message.text = message.text || "";
-	log("Socket sending", message, "to", rooms);
-	
-	rooms.map(function(room) {
-		var location, to = message.to;
-		if(message.origin) {
-			location= message.origin;
-			delete message.origin;
-		}
-		//if(message.type == "text") core.occupants(message.to, function(err, data){console.log(err, data);});
-		if(rConns[room]) rConns[room].map(function(conn) {
-			message.to = room;
-			conn.send('message', message);
-		});
-		if(location) message.origin = location;
-		message.to = to;
-	});
-};
 
 
-exports.emit = function(type, action, room) {
-	if(rConns[room]) rConns[room].map(function(conn) {
-		action.to = room;
-		conn.send(type, action);
-	});		
-};
+//function init(data, conn) {
+//	var user, session = data.session, nick = data.nick, i;
+//	
+//	session.get({ session:session, suggestedNick:data.nick }, function(err, sess) {
+//		var rooms = sess.user.rooms;
+//		var i;
+//		conn.session = session;
+//		conn.rooms = [];
+//
+//		if (sess.user.id.indexOf("guest-")===0 && data.nick)	sess.user.id="guest-"+data.nick;
+//		if(sess.pid !== pid) {
+//			sess.pid = pid;
+//			for(i in rooms) {
+//				if(rooms.hasOwnProperty(i)) rooms[i] = 0;
+//			}
+//		}
+//		var query=[];
+//		if (sess.user.id.indexOf('guest-')!==0) {
+//			query.user=sess.user.id;
+//		}
+//		core.emit("members", query,function(err,d) {
+//			var m={};
+//			if (d) {
+//				for (i=0;i<d.length;i++) {
+//					m[d[i].room]=true;
+//				}
+//			}
+//			sess.user.membership = Object.keys(m);//Room added to user object
+//			
+//			conn.send('init', {
+//				session: sess.cookie.value,
+//				user: sess.user,
+//				clientTime: data.clientTime,
+//				serverTime: new Date().getTime(),
+//			});
+//
+//			//Temp for now.
+//			core.emit("init", {
+//				from: sess.user.id,
+//				session: "web:"+conn.socket.remoteAddress+":"+conn.session,
+//				time: new Date().getTime()
+//			});
+//			session.set(conn.session, sess);
+//		});
+//	});
+//}
