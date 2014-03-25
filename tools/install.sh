570c320919b40d13146f5558cd607a44cee7405f
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-03-25 12:11:07 +0530

    Ask to install the packages only in Ubuntu or fedora

--- tools/install.sh
+++ tools/install.sh
@@ -6,62 +6,62 @@ trap exit 1 INT
 # Detect the current ditro
 distro=$(grep '^NAME=' /etc/os-release | sed -e s/NAME=//g -e s/\"//g)
 
-# We only install packages for Ubuntu and Fedora
-if [[ ! "$distro" = "Fedora" && ! "$distro" = "Ubuntu" ]]; then
+if [[ "$distro" = "Fedora" || "$distro" = "Ubuntu" ]]; then
+    # Show the list of items to install
+    pkgs=($(whiptail --separate-output \
+            --title "Review items" \
+            --checklist "The following items will be installed." \
+            --ok-button "Install" \
+            --cancel-button "Skip" \
+            --notags 15 40 5 \
+            mysql "MySQL Server" on \
+            git "GIT Version Control" on \
+            nodejs "Node.js" on \
+            redis "Redis Server" on 3>&1 1>&2 2>&3))
+
+    # Iterate over all the items in the array and perform operations accordingly
+    for (( i = 0; i < ${#pkgs[@]} ; i++ )); do
+        case "${pkgs[$i]}" in
+            mysql)
+                case "$distro" in
+                    Ubuntu)
+                        sudo apt-get install -y mysql-client mysql-server;;
+                    Fedora)
+                        sudo yum install -y mysql mysql-server;;
+                esac;;
+            git)
+                case "$distro" in
+                    Ubuntu)
+                        sudo apt-get install -y git;;
+                    Fedora)
+                        sudo yum install -y git;;
+                esac;;
+            nodejs)
+                case "$distro" in
+                    Ubuntu)
+                        sudo add-apt-repository ppa:chris-lea/node.js
+                        sudo apt-get update
+                        sudo apt-get install -y nodejs;;
+                    Fedora)
+                        sudo yum install -y nodejs npm;;
+                esac;;
+            redis)
+                case "$distro" in
+                    Ubuntu)
+                        sudo add-apt-repository ppa:rwky/redis
+                        sudo apt-get update
+                        sudo apt-get install -y redis-server;;
+                    Fedora)
+                        sudo yum install -y redis;;
+                esac;;
+        esac
+    done
+else
+    # We only install packages for Ubuntu and Fedora
     echo "Unsupported distro. You will need to install the dependencies manually. Continue anyway [y/n]?"
     read ans
     [[ "$ans" = [Yy] ]] || exit 1
 fi
-
-# Show the list of items to install
-pkgs=($(whiptail --separate-output \
-        --title "Review items" \
-        --checklist "The following items will be installed." \
-        --ok-button "Install" \
-        --cancel-button "Skip" \
-        --notags 15 40 5 \
-        mysql "MySQL Server" on \
-        git "GIT Version Control" on \
-        nodejs "Node.js" on \
-        redis "Redis Server" on 3>&1 1>&2 2>&3))
-
-# Iterate over all the items in the array and perform operations accordingly
-for (( i = 0; i < ${#pkgs[@]} ; i++ )); do
-    case "${pkgs[$i]}" in
-        mysql)
-            case "$distro" in
-                Ubuntu)
-                    sudo apt-get install -y mysql-client mysql-server;;
-                Fedora)
-                    sudo yum install -y mysql mysql-server;;
-            esac;;
-        git)
-            case "$distro" in
-                Ubuntu)
-                    sudo apt-get install -y git;;
-                Fedora)
-                    sudo yum install -y git;;
-            esac;;
-        nodejs)
-            case "$distro" in
-                Ubuntu)
-                    sudo add-apt-repository ppa:chris-lea/node.js
-                    sudo apt-get update
-                    sudo apt-get install -y nodejs;;
-                Fedora)
-                    sudo yum install -y nodejs npm;;
-            esac;;
-        redis)
-            case "$distro" in
-                Ubuntu)
-                    sudo add-apt-repository ppa:rwky/redis
-                    sudo apt-get update
-                    sudo apt-get install -y redis-server;;
-                Fedora)
-                    sudo yum install -y redis;;
-            esac;;
-    esac
-done
 
 # Are we inside the cloned repository?
 grep "\"name\": \"Scrollback\"" "../package.json" > /dev/null 2>&1
