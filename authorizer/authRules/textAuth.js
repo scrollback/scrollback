23c52070133d02058f84096c686900cd42ce1a7b
Amal Antony <amalantony06@gmail.com>
2014-04-04 15:14:49 +0530

    added the unit test for authorizer

--- authorizer/authRules/textAuth.js
+++ authorizer/authRules/textAuth.js
@@ -1,7 +1,7 @@
 var permissionLevels = require('../permissionWeights.js');
 module.exports = function(core){
 	core.on('text', function(action, callback){
-		if(permissionLevels[action.room.params.readLevel] <= permissionLevels[action.user.role]) return callback();
+		if(permissionLevels[action.room.params.writeLevel] <= permissionLevels[action.role]) return callback();
 		else return callback(new Error('ERR_NOT_ALLOWED'));
 	});
 };
