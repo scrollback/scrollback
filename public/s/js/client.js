55f7d46d3d262296e54aeaf36adac48606e3c4ba
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-04-04 18:46:16 +0530

    use vaiables for widths and fix input box hidden by textboz in android.

--- public/s/js/client.js
+++ public/s/js/client.js
@@ -28,7 +28,7 @@
             timeout = setTimeout(function(){
                 $(".mainview").removeClass("scrolling");
                 timeout = 0;
-            }, 2000);
+            }, 1000);
 
             $(".mainview").addClass("scrolling");
         });
