9fb47832801f696802cf16f647b35d9aaa270e1a
aravind <aravindet@gmail.com>
2014-04-11 21:57:52 +0530

    Forgot to add mock-libsb

---
+++ public/s/js/mock-libsb.js
@@ -0,0 +1,68 @@
+/* global window, Bus, generate */
+
+/* jshint unused: false */
+
+window.libsb = (function() {
+	var core = new Bus();
+	
+	function text(time) {
+		return {
+			from: 'guest-' + generate.word(8),
+			to: 'testroom',
+			text: generate.paragraph(2),
+			time: time
+		};
+	}
+	
+	function gotText() {
+		core.emit('text-dn', text(new Date().getTime()));
+		setTimeout(gotText, 3000 + (Math.random()-0.5)*2*2500);
+	}
+	
+	return {
+		user: {id: 'guest-default', picture: 'http://gravatar.com/avatar/hello?s=48'},
+		rooms: [],
+		occupantOf: [],
+		memberOf: [],
+		isConnected: false,
+		
+		connect: function () { core.emit('connected'); },
+		disconnect: function () { core.emit('disconnected'); },
+		
+		emit: core.emit.bind(core),
+		on: core.on.bind(core),
+		
+		getTexts: function (query, cb) {
+			var i, l, r=[], now = (new Date()).getTime(), time;
+			
+			console.log("query", query.time, query.before, query.after);
+			
+			query.before = query.before || 0;
+			query.after = query.after || 0;
+			query.time = query.time || now;
+			query.to = query.to || 'sandbox';
+			time = query.time - (query.before * 3000);
+			for(i=0, l=query.before + query.after; i<l; i++) {
+				if(time >= now) { r.push(false); break; }
+				if(time < now - 100 * 3000) {
+					if(r.length === 0) r.push(false);
+					continue;
+				}
+				r.push(text(time + (Math.random()-0.5)*2*2500));
+				time += 3000;
+			}
+			
+			setTimeout(function() { cb(null, r); }, 500);
+		},
+		getLabels: function (query, cb) {},
+		getOccupants: function (rid, cb) {},
+		getMembers: function (rid, cb) {},
+		getRooms: function (query, cb) {},
+		
+		enter: function (rid, cb) {},
+		leave: function (rid, cb) {},
+		join: function (rid, cb) {},
+		part: function (rid, cb) {},
+	};
+	
+}());
