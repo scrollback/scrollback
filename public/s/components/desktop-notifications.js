da3ee33f83cf1c946e9e74b155d59b21094d2af5
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-05-06 15:30:44 +0530

    Initial work on notifications

---
+++ public/s/components/desktop-notifications.js
@@ -0,0 +1,27 @@
+/*jslint browser: true, indent: 4, regexp: true*/
+/*global $, Notification, webkitNotifications*/
+
+function showNotifcation(title, body, icon, id, func) {
+	var notification;
+
+	if ("Notification" in window) {
+		if (Notification.permission === "granted") {
+			notification = new Notification(title, { dir: "auto", lang: "en-US", body: body, tag: id, icon: icon });
+			notification.onclick = func;
+		} else if (Notification.permission !== 'denied') {
+			Notification.requestPermission();
+		}
+	} else if ("webkitNotifications" in window) {
+		if (webkitNotifications.checkPermission() === 0) {
+			notification = webkitNotifications.createNotification(icon, title, body);
+			notification.show();
+			notification.onclick = func;
+		} else {
+			webkitNotifications.requestPermission();
+		}
+	}
+}
+
+$(".send").on("click", function() {
+	showNotifcation("Hi", "This is notification", null, null, function() { window.location.href = "http://google.com"; });
+});
