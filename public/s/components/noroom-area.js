3a5a51eabf96223daed8f5ef653884f6b9372012
Amal Antony <amalantony06@gmail.com>
2014-05-26 17:21:38 +0530

    Page reload when user claims a new room.

--- public/s/components/noroom-area.js
+++ public/s/components/noroom-area.js
@@ -23,6 +23,9 @@
         libsb.emit('room-up', roomObj,function(err, data){
            libsb.emit("navigate", {mode: 'normal', tab: 'info'});
         });
+        setTimeout(function(){
+            location.reload(); // reload page on claim.
+        });
     });
     $("#login-and-create-room-button").click(function(){
         libsb.emit("navigate", {mode: 'normal'});
