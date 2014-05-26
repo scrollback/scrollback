aed4c0f5efa63aad58e75705786e5c3a5eb87c56
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-05-26 19:32:08 +0530

    Fix configure button

--- public/s/components/noroom-area.js
+++ public/s/components/noroom-area.js
@@ -1,4 +1,4 @@
-(function(){ 
+(function(){
     libsb.on('error-dn', function(err, data){
         if(err.message === "NO_ROOM_WITH_GIVEN_ID"){
             libsb.emit("navigate", {mode:'noroom'});
@@ -6,20 +6,20 @@
     });
     $("#create-room-button").click(function(){
         var roomObj = {
-            id: generate.uid(), 
-            type: 'room', 
+            id: generate.uid(),
+            type: 'room',
             to: currentState.room,
-            room: { 
-                id: currentState.room, 
-                description: '', 
+            room: {
+                id: currentState.room,
+                description: '',
                 params: {
                     irc: {},
                     allowSEO: true,
-                    wordban: false 
-                } 
-            }, 
-            user: { id: libsb.user } 
-        }; 
+                    wordban: false
+                }
+            },
+            user: { id: libsb.user }
+        };
         libsb.emit('room-up', roomObj,function(err, data){
            libsb.emit("navigate", {mode: 'normal', tab: 'info'});
         });
@@ -28,8 +28,8 @@
         });
     });
     $("#login-and-create-room-button").click(function(){
-       if ($("body").hasClass("guest-user")) {
+       if ($("body").hasClass("role-guest")) {
            lace.popover.show($(this), $("#login-menu").html());
-       }  
+       }
     });
 })();
