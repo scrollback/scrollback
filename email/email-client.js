f2efaed9b8871be9d9e7948fbb8f9641ede01cfe
Kamal <kkjoshi.it@gmail.com>
2014-06-05 13:20:17 +0530

    email params validation

--- email/email-client.js
+++ email/email-client.js
@@ -18,7 +18,7 @@ libsb.on('pref-show', function(tabs, next){
         var div = $('<div>').addClass('list-view list-view-email-settings');
         
         if(!user.params.email) user.params.email = {};
-        if(user.params.email.notifications == "undefined") user.params.email.notifications = true;
+        if(user.params.email.notifications === undefined) user.params.email.notifications = true;
         
         div.append(formField("Mention notifications via email", "toggle", "mention", user.params.email.notifications));
         
