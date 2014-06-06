8db99d60bbd81c895d3b2c856bb33de9501e8ff5
Kamal <kkjoshi.it@gmail.com>
2014-06-06 15:22:48 +0530

    send welcome email in sign up

---
+++ email/welcomeEmail.js
@@ -0,0 +1,59 @@
+var config = require('../config.js');
+var log = require("../lib/logger.js");//.tag("mail");
+var fs=require("fs"),jade = require("jade");
+var emailConfig = config.email;
+var welcomeEmailJade;
+var core;
+var send = require('./sendEmail.js');
+
+/**
+ * It is used to Send Email
+ * Listen to Room Event
+ * @param coreObject
+ */
+module.exports = function(coreObject) {
+	core = coreObject;
+	if (config.email && config.email.auth) {
+		init();
+		core.on('user',emailRoomListner , "gateway"); 
+	}
+	else {
+		log("email module is not enabled");
+	}
+
+};
+
+function emailRoomListner(action, callback){
+    log("user welcome email ", action);
+	callback();
+     if(!action.old.id) {//Signup
+        sendWelcomeEmail(action.user);
+     }
+}
+
+function init() {
+    //read welcome email jade
+    fs.readFile(__dirname + "/views/welcomeEmail.jade", "utf8", function(err, data) {
+        if(err) throw err;
+        welcomeEmailJade = jade.compile(data,  {basedir: __dirname + "/views/"});
+        log("welcome emails " , welcomeEmailJade );
+    });
+}
+
+/**
+ *send welcome mail to user
+ *@param {Object} user
+ */
+function sendWelcomeEmail(user) {
+    var emailHtml = welcomeEmailJade(user);
+    var emailAdd = false;
+    user.identities.forEach(function (u) {
+        if (u.indexOf('mailto:') === 0) {
+            emailAdd = u.substring(7);
+        }
+    });
+    if (emailAdd) {
+        log("sending welcome email." , emailAdd);
+        send(emailConfig.from, emailAdd, "Welcome", emailHtml);
+    }
+}
