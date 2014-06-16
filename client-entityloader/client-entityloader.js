ddd8a503fa6750cfcb8c2a7a83ee21e4bb7a091b
harish kumar v <harry.softer@gmail.com>
2014-06-16 11:44:04 +0530

    Adding the client entity loader file.

---
+++ client-entityloader/client-entityloader.js
@@ -0,0 +1,22 @@
+/* global libsb*/
+var generate = require('../lib/generate.js');
+
+module.exports = function() {
+	libsb.on("navigate", function(state, next) {
+		if(state.roomName) {
+			libsb.getRooms({ref: state.roomName}, function(err, data) {
+				if(err) {
+					throw err; // handle this better
+					return;
+				}
+				if(!data || !data.results || !data.results.length) {
+					state.roomName = "pending";
+					state.room = null;
+				}else{
+					state.room = data.results[0];
+				}
+				next();
+			});
+		}
+	});
+};
