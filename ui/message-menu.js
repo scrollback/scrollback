c1e854a63b1e14de6cda1ff52e1faede93a5ad36
Amal Antony <amalantony06@gmail.com>
2014-06-23 14:52:47 +0530

    Added authorizer-client and message-menu to repo

---
+++ ui/message-menu.js
@@ -0,0 +1,30 @@
+/*
+	UI component to show the menu to the user, when he clicks on a message
+*/
+/* global $, libsb, document, currentState */
+// chat-more
+var showMenu = require('./showmenu.js');
+$(function(){
+	$(document).click('.chat-area', function(event){
+		var elem = $(event.target).closest('.chat-item').get(0);
+		var target = $(elem).find('.chat-more').get(0);
+		var messageId = elem ? elem.id : "";
+		var role = 'registered';
+		if(messageId){
+			if(/^guest-/.test(libsb.user.id)){
+				role = 'guest';
+			}else{
+				libsb.memberOf.forEach(function(room){
+					if(room.id === currentState.roomName && room.role === "owner"){
+						role = 'owner';
+					}
+				});
+			}
+			libsb.emit('text-menu', {role: role, target: elem}, function(err, menu){
+				delete menu.role;
+				delete menu.target;
+				showMenu({origin: target, items: menu});
+			});
+		}
+	});
+});
