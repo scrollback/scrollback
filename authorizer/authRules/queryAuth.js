a79f4b2425cbb7c1ec1d80f6a2eed29999b7fbd8
Kamal <kkjoshi.it@gmail.com>
2014-06-27 19:23:23 +0530

    added init.origin, bug fixes

--- authorizer/authRules/queryAuth.js
+++ authorizer/authRules/queryAuth.js
@@ -13,7 +13,7 @@ module.exports = function(core){
 		if(query.q && !query.room) {
 			return callback();
 		}
-		if(!query.room.guides || !action.room.guides.authorizer || !query.room.guides.authorizer.readLevel) return callback();
+		if(!query.room.guides || !query.room.guides.authorizer || !query.room.guides.authorizer.readLevel) return callback();
 		if(query.room.guides.authorizer.readLevel === undefined) query.room.guides.authorizer.readLevel = 'guest';
 		if(permissionLevels[query.room.guides.authorizer.readLevel] <= permissionLevels[query.user.role]) return callback();
 		else return callback(new Error('ERR_NOT_ALLOWED'));
