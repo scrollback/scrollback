e83f9bced60707cbf0eeb3a295e9c25e473b0733
Amal Antony <amalantony06@gmail.com>
2014-07-01 22:39:14 +0530

    Fixed a bug in textAuth

--- authorizer/authRules/textAuth.js
+++ authorizer/authRules/textAuth.js
@@ -4,7 +4,7 @@ module.exports = function(core){
         if(!/^web:/.test(action.session)) return callback();
 		if(!action.room.guides || !action.room.guides.authorizer || !action.room.guides.authorizer.writeLevel) return callback();
 		if(typeof action.room.guides.authorizer.writeLevel === 'undefined') action.room.guides.authorizer.writeLevel = 'guest';
-		if(permissionLevels[action.room.guides.authorizer.writeLevel] <= permissionLevels[action.role]) return callback();
+		if(permissionLevels[action.room.guides.authorizer.writeLevel] <= permissionLevels[action.user.role]) return callback();
 		else return callback(new Error('ERR_NOT_ALLOWED'));
 	}, "authorization");
 };
