e1b313cd7c32436c79b34fa29aa385b4ff8c08d0
Amal Antony <amalantony06@gmail.com>
2014-07-02 14:12:13 +0530

    Authorization error fix.

--- authorizer/authRules/queryAuth.js
+++ authorizer/authRules/queryAuth.js
@@ -4,12 +4,22 @@ var internalSession = Object.keys(config.whitelists)[0];
 module.exports = function(core){
     
 	core.on('getTexts', function(query, callback){
+		if(query.user.role === "none"){
+			if(/^guest-/.test(query.user.id)) query.user.role = 'guest';
+		}else{
+			query.user.role = "registered";	
+		} 
 		if(!query.room.guides || !query.room.guides.authorizer || !query.room.guides.authorizer.readLevel) return callback();
 		if(query.room.guides || typeof query.room.guides.authorizer.readLevel === 'undefined') query.room.guides.authorizer.readLevel = 'guest';
 		if(permissionLevels[query.room.guides.authorizer.readLevel] <= permissionLevels[query.user.role]) return callback();
 		else return callback(new Error('ERR_NOT_ALLOWED'));
 	}, "authorization");
 	core.on('getThreads', function(query, callback) {
+		if(query.user.role === "none"){
+			if(/^guest-/.test(query.user.id)) query.user.role = 'guest';
+		}else{
+			query.user.role = "registered";	
+		} 
 		var readLevel;
 		if(query.q && !query.room) {
 			return callback();
@@ -21,7 +31,12 @@ module.exports = function(core){
     
     ['getRooms', 'getUsers'].forEach(function(e) {
         core.on(e, function(query, next) {
-            if(query.identity && query.user.role !== 'su' && query.session !== internalSession) next(new Error("ERR_QUERY_NOT_ALLOWED"));
+            if(query.user.role === "none"){
+			if(/^guest-/.test(query.user.id)) query.user.role = 'guest';
+			}else{
+				query.user.role = "registered";	
+			} 
+			if(query.identity && query.user.role !== 'su' && query.session !== internalSession) next(new Error("ERR_QUERY_NOT_ALLOWED"));
             else next();
         }, "authorization");
     });
--- authorizer/authRules/textAuth.js
+++ authorizer/authRules/textAuth.js
@@ -1,6 +1,11 @@
 var permissionLevels = require('../permissionWeights.js');
 module.exports = function(core){
 	core.on('text', function(action, callback){
+		if(action.user.role === "none"){
+			if(/^guest-/.test(action.user.id)) action.user.role = 'guest';
+		}else{
+			action.user.role = "registered";	
+		} 
         if(!/^web:/.test(action.session)) return callback();
 		if(!action.room.guides || !action.room.guides.authorizer || !action.room.guides.authorizer.writeLevel) return callback();
 		if(typeof action.room.guides.authorizer.writeLevel === 'undefined') action.room.guides.authorizer.writeLevel = 'guest';
