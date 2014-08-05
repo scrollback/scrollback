dc9becd72f32304874d827e6e4f17eb577a2064d
Amal Antony <amalantony06@gmail.com>
2014-08-05 12:48:47 +0530

    Schema change for admitExpelAuth

--- authorizer/authRules/admitExpelAuth.js
+++ authorizer/authRules/admitExpelAuth.js
@@ -1,5 +1,8 @@
+var permissionWeights = require('../permissionWeights.js');
 var SbError = require('../../lib/SbError.js');
+
 module.exports = function (core) {
+	
 	core.on('admit', function (action, callback) {
 		if (action.room.guides.authorizer && action.room.guides.authorizer.openFollow === undefined) {
 			action.room.guides.authorizer.openFollow = true;
@@ -11,12 +14,22 @@ module.exports = function (core) {
 			currentRole: action.user.role
 		}));
 		else if (action.user.role === "owner" || action.user.role === "su") return callback();
-		else if (action.user.role === "moderator" && action.victim.invitedRole !== "owner" && action.victim.invitedRole !== "moderator") return callback();
-		else if (action.user.role === "follower" && action.victim.role === "registered" && action.room.guides.authorizer.openFollow) return callback();
-		else if (action.user.role !== "gagged" && action.user.role !== "banned" && action.user.role !== "registered" && action.user.role !== "guest") {
-			if (action.victim.requestedRole && action.victim.requestedRole === action.user.role) return callback();
-			else {
-				action.victim.invitedRole = action.user.role;
+		
+		else if (action.role !== "owner" && action.role !== "moderator" && action.user.role === "moderator") {
+			return callback();
+		}
+		
+		else if (action.role === "follower" && action.user.role === "follower" && action.victim.role === "registered" && action.room.guides.authorizer.openFollow) {
+			return callback();
+		}
+		
+		else if (action.user.role === "moderator" || action.user.role === "owner") {
+			if (permissionWeights[action.role] > permissionWeights[action.user.role]) {
+				return callback();
+			} else {
+				action.user.transitionRole = action.user.role;
+				action.user.transitionType = 'timeout';
+				action.user.transitionTime = 60 * 60 * 1000; // 1 hour timeout for demotion ??
 				return callback();
 			}
 		} else {
@@ -29,6 +42,7 @@ module.exports = function (core) {
 		}
 	}, "authorization");
 
+	
 	core.on('expel', function (action, callback) {
 		if (action.user.role === "guest") return callback(new SbError('ERR_NOT_ALLOWED', {
 			source: 'authorizer',
@@ -37,7 +51,8 @@ module.exports = function (core) {
 			currentRole: action.user.role
 		}));
 		if (action.user.role === "owner" || action.user.role === "su") return callback();
-		else if (action.user.role === "moderator" && action.victim.role !== "moderator" && action.victim.role !== "owner") {
+		else if (action.user.role === "moderator" && action.victim.role !== "moderator" && action.victim.role !== "owner" && 
+				 action.role === "registered") {
 			return callback();
 		}
 		return callback(new SbError('ERR_NOT_ALLOWED', {
