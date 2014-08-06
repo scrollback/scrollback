733ab0f0aecace778a1369e1f82a9df56fb49d2f
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-08-06 14:27:09 +0530

    Don't change tab while switching rooms, fix JSCS warnings

--- ui/room-item.js
+++ ui/room-item.js
@@ -4,14 +4,14 @@
 var roomEl = {},
 	$template = $(".room-item").eq(0);
 
-roomEl.render = function ($el, id, index) {
+roomEl.render = function($el, id, index) {
 	$el = $el || $template.clone(false);
 
 	$el.find(".room-name").text(id);
 	$el.attr("id", "room-item-" + id);
 	$el.attr("data-index", index);
 
-	if(window.currentState.roomName === id) {
+	if (window.currentState.roomName === id) {
 		$(".room-item.current").removeClass("current");
 		$el.addClass("current");
 	}
