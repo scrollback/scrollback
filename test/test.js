960bda26fec92cf6c615e19488a09da9c7ac4c85
kamal <kkjoshi.it@gmail.com>
2014-08-12 19:10:30 +0530

    functional test cases

--- test/test.js
+++ test/test.js
@@ -1,7 +1,2 @@
-var core = require("../test/mock-core.js")();
-var assert = require("assert");
-require("../threader/threader-test.js");
-require("../anti-abuse/anti-abuse-test.js");
-require("../validator/validator-test.js");
-require("../entityloader/entityloader-test.js");
-
+require("../irc/irc-test.js");
+require("../authorizer/tests/authorizer-test.js");
