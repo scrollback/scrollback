f089d768fc84012c70f85acaadd250911a2e3119
Amal Antony <amalantony06@gmail.com>
2014-08-20 18:26:12 +0530

    Added functionality to cache user queries

--- localStorage/membersCaching.js
+++ localStorage/membersCaching.js
@@ -1,16 +1,17 @@
+/* global libsb, currentState */ 
 var userCache = require('./userCache.js');
 
 module.exports = function (core) {
-	
+
 	core.on("getUsers", function (query, next) {
 		// run before socket
-		// TODO: add result source as localStorage
+		// TODO: add result source us localStorage
 		if (query.hasOwnProperty("memberOf")) {
 			// fetching member info
 			if (query.hasOwnProperty("ref")) { // info for a single member
 				query.results = userCache.getMembers(query.to, query.ref);
 				query.resultSource = "localStrorage";
-			} else { // return all members of requested room. 
+			} else { // return all members of requested room.
 				query.results = userCache.getMembers(query.to);
 				query.resultSource = "localStorage";
 			}
@@ -20,19 +21,59 @@ module.exports = function (core) {
 			query.resultSource = "localStorage";
 		} else if (query.hasOwnProperty("ref")) {
 			// ref alone without memberOf and occupantOf
-			// decide how to handle this. Have to return the user object.  
+			// decide how to handle this. Have to return the user object.
 		}
 		next();
 	}, 100);
-	
+
 	core.on("getUsers", function (query, next) {
 		// run after socket, do result caching here
-		if (query.hasOwnProperty("memberOf")) {
-			userCache.putMembers(query.results);
+		if (query.resultSource && query.resultSource === "localStorage") {
+            return next();
+        }
+        if (query.hasOwnProperty("memberOf")) {
+			userCache.putMembers(query.to, query.results);
 		} else if (query.hasOwnProperty("occupantOf")) {
-			userCache.putOccupants(query.results);
+			userCache.putOccupants(query.to, query.results);
 		}
 		next();
 	}, 8);
+
+	core.on("join-dn", function (join, next) {
+        userCache.putMembers(join.to, join.user);        
+        next();
+	}, 900);
+
+	core.on("part-dn", function (part, next) {
+        userCache.removeMembers(part.to, part.user);
+        next();
+	}, 900);
+
+    core.on("away-dn", function(away, next) {
+        if (away.from === libsb.user.id) {
+            userCache.saveUsers();
+        }
+        userCache.removeOccupants(away.to, away.user);        
+        next();
+    }, 900);
+
+    core.on("back-dn", function(back, next) {
+        if (back.from === libsb.user.id) {
+            userCache.populateMembers(back.to);
+            userCache.populateOccupants(back.to);
+            userCache.loadUsers();
+        }         
+        userCache.putOccupants(back.to, back.user);
+        next();
+    }, 900);
+
+	core.on("init-dn", function(init, next) {
+		// clear the LocalStorage entry for users.
+        userCache.deletePersistence();
+        userCache.populateMembers(currentState.roomName);
+        userCache.populateOccupants(currentState.roomName);
+        
+        next();
+	}, 500);
 
 };
--- localStorage/userCache.js
+++ localStorage/userCache.js
@@ -1,25 +1,136 @@
 /*
 	For members, a list of members have to maintained for each room since member roles can vary room to room.
-	
-	For occupants, a common occupant list can be maintained with each room having a list of pointers to users in this list. 
+
+	For occupants, a common occupant list can be maintained with each room having a list of pointers to users in this list.
 		By doing this the duplication of occupant objects can be eliminated.
 */
-var occupants = {}; 
-var roomOccupantList = {};
 
-var members = {};
+/* global localStorage, libsb */
+
+var roomOccupantList = {};
+var globalOccupantList = {};
+
+var roomMemberList = {};
 
 module.exports = {
+    populateMembers: function (room) {
+        // populate memebers of room into our structures.
+        var members;
+        libsb.emit("getUsers", {memberOf: room}, function (err, data) {
+           members = data.results; 
+           this.putMembers(room, members);
+        });
+    },
+    populateOccupants: function (room) {
+        var occupants;
+        libsb.emit("getUsers", {occupantOf: room}, function (err, data) {
+            occupants = data.results;
+            this.putOccupants(room, occupants);
+        });
+    },
 	getMembers: function (room, member) {
-
+		if (/^guest-/.test(member.id)) {
+			return []; // guest cannot be a member
+		}
+		if (typeof member !== "undefined") {
+			// return the single member as an Array
+            if (!roomMemberList[room].hasOwnProperty(member.id)) {
+                return [];
+            }
+			return [roomMemberList[room][member.id]];
+		} else {
+			// return all members of the room
+			var mList = roomMemberList[room];
+			var res = [];
+			mList.forEach(function(m) {
+				res.push(roomMemberList[room][m]);
+			});
+			return res;
+		}
 	},
 	getOccupants: function (room, occupant) {
-
+        if (typeof occupant !== "undefined") {
+            // return the singe occupant of this room 
+            
+            if (!roomOccupantList[room].hasOwnProperty(occupant.id)) {
+                // the room does not have this occupant
+                return [];
+            } else {
+               // return occupant object 
+                return [globalOccupantList[occupant.id]];
+            }
+        } else {
+            // return all occupants of this room as Array.
+            var res = [];
+            for (var r in roomOccupantList[room]) {
+                res.push(globalOccupantList[r]);
+            }
+            return res;
+        }
 	},
 	putMembers: function (room, memberList) {
+		if (!(memberList instanceof Array)) {
+			memberList = [memberList];
+		}
+
+		memberList.forEach(function (member) {
+			if (!roomMemberList.hasOwnProperty(room)) {
+				roomMemberList[room] = {};
+				roomMemberList[room][member.id] = member;
+			} else {
+				roomMemberList[room][member.id] = member;
+			}
+		});
 
 	},
 	putOccupants: function (room, occupantList) {
-		
-	}
-};
+		if (!(occupantList instanceof Array)) {
+			occupantList = [occupantList];
+		}
+
+		// add each user to globalOccupantList if he is not there already
+		// then make entry inside roomOccupantList
+		occupantList.forEach(function (occupant) {
+			if (!globalOccupantList.hasOwnProperty(occupant)) {
+				globalOccupantList[occupant.id] = occupant;
+			}
+			if (!roomOccupantList.hasOwnProperty(room)){
+                roomOccupantList[room] = {};
+            }
+            roomOccupantList[room][occupant.id] = true;
+		});
+
+	},
+	removeMembers: function (room, memberList) {
+        if (!(memberList instanceof Array)) {
+            memberList = [memberList];
+        }
+        memberList.forEach(function(m) {
+            delete roomMemberList[room][m.id];
+        });
+	},
+	removeOccupants: function (room, occupantList) {
+        if (!(occupantList instanceof Array)) {
+            occupantList = [occupantList];
+        }
+        occupantList.forEach(function(o) {
+            delete roomOccupantList[room][o.id];
+        });
+	},
+    loadUsers : function () {
+        roomOccupantList = JSON.parse(localStorage.roomOccupantList);
+        roomMemberList = JSON.parse(localStorage.roomMemberList);
+        globalOccupantList = JSON.parse(localStorage.globalOccupantList);
+    }, 
+    saveUsers : function () {
+        localStorage.roomOccupantList = JSON.stringify(roomOccupantList);
+        localStorage.roomMemberList = JSON.stringify(roomMemberList);
+        localStorage.globalOccupantList = JSON.stringify(globalOccupantList);
+    },
+    deletePersistence: function () {
+		// delete LS entry for users.
+        delete localStorage.roomOccupantList;
+        delete localStorage.roomMemberList;
+        delete localStorage.globalOccupantList;
+   	}
+}
