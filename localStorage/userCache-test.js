f583ee7eb9c1a37d5e13a78e2f74e61f1f026630
Amal Antony <amalantony06@gmail.com>
2014-08-20 18:26:46 +0530

    Added userCache test to repo

---
+++ localStorage/userCache-test.js
@@ -0,0 +1,42 @@
+/*global describe, it*/
+var assert = require('assert');
+
+var userCache = require('./userCache.js');
+
+describe("User cache ", function () {
+    it("should populate a room ", function () {
+
+    });
+
+    it("should add a new member to a room that does not exist ", function () {
+
+    });
+
+    it("should add a new member to a room that already exists ", function () {
+
+    });
+
+    it("should add a group of members to a room", function () {
+
+    });
+
+    it("should add an occupant to a room does not exist", function () {
+
+    });
+
+    it("should add an occupant to a room that already exists", function () {
+
+    });
+
+    it ("should add a group of occupants to a room", function () {
+
+    });
+
+    it("should remove a member from a room", function () {
+
+    });
+
+    it("should remove an occupant from a room", function () {
+
+    });
+});
