aaf6857a14dffaa8c5a186003dbb9b53ac26bdbd
Amal Antony <amalantony06@gmail.com>
2014-08-21 11:11:21 +0530

    Unit tests for Usercache.

--- localStorage/userCache-test.js
+++ localStorage/userCache-test.js
@@ -1,42 +1,56 @@
 /*global describe, it*/
 var assert = require('assert');
+var _ = require('underscore');
 
 var userCache = require('./userCache.js');
+var members = [{id: 'member1', description: "testmember1"}, {id: 'member2', description: "testmember2"}];
+var occupants = [{id: 'occupant1', description: "testoccupant1"}, {id: 'occupant2', description: "testoccupant2"}];
 
 describe("User cache ", function () {
-    it("should populate a room ", function () {
 
-    });
-
-    it("should add a new member to a room that does not exist ", function () {
-
+    it("should add new members to a room that does not exist ", function () {
+		userCache.putMembers("testroom1", members);
+		var res = userCache.getMembers("testroom1");
+		assert.equal(_.isEqual(members, res), true, "Assertion error!");
     });
 
     it("should add a new member to a room that already exists ", function () {
-
+		var newMember = {id: 'member3', description: "testmember3"};
+		members.push(newMember);
+		
+		userCache.putMembers("testroom1", newMember);
+		
+		var res = userCache.getMembers("testroom1");
+		assert.equal(_.isEqual(members, res), true, "Assertion error!");
     });
 
-    it("should add a group of members to a room", function () {
-
+    it("should add occupants to a room that does not exist", function () {
+		userCache.putOccupants("testroom1", occupants);
+		var res = userCache.getOccupants("testroom1");
+		assert.equal(_.isEqual(occupants, res), true, "Assertion error!");
     });
-
-    it("should add an occupant to a room does not exist", function () {
-
-    });
-
+	
+	// get single occupant, member (testing ref query)
+	
     it("should add an occupant to a room that already exists", function () {
-
-    });
-
-    it ("should add a group of occupants to a room", function () {
-
+		var newOccupant = {id: 'occupant3', description: "testoccupant3"};
+		userCache.putOccupants("testroom1", newOccupant);
+		occupants.push(newOccupant);
+		var res = userCache.getOccupants("testroom1");
+		assert.equal(_.isEqual(occupants, res), true, "Assertion Error!");
     });
 
     it("should remove a member from a room", function () {
-
+		userCache.removeMembers("testroom1", "member1");
+		var res = userCache.getMembers("testroom1");
+		members.splice(0, 1);
+		assert.equal(_.isEqual(members, res), true, "Assertion Error!");
     });
 
     it("should remove an occupant from a room", function () {
-
+		userCache.removeOccupants("testroom1", "occupant1");
+		var res = userCache.getOccupants("testroom1");
+		occupants.splice(0, 1);
+		assert.equal(_.isEqual(occupants, res), true, "Assertion Error!");
     });
 });
--- localStorage/userCache.js
+++ localStorage/userCache.js
@@ -13,60 +13,64 @@ var globalOccupantList = {};
 var roomMemberList = {};
 
 module.exports = {
-    populateMembers: function (room) {
-        // populate memebers of room into our structures.
-        var members;
-        libsb.emit("getUsers", {memberOf: room}, function (err, data) {
-           members = data.results; 
-           this.putMembers(room, members);
-        });
-    },
-    populateOccupants: function (room) {
-        var occupants;
-        libsb.emit("getUsers", {occupantOf: room}, function (err, data) {
-            occupants = data.results;
-            this.putOccupants(room, occupants);
-        });
-    },
-	getMembers: function (room, member) {
-		if (/^guest-/.test(member.id)) {
-			return []; // guest cannot be a member
-		}
+	populateMembers: function (room) {
+		// populate memebers of room into our structures.
+		var members;
+		libsb.emit("getUsers", {
+			memberOf: room
+		}, function (err, data) {
+			members = data.results;
+			this.putMembers(room, members);
+		});
+	},
+	populateOccupants: function (room) {
+		var occupants;
+		libsb.emit("getUsers", {
+			occupantOf: room
+		}, function (err, data) {
+			occupants = data.results;
+			this.putOccupants(room, occupants);
+		});
+	},
+	getMembers: function (room, memberId) {
 		if (typeof member !== "undefined") {
+			if (/^guest-/.test(memberId)) {
+				return []; // guest cannot be a member
+			}
 			// return the single member as an Array
-            if (!roomMemberList[room].hasOwnProperty(member.id)) {
-                return [];
-            }
-			return [roomMemberList[room][member.id]];
+			if (!roomMemberList[room].hasOwnProperty(memberId)) {
+				return [];
+			}
+			return [roomMemberList[room][memberId]];
 		} else {
 			// return all members of the room
 			var mList = roomMemberList[room];
 			var res = [];
-			mList.forEach(function(m) {
+			for (var m in mList) {
 				res.push(roomMemberList[room][m]);
-			});
+			}
 			return res;
 		}
 	},
-	getOccupants: function (room, occupant) {
-        if (typeof occupant !== "undefined") {
-            // return the singe occupant of this room 
-            
-            if (!roomOccupantList[room].hasOwnProperty(occupant.id)) {
-                // the room does not have this occupant
-                return [];
-            } else {
-               // return occupant object 
-                return [globalOccupantList[occupant.id]];
-            }
-        } else {
-            // return all occupants of this room as Array.
-            var res = [];
-            for (var r in roomOccupantList[room]) {
-                res.push(globalOccupantList[r]);
-            }
-            return res;
-        }
+	getOccupants: function (room, occupantId) {
+		if (typeof occupantId !== "undefined") {
+			// return the singe occupant of this room 
+
+			if (!roomOccupantList[room].hasOwnProperty(occupantId)) {
+				// the room does not have this occupant
+				return [];
+			} else {
+				// return occupant object 
+				return [globalOccupantList[occupantId.id]];
+			}
+		} else {
+			// return all occupants of this room as Array.
+			var res = [];
+			for (var r in roomOccupantList[room]) {
+				res.push(globalOccupantList[r]);
+			}
+			return res;
+		}
 	},
 	putMembers: function (room, memberList) {
 		if (!(memberList instanceof Array)) {
@@ -94,43 +98,43 @@ module.exports = {
 			if (!globalOccupantList.hasOwnProperty(occupant)) {
 				globalOccupantList[occupant.id] = occupant;
 			}
-			if (!roomOccupantList.hasOwnProperty(room)){
-                roomOccupantList[room] = {};
-            }
-            roomOccupantList[room][occupant.id] = true;
+			if (!roomOccupantList.hasOwnProperty(room)) {
+				roomOccupantList[room] = {};
+			}
+			roomOccupantList[room][occupant.id] = true;
 		});
 
 	},
 	removeMembers: function (room, memberList) {
-        if (!(memberList instanceof Array)) {
-            memberList = [memberList];
-        }
-        memberList.forEach(function(m) {
-            delete roomMemberList[room][m.id];
-        });
+		if (!(memberList instanceof Array)) {
+			memberList = [memberList];
+		}
+		memberList.forEach(function (m) {
+			delete roomMemberList[room][m];
+		});
 	},
 	removeOccupants: function (room, occupantList) {
-        if (!(occupantList instanceof Array)) {
-            occupantList = [occupantList];
-        }
-        occupantList.forEach(function(o) {
-            delete roomOccupantList[room][o.id];
-        });
+		if (!(occupantList instanceof Array)) {
+			occupantList = [occupantList];
+		}
+		occupantList.forEach(function (o) {
+			delete roomOccupantList[room][o];
+		});
 	},
-    loadUsers : function () {
-        roomOccupantList = JSON.parse(localStorage.roomOccupantList);
-        roomMemberList = JSON.parse(localStorage.roomMemberList);
-        globalOccupantList = JSON.parse(localStorage.globalOccupantList);
-    }, 
-    saveUsers : function () {
-        localStorage.roomOccupantList = JSON.stringify(roomOccupantList);
-        localStorage.roomMemberList = JSON.stringify(roomMemberList);
-        localStorage.globalOccupantList = JSON.stringify(globalOccupantList);
-    },
-    deletePersistence: function () {
+	loadUsers: function () {
+		roomOccupantList = JSON.parse(localStorage.roomOccupantList);
+		roomMemberList = JSON.parse(localStorage.roomMemberList);
+		globalOccupantList = JSON.parse(localStorage.globalOccupantList);
+	},
+	saveUsers: function () {
+		localStorage.roomOccupantList = JSON.stringify(roomOccupantList);
+		localStorage.roomMemberList = JSON.stringify(roomMemberList);
+		localStorage.globalOccupantList = JSON.stringify(globalOccupantList);
+	},
+	deletePersistence: function () {
 		// delete LS entry for users.
-        delete localStorage.roomOccupantList;
-        delete localStorage.roomMemberList;
-        delete localStorage.globalOccupantList;
-   	}
-}
+		delete localStorage.roomOccupantList;
+		delete localStorage.roomMemberList;
+		delete localStorage.globalOccupantList;
+	}
+};
