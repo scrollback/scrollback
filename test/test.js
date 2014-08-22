ffefa4e73a838857987250f9ad1e032d3ffae10f
kamal <kkjoshi.it@gmail.com>
2014-08-22 17:49:24 +0530

    fixed threader test

--- test/test.js
+++ test/test.js
@@ -3,3 +3,4 @@ cd = cd.substring(0, cd.length - 4);
 require("blanket")({pattern: cd});
 require("../irc/irc-test.js");
 require("../authorizer/tests/authorizer-test.js");
+require("../threader/threader-test.js");
