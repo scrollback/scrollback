65c67fce50fb2a6da2d8dd54fcd028a87b4d89da
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-08-26 15:39:36 +0530

    Deletedd files

--- tools/sql/database.sql
+++
@@ -1,3 +0,0 @@
-CREATE database scrollback;
-CREATE USER 'scrollback'@'localhost' IDENTIFIED BY 'scrollback';
-GRANT ALL ON scrollback.* TO 'scrollback'@'localhost';
--- tools/sql/tables.3up4.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `scrollback`.`accounts` ADD COLUMN `params` TEXT NULL AFTER `gateway`;
-UPDATE `scrollback`.`messages` SET `from`=CONCAT('guest-', `from`);
