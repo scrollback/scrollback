00f68af77a9709cfb8fde0e16db36910205c05b9
kamal <kkjoshi.it@gmail.com>
2014-08-26 19:10:01 +0530

    formating of test results email

--- test/send-test-results.js
+++ test/send-test-results.js
@@ -2,23 +2,43 @@ var send = require('../email/sendEmail.js');//TODO remove this dependency
 var htmlEncode = require('htmlencode');
 var fs = require('fs');
 var config = require('../config.js');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
 var to = config.test.to;
 
 function sendResults() {
     var r = fs.readFileSync('./xunit.xml', 'utf8');
-    r = formatResults(r);
-    to.forEach(function(t) {
-        send('askabt@scrollback.io', t, 'Test results', r);
+    formatResults(r, function(email) {
+        to.forEach(function(t) {
+            send('askabt@scrollback.io', t, email.subject, email.body);
+        });    
     });
 }
 
 sendResults();
 
-function formatResults(r) {
-    return '<html><body>' +
-        getCoverage() + "<p>" + 
-        htmlEncode.htmlEncode(r) + 
-        '</p></body></html>';
+function formatResults(r, callback) {
+    parser.parseString(r, function(err, data) {
+        console.log("data=", JSON.stringify(data)); 
+        var ret = {};
+        var subject = "Test Results Failures: " + data.testsuite.$.failures + " Total: " + data.testsuite.$.tests + " Errors: " + data.testsuite.$.errors;
+        ret.subject = subject;
+        var s = [];
+        data.testsuite.testcase.forEach(function(test) {
+            var style = "font-size: 14px;";
+            if (test.failure) { 
+                style += "color: #d9534f;";
+            } else {
+                style += "color: #5cb85c;";    
+            }
+            s.push("<pre style='" + style + "'>" + htmlEncode.htmlEncode(JSON.stringify(test, null, 4)).replace(/\\n/g, "<br/>") + "</pre>");
+        });
+        ret.body = '<html><body>' +
+            getCoverage() + "<p>" + 
+            s.join("\n") + 
+            '</p></body></html>';
+        callback(ret);
+    });
 }
 
 function getCoverage() {
