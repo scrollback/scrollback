e386d621c11b12b17f25db3ced6b268b17dbf3a0
kamal <kkjoshi.it@gmail.com>
2014-09-02 12:34:43 +0530

    capabilities will be defined in config.js

--- test/selenium/test.js
+++ test/selenium/test.js
@@ -1,47 +1,7 @@
 //http://www.browserstack.com/automate/node 
 //npm install -g browserstack-webdriver
 var config = require("../config.js");
-var capabilities = [
-	{
-		'browserName' : 'iPhone',
-		'platform' : 'MAC',
-		'device' : 'iPhone 5'
-	}, {
-		'browserName' : 'android',
-		'platform' : 'ANDROID',
-		'device' : 'LG Nexus 4'
-	},{
-		'browser' : 'Safari',
-		'browser_version' : '7.0',
-		'os' : 'OS X',
-		'os_version' : 'Mavericks',
-		'resolution' : '1024x768'
-	}, {
-		'browser' : 'IE',
-		'browser_version' : '10.0',
-		'os' : 'Windows',
-		'os_version' : '8',
-		'resolution' : '1280x1024'
-	}, {
-		'browser' : 'IE',
-		'browser_version' : '11.0',
-		'os' : 'Windows',
-		'os_version' : '8.1',
-		'resolution' : '1366x768'
-	}, {
-		'browser' : 'Chrome',
-		'browser_version' : '36.0',
-		'os' : 'Windows',
-		'os_version' : '8.1',
-		'resolution' : '1366x768'
-	}, {
-		'browser' : 'Firefox',
-		'browser_version' : '31.0',
-		'os' : 'Windows',
-		'os_version' : '8.1',
-		'resolution' : '1366x768'
-	}
-];
+var capabilities = config.selenium.capabilities;
 
 capabilities.forEach(function(c) {
 	c['browserstack.debug'] = config.selenium.debug;
