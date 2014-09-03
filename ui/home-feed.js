bf39cf11280bcd19e3032aaf530a55dbf01a550b
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-09-03 18:22:10 +0530

    Remove dummy data

--- ui/home-feed.js
+++ ui/home-feed.js
@@ -4,136 +4,68 @@
 $(function() {
 	var $cardContainer = $(".card-container"),
 		$gotoform = $("#home-go-to-room-form"),
-		$gotoentry = $("#home-go-to-room-entry");
-
-	function goToRoom(roomName) {
-		if (!roomName) {
-			return;
-		}
-
-		libsb.emit("navigate", {
-			roomName: roomName,
-			mode: "normal",
-			view: "normal",
-			source: "home-feed",
-			query: null,
-			thread: null,
-			time: null
-		});
-	}
-
-	function setCard(cardObj) {
-		var attrs = [],
-			events = [],
-			segments = [],
-			$card;
-
-		if (!cardObj) {
-			return;
-		}
-
-		cardObj.id = cardObj.id || "card-" + new Date().getTime();
-
-		$card = $("<div>").attr("id", cardObj.id);
-
-		for (var i in cardObj) {
-			if (typeof cardObj[i] === "string") {
-				attrs.push(i);
-			} else if ((/^on/).test(i) && typeof cardObj[i] === "function") {
-				events.push(i);
-			} else if (typeof cardObj[i] === "object") {
-				segments.push(i);
+		$gotoentry = $("#home-go-to-room-entry"),
+		$template = $(".card-item-wrap").eq(0),
+		roomCard = {},
+		goToRoom = function(roomName) {
+			if (!roomName) {
+				return;
 			}
+
+			libsb.emit("navigate", {
+				roomName: roomName,
+				mode: "normal",
+				view: "normal",
+				source: "room-list",
+				query: null,
+				thread: null,
+				time: null
+			});
+		};
+
+	roomCard.render = function($el, room, online, index) {
+		var $card;
+
+		if (!room) {
+			return;
 		}
 
-		attrs.forEach(function(attr) {
-			if (attr === "className") {
-				$card.removeClass().addClass(cardObj[attr]);
+		$el = $el || $template.clone(false);
+
+		$card = $el.find(".card-item");
+
+		$card.attr("id", "room-card-" + room.id);
+		$card.attr("data-room", room.id);
+		$card.attr("data-index", index);
+
+		$card.find(".card-header").text(room.id);
+		$card.find(".card-content-summary").text(room.description || "This room has no description.");
+		$card.find(".card-actions-online").text(online);
+
+		return $el;
+	};
+
+	$cardContainer.on("click", function(e) {
+		var $card = $(e.target).closest(".card-item");
+
+		if (!$card.length) {
+			return;
+		}
+
+		goToRoom($card.data("room-name"));
+	});
+
+	libsb.on("navigate", function(state, next) {
+		if (state.old && state.mode !== state.old.mode) {
+			if (state.mode === "home") {
+				// render rooms
 			} else {
-				$card.attr(attr, cardObj[attr]);
+//				$cardContainer.empty();
 			}
-		});
-
-		$card.addClass("card-item");
-
-		events.forEach(function(event) {
-			$card.on(event.replace(/^on/, ""), cardObj[event]);
-		});
-
-		segments.forEach(function(segment) {
-			var $segment;
-
-			if (cardObj[segment]) {
-				$segment = $card.find(".card-" + segment);
-
-				if (!($segment.length)) {
-					$segment = $("<div>").addClass("card-segment card-" + segment);
-					$segment.appendTo($card);
-				}
-
-				for (var item in cardObj[segment]) {
-					var className = "card-" + segment + "-" + item,
-						$oldel = $segment.find("." + className),
-						$newel = $("<div>").addClass(className).html(cardObj[segment][item]);
-
-					if ($oldel.length) {
-						$oldel.replaceWith($newel);
-					} else {
-						$newel.appendTo($segment);
-					}
-				}
-			}
-		});
-
-		return $card;
-	}
-
-	function updateCard(cardObj) {
-		var $oldCard, $newCard;
-
-		if (!(cardObj && $cardContainer.length)) {
-			return;
 		}
 
-		$newCard = setCard(cardObj);
-
-		if (cardObj.id) {
-			$oldCard = $("#" + cardObj.id);
-		}
-
-		if ($oldCard && $oldCard.length && $oldCard.hasClass("card-item")) {
-			$oldCard.replaceWith($newCard);
-		} else {
-			$cardContainer.append($("<div class='card-item-wrap'></div>").append($newCard));
-		}
-
-		return $newCard;
-	}
-
-	function randomStr(n) {
-		var str = (Math.random() + 1).toString(36).substring(2, (n + 2));
-
-		return str.charAt(0).toUpperCase() + str.slice(1);
-	}
-
-	for (var i = 0; i < 20; i++) {
-		updateCard({
-			id: randomStr(5),
-			onclick: function() { console.log("hi"); },
-			header: {
-				title: randomStr(7),
-				badge: Math.round(Math.random() * 100)
-			},
-			content: {
-				title: randomStr(15),
-				summary: randomStr(80)
-			},
-			actions: {
-				view: "View conversation",
-				online: "<span class='card-actions-online-number'>124</span> people talking"
-			}
-		});
-	}
+		next();
+	}, 500);
 
 	$gotoform.on("submit", function(e) {
 		var roomName = $gotoentry.val();
