35503ef00c74273da93ab4b826b08434844820cd
Amal Antony <amalantony06@gmail.com>
2014-09-08 19:48:40 +0530

    Facebook msg posting via Localstorage

--- ui/facebook.js
+++ ui/facebook.js
@@ -1,17 +1,38 @@
-$(window).on("message", function(event) {
-	var data = event.originalEvent.data;
-	var action;
-	if(event.originalEvent.origin !== "https://"+location.host) return;
+window.addEventListener("storage", function (event) {
+	if (event.key === "authValue") {
+		var data = event.newValue;
+		var action;
+		if(event.originalEvent.origin !== "https://"+location.host) return;
 
-	if(typeof data === 'string') {
-		try { action = JSON.parse(data); }
-		catch(e) {
-			console.log("Error parsing incoming action: ", data, e);
-			return;
-		}
-	} else { action = data; }
+		if(typeof data === 'string') {
+			try { action = JSON.parse(data); }
+			catch(e) {
+				console.log("Error parsing incoming action: ", data, e);
+				return;
+			}
+		} else { action = data; }
 
-	if(!data.command || data.command != "signin") return;
-	delete data.command;
-	libsb.emit("init-up", action, function(err, data) {});	
+		if(!data.command || data.command != "signin") return;
+		delete data.command;
+		libsb.emit("init-up", action, function(err, data) {});	
+	}
 });
+
+//
+//$(window).on("message", function(event) {
+//	var data = event.originalEvent.data;
+//	var action;
+//	if(event.originalEvent.origin !== "https://"+location.host) return;
+//
+//	if(typeof data === 'string') {
+//		try { action = JSON.parse(data); }
+//		catch(e) {
+//			console.log("Error parsing incoming action: ", data, e);
+//			return;
+//		}
+//	} else { action = data; }
+//
+//	if(!data.command || data.command != "signin") return;
+//	delete data.command;
+//	libsb.emit("init-up", action, function(err, data) {});	
+//});
