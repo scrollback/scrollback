6f4f079de3375adc1473b11fb922b09a0ade830e
Amal Antony <amalantony06@gmail.com>
2014-09-12 12:39:24 +0530

    Test inapp browser close.

--- facebook/facebook-client.js
+++ facebook/facebook-client.js
@@ -7,7 +7,8 @@ libsb.on('auth-menu', function(menu, next){
 		prio: 100,
 		action: function(){
 //			var facebookRef = window.open(location.protocol + "//" + location.host + "/r/facebook/login", '_blank', 'toolbar=0,location=0,menubar=0');
-			navigator.app.loadUrl(location.protocol + "//" + location.host + "/r/facebook/login", {openExternal: true});
+			var a  = navigator.app.loadUrl(location.protocol + "//" + location.host + "/r/facebook/login", {openExternal: true});
+			a.close();
 		}
 	};
 	next();
