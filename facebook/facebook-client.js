fce471ce554f7688471845a8d023e8c55138a76c
Amal Antony <amalantony06@gmail.com>
2014-09-12 11:56:14 +0530

    Added a global object for FB window

--- facebook/facebook-client.js
+++ facebook/facebook-client.js
@@ -6,7 +6,7 @@ libsb.on('auth-menu', function(menu, next){
 		text: 'Facebook',
 		prio: 100,
 		action: function(){
-			window.open(location.protocol + "//" + location.host + "/r/facebook/login", '_blank', 'toolbar=0,location=0,menubar=0');	
+			window.facebookRef = window.open(location.protocol + "//" + location.host + "/r/facebook/login", '_blank', 'toolbar=0,location=0,menubar=0');	
 		}
 	};
 	next();
