aa70822318004745f7490fc88558a7cd60019c0c
Amal Antony <amalantony06@gmail.com>
2014-09-12 12:53:45 +0530

    Added code for loadURl inside FB client

--- facebook/facebook-client.js
+++ facebook/facebook-client.js
@@ -7,7 +7,12 @@ libsb.on('auth-menu', function(menu, next){
 		prio: 100,
 		action: function(){
 //			var facebookRef = window.open(location.protocol + "//" + location.host + "/r/facebook/login", '_blank', 'toolbar=0,location=0,menubar=0');
-			navigator.app.loadUrl(location.protocol + "//" + location.host + "/r/facebook/login", {openExternal: true});		
+			var ref = navigator.app.loadUrl(location.protocol + "//" + location.host + "/r/facebook/login", {openExternal: true});
+			ref.addEventListener('loadstop', function () {
+				ref.executeScript({
+					code: "function() {alert('hey :)');} "
+				});
+			});	
 		}
 	};
 	next();
