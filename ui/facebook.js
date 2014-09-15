16ce872f5d0bc9285e89e069b524fa471c90a99b
Amal Antony <amalantony06@gmail.com>
2014-09-15 15:57:12 +0530

    Reverted some phonegap related code.

--- ui/facebook.js
+++ ui/facebook.js
@@ -1,24 +1,5 @@
-//window.addEventListener("storage", function (event) {
-//	if (event.key === "authValue") {
-//		var data = JSON.parse(event.newValue);
-//		delete localStorage.authValue;
-//		var action;
-//		// if(event.originalEvent.origin !== "https://"+location.host) return;
-//
-//		if(typeof data === 'string') {
-//			try { action = JSON.parse(data); }
-//			catch(e) {
-//				console.log("Error parsing incoming action: ", data, e);
-//				return;
-//			}
-//		} else { action = data; }
-//
-//		if(!data.command || data.command != "signin") return;
-//		delete data.command;
-//		libsb.emit("init-up", action, function(err, data) {});	
-//	}
-//});
-
+/* jshint browser:true */
+/* global $, libsb */
 $(window).on("message", function(event) {
 	var data = event.originalEvent.data;
 	var action;
