8f5a98b94677a8fe27f6d658f62bdff4d5262243
scrollback <scrollback@dev.scrollback.io>
2014-09-16 16:54:36 +0000

    Merge conflicts resolved

--- phonegap-fb/phonegap-fb.js
+++ phonegap-fb/phonegap-fb.js
@@ -6,7 +6,7 @@ currentState.phonegap = false;
 if (currentState.phonegap) {
 	console.log("Phonegap FB called, condition is happy :)");
 	setInterval(function () {
-		fbRef.executeScript("var p = window.postedMessages; window.postedMessages = []; return p;", function (ret) {
+		window.fbRef.executeScript("var p = window.postedMessages; window.postedMessages = []; return p;", function (ret) {
 			if (ret && ret.length > 0) {
 				$(window).trigger('messsage', ret[0]);
 				fbRef.close();
