a2ca6f57652ad188151cea95825f1451a2228fcd
Amal Antony <amalantony06@gmail.com>
2014-09-17 12:48:59 +0530

    Made host configurable for FB login

--- facebook/facebook-client.js
+++ facebook/facebook-client.js
@@ -1,5 +1,6 @@
 /*jshint browser:true*/
 /*global libsb, $*/
+var config = require("../client-config.js");
 
 function getParameterByName(name, url) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
@@ -14,7 +15,7 @@ libsb.on('auth-menu', function(menu, next){
 		prio: 100,
 		action: function(){
 		//window.open(location.protocol + "//" + location.host + "/r/facebook/login", '_blank', 'toolbar=0,location=0,menubar=0');	
-            window.fbRef = window.open("https://dev.scrollback.io/r/facebook/login", "_blank", "toolbar=0,location=0,menubar=0");
+            window.fbRef = window.open("https:" + config.server.host + "/r/facebook/login", "_blank", "toolbar=0,location=0,menubar=0");
 			window.fbRef.addEventListener('loadstop', function (event) {
 				var url = event.url;
 				var code = getParameterByName('code', url);
