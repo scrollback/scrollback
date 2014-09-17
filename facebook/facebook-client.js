c9a26f343fc415baf3c5c50ba534d93f6475e988
Amal Antony <amalantony06@gmail.com>
2014-09-17 12:19:21 +0530

    Testing a fix for loadstop triggering.

--- facebook/facebook-client.js
+++ facebook/facebook-client.js
@@ -14,7 +14,7 @@ libsb.on('auth-menu', function(menu, next){
 		prio: 100,
 		action: function(){
 		//window.open(location.protocol + "//" + location.host + "/r/facebook/login", '_blank', 'toolbar=0,location=0,menubar=0');	
-            window.fbRef = window.open("https://dev.scrollback.io/r/facebook/login", "_blank", "toolbar=0,location=0,menubar=0");
+			window.fbRef = window.open("about:blank", "_blank", "toolbar=0,location=0,menubar=0");
 			window.fbRef.addEventListener('loadstop', function (event) {
 				var url = event.url;
 				var code = getParameterByName('code', url);
@@ -33,6 +33,7 @@ libsb.on('auth-menu', function(menu, next){
 					window.fbRef.close();
 				}
 			});
+			window.fbRef = window.open("https://dev.scrollback.io/r/facebook/login");
 		}
 	};
 	next();
