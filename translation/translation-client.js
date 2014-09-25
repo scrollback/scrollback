5b63dc357805fbe5cb4c9c8f7c9ebf451ae17fca
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-09-25 17:20:28 +0530

    Refactor code

--- translation/translation-client.js
+++ translation/translation-client.js
@@ -1,56 +1,50 @@
 /* jslint browser: true */
 /* global $ */
 
-var Translator = (function() {
-	var _translateString = function(str, cdata) {
-		var data = require("./en_us.js");
+var Translator = function(cdata) {
+	var data = require("./strings.js");
 
-		if (cdata) {
-			data = $.extend(data, cdata);
+	this.data = cdata ? $.extend(data, cdata) : data;
+};
+
+Translator.prototype.translateString = function(str) {
+	return this.data[str] || str;
+};
+
+Translator.prototype.translate = function(els) {
+	var self = this,
+		$els = $(els);
+
+	$els.each(function() {
+		var $this = $(this),
+			regex = /(^[aA-zZ\-]+)(::)(.+$)/,
+			key = $this.attr("data-string"),
+			segments, attr, value;
+
+		if (regex.test(key)) {
+			segments = regex.exec(key);
+			attr = segments[1];
+			value = self.translateString(segments[3]);
+
+			$this.attr(attr, value);
+		} else {
+			value = self.translateString(key);
+
+			$this.text(value);
 		}
+	});
 
-		return data[str] || str;
-	},
-	_translate = function(els, cdata) {
-		var $els = $(els);
+	return $els;
+};
 
-		$els.each(function() {
-			var $this = $(this),
-				regex = /(^[aA-zZ\-]+)(::)(.+$)/,
-				key = $this.attr("data-string"),
-				segments, attr, value;
+Translator.prototype.translateAll = function() {
+	var self = this;
 
-			if (regex.test(key)) {
-				segments = regex.exec(key);
-				attr = segments[1];
-				value = _translateString(segments[3], cdata);
+	$(function() {
+		var $els = $("[data-string]");
 
-				$this.attr(attr, value);
-			} else {
-				value = _translateString(key, cdata);
-
-				$this.text(value);
-			}
-		});
-
-		return $els;
-	};
-
-	return function() {
-		var _this = this;
-
-		_this.translate = _translate;
-
-		_this.translateString = _translateString;
-
-		_this.translateAll = function(cdata) {
-			$(function() {
-				var $els = $("[data-string]");
-
-				return _translate($els, cdata);
-			});
-		};
-	};
-}());
+		self.translate($els);
+	});
+};
 
 module.exports = new Translator();
