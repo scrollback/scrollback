495a088bd1374075cae42153170739b98c848513
kamal <kkjoshi.it@gmail.com>
2014-09-26 18:42:29 +0530

    beautify

--- ui/noroom-area.js
+++ ui/noroom-area.js
@@ -1,28 +1,31 @@
 /* jshint browser: true */
 /* global $, libsb, currentState */
 
-libsb.on('navigate', function(state, next){
-    if(state.mode === 'profile' && state.source === 'noroom'){
-        $('#profile-view-name').text(currentState.roomName);
-    }
-    next();
+libsb.on('navigate', function (state, next) {
+	if (state.mode === 'profile' && state.source === 'noroom') {
+		$('#profile-view-name').text(currentState.roomName);
+	}
+	next();
 }, 100);
 
-$("#noroom-view-create").on("click", function() {
-    var roomObj = {
-        to: currentState.roomName,
-        room: {
-            id: currentState.roomName,
-            description: '',
-            params: {
+$("#noroom-view-create").on("click", function () {
+	var roomObj = {
+		to: currentState.roomName,
+		room: {
+			id: currentState.roomName,
+			description: '',
+			params: {
 
-            },
-            guides: {}
-        }
-    };
-    libsb.emit('room-up', roomObj,function(){
-         libsb.emit("navigate", {mode: 'normal', tab: 'info'}, function(){
-             location.reload();
-         });
-    });
+			},
+			guides: {}
+		}
+	};
+	libsb.emit('room-up', roomObj, function () {
+		libsb.emit("navigate", {
+			mode: 'normal',
+			tab: 'info'
+		}, function () {
+			location.reload();
+		});
+	});
 });
