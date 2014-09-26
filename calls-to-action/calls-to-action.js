12c4403864b2b2af1ee3a782ce6ea07af96593d5
Amal Antony <amalantony06@gmail.com>
2014-09-26 12:50:14 +0530

    Added the calls to actions app.

---
+++ calls-to-action/calls-to-action.js
@@ -0,0 +1,19 @@
+/* jshint browser:true */
+/* global libsb */
+
+var timeOnline;
+
+// get user info
+
+var shownActions = {};
+
+var userActions = {
+	signUp : "Sign up to pick a nickname.",
+	choosePic: "Choose a picture and set your preferences in Account Settings",
+	enableNotifications: "Turn on Desktop notifications to get notified when people address you.",
+	followRoom: "Follow this room to stay in the loop even when you are offline",
+	viewDiscussions: "Did you know that you can browse archives by discussion?",
+	searchArchives: "Did you know that you can search the chat archives?"
+};
+
+
