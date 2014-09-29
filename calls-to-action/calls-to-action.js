bd2ef338044443c2cad4fd5bd937dc5c8c683498
Amal Antony <amalantony06@gmail.com>
2014-09-29 11:10:14 +0530

    Desktop notification CTA.

--- calls-to-action/calls-to-action.js
+++ calls-to-action/calls-to-action.js
@@ -5,6 +5,10 @@
 
 var shownActions = {};
 
+if (localStorage.hasOwnProperty('shownActions')) {
+	shownActions = JSON.parse(localStorage.shownActions);
+}
+
 var userActions = {
 	signIn : "Sign into Scrollback to pick a nickname.",
 	choosePic: "Choose a picture and set your preferences in Account Settings",
@@ -18,6 +22,7 @@ function showNotification(origin, notifcationName) {
 	if(shownActions[notifcationName] !== true) {
 		lace.popover.show({origin: origin, body: userActions[notifcationName]});
 		shownActions[notifcationName] = true;
+		localStorage.shownActions = JSON.stringify(shownActions);
 	}
 }
 
@@ -30,4 +35,28 @@ libsb.on('text-up', function (text, next) {
 $('.chat-entry').click(function () {
 	if(!/^guest-/.test(libsb.user.id)) return;
 	showNotification($('.user-area'), 'signIn');
+});
+
+libsb.on('user-dn', function (user, next) {
+	if (!/^guest-/.test(user.from)) return next(); // not a new signup
+	showNotification($('.user-area'), 'choosePic');
+	next();
+}, 800);
+
+function showPrefNotification (user) {
+	if (user.params.notifications.desktop === false) {
+		showNotification($('.user-area'), 'enableNotifications');
+	}
+}
+
+libsb.on('pref-save', function (user, next) {
+	console.log("GOT pref save, user is", user);
+	showPrefNotification(user);
+	next();
+}, 100);
+
+$(".conf-cancel").on("click", function() {
+	if (window.currentState.mode === "pref") {
+		showPrefNotification(libsb.user);
+	}
 });
