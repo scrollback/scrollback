9244b1bd9cd72d8aeee1f0bd2c8c14af666497d2
kamal <kkjoshi.it@gmail.com>
2014-09-30 15:41:13 +0530

    login based on common visible element

--- test/selenium/testUtils.js
+++ test/selenium/testUtils.js
@@ -1,6 +1,7 @@
 var webdriver = require('browserstack-webdriver');
 module.exports.openUrl = openUrl;
-module.exports.loginPersona = loginPersona;	
+module.exports.loginPersona = loginPersona;
+
 function openUrl(capabilities, server, roomid) {
 	var driver = new webdriver.Builder().
 	usingServer('http://hub.browserstack.com/wd/hub').
@@ -11,32 +12,59 @@ function openUrl(capabilities, server, roomid) {
 }
 
 function loginPersona(driver, id, password, callback) {
-	driver.findElement(webdriver.By.css('.user-area')).click().
-	then(function() {
-		driver.findElement(webdriver.By.css('.persona')).click().then(function() {
-			driver.getAllWindowHandles().then(function(win) {
-				console.log("win", win);
-				driver.switchTo().window(win[1]).then(function(){
-					setTimeout(function() {
-						driver.findElement(webdriver.By.css(".isDesktopOrStart")).
-						sendKeys(id).
-						then(function() {
+
+	findVisibleElementByClass(driver, ".js-has-auth-menu", function (el) {
+		el.click().
+		then(function () {
+			driver.findElement(webdriver.By.css('.persona')).click().then(function () {
+				driver.getAllWindowHandles().then(function (win) {
+					driver.switchTo().window(win[1]).then(function () {
+						setTimeout(function () {
 							driver.findElement(webdriver.By.css(".isDesktopOrStart")).
-							sendKeys(webdriver.Key.RETURN).then(function() {
-								setTimeout(function() {
-									driver.findElement(webdriver.By.id("authentication_password")).
-									sendKeys(password).then(function() { 
+							sendKeys(id).
+							then(function () {
+								driver.findElement(webdriver.By.css(".isDesktopOrStart")).
+								sendKeys(webdriver.Key.RETURN).then(function () {
+									setTimeout(function () {
 										driver.findElement(webdriver.By.id("authentication_password")).
-										sendKeys(webdriver.Key.RETURN).then(function() {
-											driver.switchTo().window(win[0]).then(callback);
+										sendKeys(password).then(function () {
+											driver.findElement(webdriver.By.id("authentication_password")).
+											sendKeys(webdriver.Key.RETURN).then(function () {
+												driver.switchTo().window(win[0]).then(callback);
+											});
 										});
-									});
-								}, 7000);
+									}, 7000);
+								});
 							});
-						});
-					}, 4000);
+						}, 4000);
+					});
 				});
-			});	
+			});
 		});
+	});
+}
+
+/**
+ * Find one visible element by css
+ */
+function findVisibleElementByClass(driver, name, cb) {
+	driver.findElements(webdriver.By.css(name)).
+	then(function (el) {
+		var element;
+		var c = 0;
+		function done() {
+			if (++c === el.length) {
+				cb(element);
+			}
+		}
+		el.forEach(function (e) {
+			e.isDisplayed().then(function (a) {
+				if (a) {
+					element = e;
+				}
+				done();
+			});
+		});
+
 	});
 }
