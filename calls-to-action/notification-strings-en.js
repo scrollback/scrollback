0ec72bc0839df6f3f9213cd355388838be97fbf1
Amal Antony <amalantony06@gmail.com>
2014-09-30 11:11:57 +0530

    Moved notification strings to a new file.

---
+++ calls-to-action/notification-strings-en.js
@@ -0,0 +1,8 @@
+module.exports = {
+	signIn: "Sign into Scrollback to pick a nickname.",
+	choosePic: "Choose a picture and set your preferences in Account Settings.",
+	enableNotifications: "Turn on Desktop notifications in your account settings to get notified when people address you.",
+	followRoom: "Follow this room to stay in the loop even when you are offline.",
+	browseArchives: "Did you know that you can browse the archives by discussion?",
+	searchArchives: "Did you know that you can search the chat archives?"
+};
--- calls-to-action/showNotification.js
+++ calls-to-action/showNotification.js
@@ -1,6 +1,8 @@
 /* jshint browser:true */
 /* global $, lace */
 
+var userActions = require('./notification-strings-en.js');
+
 var shownActions = {};
 var notificationQueue = [];
 var intervalSet = false;
@@ -8,15 +10,6 @@ var intervalSet = false;
 if (localStorage.hasOwnProperty('shownActions')) {
 	// shownActions = JSON.parse(localStorage.shownActions);
 }
-
-var userActions = {
-	signIn: "Sign into Scrollback to pick a nickname.",
-	choosePic: "Choose a picture and set your preferences in Account Settings.",
-	enableNotifications: "Turn on Desktop notifications in your account settings to get notified when people address you.",
-	followRoom: "Follow this room to stay in the loop even when you are offline.",
-	browseArchives: "Did you know that you can browse the archives by discussion?",
-	searchArchives: "Did you know that you can search the chat archives?"
-};
 
 function notify(action) {
 	if (shownActions[action.notificationName] !== true) {
