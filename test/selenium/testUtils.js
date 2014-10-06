bd35497a59fff8e25de302cc5b3a2c6bb5730a66
kamal <kkjoshi.it@gmail.com>
2014-10-06 12:42:21 +0530

    loginPersona function is using promises pattern

--- test/selenium/testUtils.js
+++ test/selenium/testUtils.js
@@ -1,7 +1,7 @@
 var webdriver = require('browserstack-webdriver');
 module.exports.openUrl = openUrl;
 module.exports.loginPersona = loginPersona;
-
+var q = require('q');
 function openUrl(capabilities, server, roomid) {
 	var driver = new webdriver.Builder().
 	usingServer('http://hub.browserstack.com/wd/hub').
@@ -14,32 +14,32 @@ function openUrl(capabilities, server, roomid) {
 function loginPersona(driver, id, password, callback) {
 
 	findVisibleElementByClass(driver, ".js-has-auth-menu", function (el) {
+		var win;
 		el.click().
 		then(function () {
-			driver.findElement(webdriver.By.css('.persona')).click().then(function () {
-				driver.getAllWindowHandles().then(function (win) {
-					driver.switchTo().window(win[1]).then(function () {
-						setTimeout(function () {
-							driver.findElement(webdriver.By.css(".isDesktopOrStart")).
-							sendKeys(id).
-							then(function () {
-								driver.findElement(webdriver.By.css(".isDesktopOrStart")).
-								sendKeys(webdriver.Key.RETURN).then(function () {
-									setTimeout(function () {
-										driver.findElement(webdriver.By.id("authentication_password")).
-										sendKeys(password).then(function () {
-											driver.findElement(webdriver.By.id("authentication_password")).
-											sendKeys(webdriver.Key.RETURN).then(function () {
-												driver.switchTo().window(win[0]).then(callback);
-											});
-										});
-									}, 7000);
-								});
-							});
-						}, 4000);
-					});
-				});
-			});
+			return driver.findElement(webdriver.By.css('.persona')).click();
+		}).then(function () {
+			return driver.getAllWindowHandles();
+		}).then(function (w) {
+			win = w;
+			return driver.switchTo().window(win[1]);
+		}).then(function () {
+			return q.delay(4000);
+		}).then(function() {
+			return driver.findElement(webdriver.By.css(".isDesktopOrStart")).sendKeys(id);
+		}).then(function () {
+			return driver.findElement(webdriver.By.css(".isDesktopOrStart"))
+			.sendKeys(webdriver.Key.RETURN);
+		}).then(function () {
+			return q.delay(7000);
+		}).then(function() {
+			return driver.findElement(webdriver.By.id("authentication_password")).
+			sendKeys(password);
+		}).then(function () {
+			return driver.findElement(webdriver.By.id("authentication_password")).
+			sendKeys(webdriver.Key.RETURN);
+		}).then(function () {
+			driver.switchTo().window(win[0]).then(callback);
 		});
 	});
 }
