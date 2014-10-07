07de29d0d91d30f86d658464e8b2b7814e289364
Amal Antony <amalantony06@gmail.com>
2014-10-07 12:29:42 +0530

    Intermediatory commit

--- push-notification/push-notification-client.js
+++ push-notification/push-notification-client.js
@@ -3,8 +3,20 @@
 
 libsb.on('pref-show', function(tabs, next) {
 	var $div = $('<div>');
-	
 	var user = tabs.user;
+	
+	var devices = user.params.pushNotifications && user.params.pushNotifications.devices ? user.params.pushNotifications.devices : [];
+	/*
+		Structure of user.params.pushNotifications
+		
+		user.params.pushNotifications = {
+			devices : [ 
+				{platform: "Android", deviceId: "adsfaf32r23sdf21e123", enabled: true}, 
+				{platform: "iOS", deviceId: "234jkidksf9325pi23d2sdf", enabled: false}
+			]
+		}
+	
+	*/
 	
 	tabs.pushnotification = {
 		text: "Your Devices",
