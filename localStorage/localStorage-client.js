15070d6ada3a0cb2c24e91237d4cb3e0affe7b10
Amal Antony <amalantony06@gmail.com>
2014-10-09 16:24:47 +0530

    occupantCaching is functional.

--- localStorage/localStorage-client.js
+++ localStorage/localStorage-client.js
@@ -3,9 +3,9 @@ var objCacheOps = require('./objCacheOps.js');
 
 require('./textsCaching.js')(arrayCacheOps);
 require('./threadsCaching.js')(arrayCacheOps);
+require('./occupantCaching.js')(arrayCacheOps);
 
 require('./roomsCaching.js')(objCacheOps);
 require('./usersCaching.js')(objCacheOps);
-require('./occupantCaching.js')(objCacheOps);
 
 require('./sessionCaching.js')(objCacheOps);
