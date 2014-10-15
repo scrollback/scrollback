17177a3ed73aa1c831a6dcaa0a523e571dbb9e04
kamal <kkjoshi.it@gmail.com>
2014-10-15 17:46:31 +0530

    added featured room option in getRooms query

---
+++ recommendation/recommendation.js
@@ -0,0 +1,18 @@
+var config = require('../config.js');
+var log = require("../lib/logger.js");
+var redis = require('../lib/redisProxy.js').select(config.redisDB.recommandation);
+var core;
+module.exports = function(coreObject) {
+	core = coreObject;
+	core.on("getRooms", function(q, callback) {
+		if (q.featured) {
+			log.i("Query for featured room");
+			log.d("Query:", q);
+			redis.smembers("featured:room", function(err, roomids) {
+				log.d("Rooms retured from redis: ", roomids);
+				q.ref = roomids;
+				callback();
+			});
+		} else callback();
+	}, "gateway");
+};
