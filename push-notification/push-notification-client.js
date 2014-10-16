eafc4cc0398661da2d6cabea2115368507430c08
Amal Antony <amalantony06@gmail.com>
2014-10-16 18:12:56 +0530

    Mapping push registration ids to user objects.

--- push-notification/push-notification-client.js
+++ push-notification/push-notification-client.js
@@ -1,12 +1,48 @@
 /* jshint browser:true */
-/* global libsb, $ */
+/* global libsb, device */
 
-libsb.on('pref-show', function(tabs, next) {
-	var $div = $('<div>');
-	var user = tabs.user;
-	
+/*
+	devices : [{deviceName: device.name, registrationId: true}]
+*/
+
+function mapDevicetoUser(regId) {
+	/* Checks if device is registered to User for push notification, if not adds it */
+	var user = libsb.user;
+	var deviceRegistered = false;
+	if (typeof user.params.pushNotifications === "undefined") {
+		user.params.pushNotifications = {devices: []};
+	}
 	var devices = user.params.pushNotifications && 
-        user.params.pushNotifications.devices ? user.params.pushNotifications.devices : [];
+		user.params.pushNotifications.devices ? user.params.pushNotifications.devcies: [];
+	devices.forEach(function(device) {
+		if (device && device.hasOwnProperty('registrationId')) {
+			if (device.registrationId === regId) {
+				deviceRegistered = true;
+			}	
+		}
+	});
+	var newDevice = {deviceName: device.name};
+	newDevice[regId] = true;
+	if (deviceRegistered === false) {
+		devices.push(newDevice);
+		user.params.pushNotifications.devices = devices;
+		libsb.emit('user-up', user);
+	}
+}
+
+if (window.phonegap) {
+	// add the device registration id to the users params.
+	var regId = localStorage.phonegapRegId;
+	console.log("Inside push-notification-client, reg id is ", regId);
+	mapDevicetoUser(regId);
+}
+
+//libsb.on('pref-show', function(tabs, next) {
+//	var $div = $('<div>');
+//	var user = tabs.user;
+//	
+//	var devices = user.params.pushNotifications && 
+//        user.params.pushNotifications.devices ? user.params.pushNotifications.devices : [];
 	/*
 		Structure of user.params.pushNotifications
 		
@@ -19,11 +55,11 @@ libsb.on('pref-show', function(tabs, next) {
 	
 	*/
 	
-	tabs.pushnotification = {
-		text: "Your Devices",
-		html: $div,
-		prio: 1000
-	};
-	
-	next();
-});
+//	tabs.pushnotification = {
+//		text: "Your Devices",
+//		html: $div,
+//		prio: 1000
+//	};
+//	
+//	next();
+//});
