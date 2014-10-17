7e0f13da50dd4c22dce4754722e13b3200ff3cc3
Amal Antony <amalantony06@gmail.com>
2014-10-17 18:53:48 +0530

    Moved code to handle push notifcations to inside pushnotification-client.js

--- push-notification/push-notification-client.js
+++ push-notification/push-notification-client.js
@@ -5,6 +5,40 @@
 	devices : [{deviceName: device.name, registrationId: registrationId, enabled: true}]
 */
 
+window.onNotificationGCM = function (e) {
+	
+	// handler for push notifications.
+	
+	console.log("Got notification", e.event);
+
+	switch (e.event) {
+	case 'registered':
+		if (e.regid.length > 0) {
+			// Storing regId to be used by GCM to make push notifications.
+			console.log("regID = " + e.regid);
+			localStorage.phonegapRegId = e.regid;
+			console.log("Stored regid to localStorage ", localStorage.phonegapRegId);
+		}
+		break;
+
+	case 'message':
+		console.log(e);
+		// e.foreground is true if the notification came in when the user is in the foreground.
+		if (e.foreground) {
+			console.log(e.payload.message);
+		}
+		break;
+
+	case 'error':
+		console.log(e.msg);
+		break;
+
+	default:
+		console.log(e);
+		break;
+	}
+};
+
 function mapDevicetoUser(regId) {
 	/* Checks if device is registered to User for push notification, if not adds it */
 	var user = libsb.user;
@@ -26,7 +60,6 @@ function mapDevicetoUser(regId) {
 	if (deviceRegistered === false) {
 		devices.push(newDevice);
 		user.params.pushNotifications.devices = devices;
-		console.log("----- Devices, user is ", devices, user);
 		libsb.emit('user-up', user);
 	}
 }
@@ -37,6 +70,8 @@ if (window.phonegap) {
 	console.log("Inside push-notification-client, reg id is ", regId);
 	mapDevicetoUser(regId);
 }
+
+
 
 //libsb.on('pref-show', function(tabs, next) {
 //	var $div = $('<div>');
