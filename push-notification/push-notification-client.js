fea219c47f03029dc366869f9aad68e80bced6ef
Amal Antony <amalantony06@gmail.com>
2014-10-20 13:05:35 +0530

    Moved onNotificationGCM to inside registerPushNotification.

--- push-notification/push-notification-client.js
+++ push-notification/push-notification-client.js
@@ -9,6 +9,40 @@ document.addEventListener('deviceready', registerPushNotification, false);
 var pushNotification;
 
 function registerPushNotification() {
+	function onNotificationGCM(e) {
+
+		console.log("CALLED onNotificationGCM **********");
+		// handler for push notifications.
+
+		console.log("Got notification", e.event);
+
+		switch (e.event) {
+		case 'registered':
+			if (e.regid.length > 0) {
+				// Storing regId to be used by GCM to make push notifications.
+				console.log("regID = " + e.regid);
+				localStorage.phonegapRegId = e.regid;
+				console.log("Stored regid to localStorage ", localStorage.phonegapRegId);
+			}
+			break;
+
+		case 'message':
+			console.log(e);
+			// e.foreground is true if the notification came in when the user is in the foreground.
+			if (e.foreground) {
+				console.log(e.payload.message);
+			}
+			break;
+
+		case 'error':
+			console.log(e.msg);
+			break;
+
+		default:
+			console.log(e);
+			break;
+		}
+	}
 	console.log('inside push notification client , got device ready');
 	pushNotification = window.plugins && window.plugins.pushNotification;
 	if (!pushNotification) {
@@ -33,41 +67,6 @@ function successHandler(result) {
 // result contains any error description text returned from the plugin call
 function errorHandler(error) {
 	console.log('registration error = ' + error);
-}
-
-function onNotificationGCM(e) {
-
-	console.log("CALLED onNotificationGCM **********");
-	// handler for push notifications.
-
-	console.log("Got notification", e.event);
-
-	switch (e.event) {
-	case 'registered':
-		if (e.regid.length > 0) {
-			// Storing regId to be used by GCM to make push notifications.
-			console.log("regID = " + e.regid);
-			localStorage.phonegapRegId = e.regid;
-			console.log("Stored regid to localStorage ", localStorage.phonegapRegId);
-		}
-		break;
-
-	case 'message':
-		console.log(e);
-		// e.foreground is true if the notification came in when the user is in the foreground.
-		if (e.foreground) {
-			console.log(e.payload.message);
-		}
-		break;
-
-	case 'error':
-		console.log(e.msg);
-		break;
-
-	default:
-		console.log(e);
-		break;
-	}
 }
 
 function mapDevicetoUser(regId) {
