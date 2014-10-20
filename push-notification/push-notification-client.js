7bbe01370434f325b82c519cb7afa9366bda8701
Amal Antony <amalantony06@gmail.com>
2014-10-20 13:15:10 +0530

    Made pushnotification handler a global function.

--- push-notification/push-notification-client.js
+++ push-notification/push-notification-client.js
@@ -8,41 +8,42 @@ document.addEventListener('deviceready', registerPushNotification, false);
 
 var pushNotification;
 
-function registerPushNotification() {
-	function onNotificationGCM(e) {
+window.onNotificationGCM = function (e) {
 
-		console.log("CALLED onNotificationGCM **********");
-		// handler for push notifications.
+	console.log("CALLED onNotificationGCM **********");
+	// handler for push notifications.
 
-		console.log("Got notification", e.event);
+	console.log("Got notification", e.event);
 
-		switch (e.event) {
-		case 'registered':
-			if (e.regid.length > 0) {
-				// Storing regId to be used by GCM to make push notifications.
-				console.log("regID = " + e.regid);
-				localStorage.phonegapRegId = e.regid;
-				console.log("Stored regid to localStorage ", localStorage.phonegapRegId);
-			}
-			break;
-
-		case 'message':
-			console.log(e);
-			// e.foreground is true if the notification came in when the user is in the foreground.
-			if (e.foreground) {
-				console.log(e.payload.message);
-			}
-			break;
-
-		case 'error':
-			console.log(e.msg);
-			break;
-
-		default:
-			console.log(e);
-			break;
+	switch (e.event) {
+	case 'registered':
+		if (e.regid.length > 0) {
+			// Storing regId to be used by GCM to make push notifications.
+			console.log("regID = " + e.regid);
+			localStorage.phonegapRegId = e.regid;
+			console.log("Stored regid to localStorage ", localStorage.phonegapRegId);
 		}
+		break;
+
+	case 'message':
+		console.log(e);
+		// e.foreground is true if the notification came in when the user is in the foreground.
+		if (e.foreground) {
+			console.log(e.payload.message);
+		}
+		break;
+
+	case 'error':
+		console.log(e.msg);
+		break;
+
+	default:
+		console.log(e);
+		break;
 	}
+}
+
+function registerPushNotification() {
 	console.log('inside push notification client , got device ready');
 	pushNotification = window.plugins && window.plugins.pushNotification;
 	if (!pushNotification) {
