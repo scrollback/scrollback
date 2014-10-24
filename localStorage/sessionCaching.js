39d85b74b2e02412d8a571247e17ad3cca0f173d
Amal Antony <amalantony06@gmail.com>
2014-10-24 17:27:51 +0530

    Used-dn updates lisb.user.

--- localStorage/sessionCaching.js
+++ localStorage/sessionCaching.js
@@ -26,6 +26,11 @@ module.exports = function (objCacheOps) {
 		init.session = sid;
 		return next();
 	}, "validation");
+	
+	libsb.on('user-dn', function (user, next) {
+		libsb.user = user.user;
+		next();
+	}, 1000);
 
 
 	libsb.on('init-dn', function (init, next) {
