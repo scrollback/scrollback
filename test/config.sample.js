7fd1ee0c629a4d3bfdea2e9b9af58a0b1035db41
harish kumar v <harry.softer@gmail.com>
2014-10-28 18:45:28 +0530

    beautify and deleting few temp files.

--- test/config.sample.js
+++ test/config.sample.js
@@ -2,58 +2,58 @@ module.exports = {
 	personaUser: {
 		email: "email",
 		password: "jdalk",
-		username: "testinguser"//username in scrollback
+		username: "testinguser" //username in scrollback
 	},
 	selenium: {
-		username: "username",//browserstack keys
+		username: "username", //browserstack keys
 		accessKey: "access_key",
 		debug: true,
-		capabilities : [// capabilities can be generated using: https://www.browserstack.com/automate/node
+		capabilities: [ // capabilities can be generated using: https://www.browserstack.com/automate/node
 			{
-			 'browserName' : 'iPhone',
-			 'platform' : 'MAC',
-			 'device' : 'iPhone 5'
+				'browserName': 'iPhone',
+				'platform': 'MAC',
+				'device': 'iPhone 5'
 			 }, {
-			 'browserName' : 'android',
-			 'platform' : 'ANDROID',
-			 'device' : 'LG Nexus 4'
+				'browserName': 'android',
+				'platform': 'ANDROID',
+				'device': 'LG Nexus 4'
 			 }, {
-			 'browser' : 'Safari',
-			 'browser_version' : '7.0',
-			 'os' : 'OS X',
-			 'os_version' : 'Mavericks',
-			 'resolution' : '1024x768'
+				'browser': 'Safari',
+				'browser_version': '7.0',
+				'os': 'OS X',
+				'os_version': 'Mavericks',
+				'resolution': '1024x768'
 			 }, {
-			 'browser' : 'IE',
-			 'browser_version' : '10.0',
-			 'os' : 'Windows',
-			 'os_version' : '8',
-			 'resolution' : '1280x1024'
+				'browser': 'IE',
+				'browser_version': '10.0',
+				'os': 'Windows',
+				'os_version': '8',
+				'resolution': '1280x1024'
 			 }, {
-			 'browser' : 'IE',
-			 'browser_version' : '11.0',
-			 'os' : 'Windows',
-			 'os_version' : '8.1',
-			 'resolution' : '1366x768'
+				'browser': 'IE',
+				'browser_version': '11.0',
+				'os': 'Windows',
+				'os_version': '8.1',
+				'resolution': '1366x768'
 			 }, {
-			 'browser' : 'Chrome',
-			 'browser_version' : '36.0',
-			 'os' : 'Windows',
-			 'os_version' : '8.1',
-			 'resolution' : '1366x768'
+				'browser': 'Chrome',
+				'browser_version': '36.0',
+				'os': 'Windows',
+				'os_version': '8.1',
+				'resolution': '1366x768'
 			 }, {
-				'browser' : 'Firefox',
-				'browser_version' : '31.0',
-				'os' : 'Windows',
-				'os_version' : '8.1',
-				'resolution' : '1366x768'
+				'browser': 'Firefox',
+				'browser_version': '31.0',
+				'os': 'Windows',
+				'os_version': '8.1',
+				'resolution': '1366x768'
 			}
 		],
-		tests: [//list of test
+		tests: [ //list of test
 		"chat-area-test.js",
 		"meta-area-test.js",
 		"login-test.js"
 		]
 	}
-	
+
 };
