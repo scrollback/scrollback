2e16ec610d5ad4f15a2538c40d39d8ad61668e47
kamal <kkjoshi.it@gmail.com>
2014-10-29 17:42:15 +0530

    room URL, share and QR code are added to embed tab

--- embed/embed-config.js
+++ embed/embed-config.js
@@ -1,23 +1,52 @@
 /* jslint browser: true, indent: 4, regexp: true */
-/* global $, libsb */
-
+/* global $, libsb, currentState */
+var formField = require('../lib/formField.js');
 libsb.on("config-show", function (conf, next) {
-    var code = '<script>window.scrollback = {room:"' + window.currentState.roomName + '",form:"toast",theme:"dark",minimize:true};(function(d,s,h,e){e=d.createElement(s);e.async=1;e.src=(location.protocol === "https:" ? "https:" : "http:") + "//' + window.location.host + '/client.min.js";d.getElementsByTagName(s)[0].parentNode.appendChild(e);}(document,"script"));</script>',
-        $textarea = $("<textarea>").addClass("embed-code").attr("readonly", true).text(code),
-        $div = $("<div>").append($("<div>").addClass("settings-item").append(
-            $("<p>").text("Place the following code just before the closing </body> tag "),
-            $textarea
-        ));
+   //room URL
+	var $div = $("<div>");
+	var roomURL = "https://" + window.location.host + "/" + currentState.roomName;
+	var $roomURLField = formField("URL", "text", "embed-room-url", roomURL).attr("readonly", "readonly");
+	$roomURLField.click(function() {
+		this.select();
+	});
+    $div.append($roomURLField);
 
-    $textarea.click(function() {
-        this.select();
-    });
+	var $shareDiv = $("<div>").addClass('embed-share').append($("<a>").attr("href", "https://plus.google.com/share?url=" + roomURL).
+					  attr("target", "_blank").
+					  addClass("google  embed-share-button").text("Google+"));
+	$shareDiv.append($("<a>").attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + roomURL).
+					 attr("target", "_blank").
+					 addClass("facebook  embed-share-button").text("facebook"));
+	$shareDiv.append($("<a>").attr("href", "https://twitter.com/intent/tweet?url=" + roomURL).
+					 attr("target", "_blank").
+					 addClass("twitter embed-share-button").text("twitter"));
+	$div.append(
+		formField("Share on", "", "share-embed", $shareDiv)
+	);
 
+	//qr code
+	var qrCode = $("<img>").attr("src", "https://api.qrserver.com/v1/create-qr-code/?size=128x128&data=https://sb.lk/" + currentState.roomName);
+	$div.append(
+		formField("QR code", "", "embed-qr-code", qrCode)
+	);
+
+	var code = '<script>window.scrollback = {room:"' + window.currentState.roomName + '",form:"toast",theme:"dark",minimize:true};(function(d,s,h,e){e=d.createElement(s);e.async=1;e.src=(location.protocol === "https:" ? "https:" : "http:") + "//' + window.location.host + '/client.min.js";d.getElementsByTagName(s)[0].parentNode.appendChild(e);}(document,"script"));</script>';
+	var $textarea = $("<textarea>").addClass("embed-code").attr("readonly", true).text(code);
+  	$div.append(
+    	formField("Code","", "embed-code", $textarea)
+		/*$("<p>").text("Place the following code just before the closing </body> tag "),
+		$textarea*/
+  	);
+
+	$textarea.click(function() {
+		this.select();
+	});
     conf.embed = {
-        text: "Embed",
+      text: "Share & Embed",
         html: $div,
         prio: 400
     };
 
     next();
 }, 500);
+
