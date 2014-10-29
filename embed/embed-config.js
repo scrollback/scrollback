e0b617b1ee5332a5bbac0e9d506c8a992c8bba65
kamal <kkjoshi.it@gmail.com>
2014-10-29 20:00:32 +0530

    background color selector

--- embed/embed-config.js
+++ embed/embed-config.js
@@ -5,7 +5,7 @@ libsb.on("config-show", function (conf, next) {
    //room URL
 	var $div = $("<div>");
 	var roomURL = "https://" + window.location.host + "/" + currentState.roomName;
-	var $roomURLField = formField("URL", "text", "embed-room-url", roomURL).attr("readonly", "readonly");
+	var $roomURLField = formField("URL", "text", "embed-room-url", roomURL).attr("readonly", true);
 	$roomURLField.click(function() {
 		this.select();
 	});
@@ -23,14 +23,19 @@ libsb.on("config-show", function (conf, next) {
 	$div.append(
 		formField("Share on", "", "share-embed", $shareDiv)
 	);
-
 	//qr code
-	var qrCode = $("<img>").attr("src", "https://api.qrserver.com/v1/create-qr-code/?size=128x128&data=https://sb.lk/" + currentState.roomName);
+	var qrCode = $("<img>").attr("src", "https://api.qrserver.com/v1/create-qr-code/?size=128x128&data=https://sb.lk/" + window.currentState.roomName);
 	$div.append(
 		formField("QR code", "", "embed-qr-code", qrCode)
 	);
+	//change title color
+	var $titleBackground = formField("Title Background Color", "text", "embed-title-background", "#");
+	$titleBackground.find("input").attr("type", "color");
 
-	var code = '<script>window.scrollback = {room:"' + window.currentState.roomName + '",form:"toast",theme:"dark",minimize:true};(function(d,s,h,e){e=d.createElement(s);e.async=1;e.src=(location.protocol === "https:" ? "https:" : "http:") + "//' + window.location.host + '/client.min.js";d.getElementsByTagName(s)[0].parentNode.appendChild(e);}(document,"script"));</script>';
+	$div.append($titleBackground);
+
+
+	var code = getCode();
 	var $textarea = $("<textarea>").addClass("embed-code").attr("readonly", true).text(code);
   	$div.append(
     	formField("Code","", "embed-code", $textarea)
@@ -41,6 +46,21 @@ libsb.on("config-show", function (conf, next) {
 	$textarea.click(function() {
 		this.select();
 	});
+	var $titleTextField;
+	$titleBackground.on("keydown paste input change", function() {
+		if (!$titleTextField) $titleTextField = $("#embed-title-background");
+		var text = $titleTextField.val();
+		var code = "";
+		console.log("text:",text);
+		if (/(^#[0-9A-Fa-f]{6}$)|(^#[0-9A-Fa-f]{3}$)/i.test(text)) {
+			code = getCode(text);
+			$textarea.text(code);
+			$titleTextField.removeClass("error");
+		} else {
+			$titleTextField.addClass("error");
+		}
+
+	});
     conf.embed = {
       text: "Share & Embed",
         html: $div,
@@ -50,3 +70,18 @@ libsb.on("config-show", function (conf, next) {
     next();
 }, 500);
 
+function getCode(titleBackground) {
+	var code = '<script>window.scrollback = {%sroom:"' + window.currentState.roomName + '",form:"toast",theme:"dark",minimize:true};(function(d,s,h,e){e=d.createElement(s);e.async=1;e.src=(location.protocol === "https:" ? "https:" : "http:") + "//' + window.location.host + '/client.min.js";d.getElementsByTagName(s)[0].parentNode.appendChild(e);}(document,"script"));</script>';
+	if (titleBackground) code = parse(code, "titleBackground:" + titleBackground + ",");
+	else code = parse(code, "");
+	return code;
+}
+
+function parse(str) {
+	var args = [].slice.call(arguments, 1),
+		i = 0;
+
+	return str.replace(/%s/g, function() {
+		return args[i++];
+	});
+}
