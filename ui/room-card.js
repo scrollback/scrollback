cda3d405d97ba5f0ace2df065140ab4827ceb72c
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-11-03 18:03:36 +0530

    Add colors to room cards

--- ui/room-card.js
+++ ui/room-card.js
@@ -4,8 +4,28 @@
 var roomCard = {},
 	$template = $(".card-item-wrap").eq(0);
 
+function getHashCode(str) {
+	var hash = 0, i, chr, len;
+
+	if (str.length === 0) {
+		return hash;
+	}
+
+	for (i = 0, len = str.length; i < len; i++) {
+		chr   = str.charCodeAt(i);
+		hash  = ((hash << 5) - hash) + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	if (hash < 0) {
+		hash = hash >>> 1;
+	}
+
+	return hash % 10;
+}
+
 roomCard.render = function(roomObj, $el) {
-	var $card;
+	var $card,
+		roomName;
 
 	$el = $el || $template.clone(false);
 
@@ -13,13 +33,16 @@ roomCard.render = function(roomObj, $el) {
 		return;
 	}
 
+	roomName = roomObj.id;
+
 	$el = $el || $template.clone(false);
+
+	$el.addClass("color-" + getHashCode(roomName));
 
 	$card = $el.find(".card-item");
 
-	$card.attr("data-room", roomObj.id);
-
-	$card.find(".card-header-title").text(roomObj.id);
+	$card.attr("data-room", roomName);
+	$card.find(".card-header-title").text(roomName);
 	$card.find(".card-content-summary").text(roomObj.description || "This room has no description.");
 
 	return $el;
