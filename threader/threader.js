e6cccb5fb02f4604d26116c9ffbc83711db70c97
kamal <kkjoshi.it@gmail.com>
2014-11-06 17:16:17 +0530

    make threading optional

--- threader/threader.js
+++ threader/threader.js
@@ -5,7 +5,15 @@ var timeout = 60 * 1000;
 var client;
 var pendingCallbacks = {};
 var core;
+var validator = new (require('valid'))();
 
+var threaderValidator = {
+	params: [{
+		threader: ['undefined', {
+			enabled: ['boolean']
+		}]
+	}]
+};
 /**
 Communicate with scrollback java Process through TCP and set message.threads.
 */
@@ -13,12 +21,21 @@ module.exports = function(coreObj) {
 	core = coreObj;
 	if (config.threader) {
 		init();
+		core.on("room", function(action, callback) {
+			var result = validator.validate(action.room, threaderValidator);
+			if (!result.status) {
+				log.e("Error: invalid action params", JSON.stringify(result), " ID: ", action.id);
+				callback(new Error("INVALID_THREADER_PARAMS"));
+			} else callback();
+		}, "appLevelValidation");
+
 		core.on('text', function(message, callback) {
-			if(message.type == "text" && client.writable) {//if client connected and text message
+			var room = message.room;
+			if (client.writable && (!room.params || !room.params.threader || room.params.threader.enabled)) {//if client connected
 				var threadId = message.threads && message.threads[0] ? message.threads[0].id : undefined;
 				var msg = JSON.stringify({
 					id: message.id, time: message.time, author: message.from.replace(/guest-/g,""),
-					text: message.text/*.replace(/['"]/g, '').replace(/\n/g," ")*/,
+					text: message.text,
 					room: message.to,
 					threadId: threadId
 				});
@@ -85,7 +102,7 @@ function processReply(data){
 					message.threads.splice(i, 1);
 					i--;
 				}
-			};
+			}
 			if (!update) {
 				message.threads.push({id: id, title: title, score: 1});
 			}
@@ -115,7 +132,7 @@ function init(){
 	log("Trying to connect.... ");
 	client = net.connect({port: config.threader.port, host: config.threader.host},
 		function() { //'connect' listener
-		console.log('client connected');
+		log('client connected');
 		client.write("[");//sending array of JSON objects
 	});
 	var d = "";//wait for new line.
@@ -131,8 +148,9 @@ function init(){
 		}
 	});
 
-	client.on('error', function(error){
-		setTimeout(function(){
+	client.on('error', function(error) {
+		log("Error: ", error);
+		setTimeout(function() {
 			init();
 		}, timeout);//try to reconnect after 1 min
 	});
