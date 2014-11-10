99df0bfbecc56dda969cb32ecbe5544a70172131
harish kumar v <harry.softer@gmail.com>
2014-11-10 16:45:10 +0530

    Fixing sound bugs. adding mock storage for tests.

--- ui/guest-settings.js
+++ ui/guest-settings.js
@@ -2,18 +2,18 @@
 /* global $, libsb */
 
 libsb.on("auth-menu", function(menu, next) {
-	var soundNotification = (libsb.user.params.notification && libsb.user.params.notification.sound);
+	var soundNotification = (libsb.user.params.notifications && typeof libsb.user.params.notifications.sound == "boolean")? libsb.user.params.notifications.sound: true;
 
-	menu.items["guest-sound-notification-" + (soundNotification ? "enable" : "disable")] = {
-		text: (soundNotification ? "Enable" : "Disable") + ' sound notifications',
+	menu.items["guest-sound-notification-" + (soundNotification ? "disable" : "enable")] = {
+		text: (soundNotification ? "Disable" : "Enable") + ' sound notifications',
 		prio: 500,
 		action: function() {
 			var user = $.extend({}, libsb.user);
 
-			if (!user.params.notification) {
-				user.params.notification = { sound: true };
+			if (!user.params.notifications) {
+				user.params.notifications = { sound: true };
 			} else {
-				user.params.notification.sound = !user.params.notification.sound;
+				user.params.notifications.sound = !user.params.notifications.sound;
 			}
 
 			libsb.emit("user-up", {user: user}, function() {
