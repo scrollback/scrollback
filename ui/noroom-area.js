5ecf15fa593f2bbf6e0cc540352f5178292c09d7
Amal Antony <amalantony06@gmail.com>
2014-11-10 16:12:13 +0530

    Fixed the noroom are username issue.

--- ui/noroom-area.js
+++ ui/noroom-area.js
@@ -1,30 +1,9 @@
 /* jshint browser: true */
-/* global $, libsb, currentState */
+/* global $, libsb */
 
 libsb.on('navigate', function(state, next) {
-	if (state.mode === 'profile' && state.source === 'noroom') {
-		$('#profile-view-name').text(currentState.roomName);
+	if (state.mode === 'profile') {
+		$('#profile-view-name').text(state.roomName);
 	}
 	next();
 }, 100);
-
-//$("#noroom-view-create").on("click", function() {
-//	var roomObj = {
-//		to: currentState.roomName,
-//		room: {
-//			id: currentState.roomName,
-//			description: '',
-//			params: {
-//
-//			},
-//			guides: {}
-//		}
-//	};
-//	libsb.emit('room-up', roomObj, function() {
-//		libsb.emit("navigate", {
-//			mode: 'normal',
-//			tab: 'info',
-//			time: new Date().getTime()
-//		});
-//	});
-//});
