4637d2f7084c97595af695ab5f9086ed308c5c37
Amal Antony <amalantony06@gmail.com>
2014-11-11 17:22:44 +0530

    Fixed the push-notification issues.

--- push-notification/push-notification.js
+++ push-notification/push-notification.js
@@ -1,4 +1,4 @@
-// var log = require('../lib/logger.js');
+var logger = require('../lib/logger.js');
 var notify = require('./notify.js');
 var config = require('../config.js');
 var internalSession = Object.keys(config.whitelists)[0];
@@ -29,15 +29,31 @@ module.exports = function(core) {
 			});
 		}
 	}
+	
+	function makePayload(title, message, text) {
+		var payload = {
+			title: title,
+			message: message,
+			roomName: text.to,
+			time: text.time,
+			threadId: text.threads[0].id
+		};
+		var msgLen = JSON.stringify(payload).length;
+		
+		if (msgLen > 4 * 1024) {
+			logger.e("Payload too big for push notification!");
+			payload.message = payload.message.substring(0, 700);
+		}
+		return payload;
+	}
+	
 	core.on('text', function(text, next) {
-
+		if (!text.threads || !text.threads[0]) return next();
 		// push notification when user is mentioned in a text message.
 		var mentions = text.mentions ? text.mentions : [];
-		var payload = {
-			title: text.from + " has mentioned you on " + text.to,
-			message: text.text,
-			text: text
-		};
+		var title = text.from + " has mentioned you on " + text.to;
+		var message = text.text;
+		var payload = makePayload(title, message, text);
 		mentions.forEach(function(user) {
 			notifyUserId(user, payload);
 		});
@@ -46,11 +62,9 @@ module.exports = function(core) {
 		// push notification on new thread creation.
 		if (text.labels && text.labels.hasOwnProperty('startOfThread') &&
 			text.labels.startOfThread === 1 && text.threads[0]) {
-			payload = {
-				title: text.from + " has started a new discussion on " + text.to,
-				message: text.threads[0].title,
-				text: text
-			};
+			title = text.from + " has started a new discussion on " + text.to;
+			message = text.threads[0].title;
+			payload = makePayload(title, message, text);
 			core.emit("getUsers", {
 				memberOf: text.to,
 				session: internalSession
