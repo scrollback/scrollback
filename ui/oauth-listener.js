20e0aae949522869931b6dffc26a1c436b23bf82
harish kumar v <harry.softer@gmail.com>
2014-11-19 18:50:41 +0530

    Possible fix for the google invalid credentials error.

--- ui/oauth-listener.js
+++ ui/oauth-listener.js
@@ -1,10 +1,12 @@
 /* jshint browser:true */
 /* global $, libsb */
+var initSent = false;
+
 $(window).on("message", function(event) {
 	var data = event.originalEvent.data;
 	var action;
 	if (event.originalEvent.origin !== "https://" + location.host) return;
-
+	if(initSent) return;
 	if (typeof data === 'string') {
 		try {
 			action = JSON.parse(data);
@@ -18,10 +20,19 @@ $(window).on("message", function(event) {
 
 	if (!data.command || data.command != "signin") return;
 	delete data.command;
-	libsb.emit("init-up", action, function() {});
+	initSent = true;
+	libsb.emit("init-up", action, done);
 });
 
 $(window).on("phonegapmsg", function(e, p) {
 	delete p.command;
-	libsb.emit('init-up', p);
+	if(initSent) return;
+	initSent = true;
+	libsb.emit('init-up', p, done);
 });
+
+
+
+function done() {
+	initSent = false;
+}
