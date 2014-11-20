cbe152e75914463ed6d66005d50a1b749b168ebb
kamal <kkjoshi.it@gmail.com>
2014-11-20 12:29:43 +0530

    removed console.log

--- phonegap/phonegap-client.js
+++ phonegap/phonegap-client.js
@@ -5,13 +5,11 @@ libsb.on("navigate", function(state, next) {
 	var regex;
 	if (state.phonegap && state.source === 'boot') {
 		regex = new RegExp("(^https?:" + config.server.host + ")(($)|(\/)).*");
-		console.log("test", regex.test("https://local.scrollback.io/akdshf");
 		window.openExternal = function openExternal(elem) {
 			window.open(elem.href, "_system");
 			return false;
 		};
 		$(document).on('click', "a[href]", function(e) {
-			// config.host e.g- //local.scrollback.io
 
 			if (!(regex).test(this.href)) {
 				e.preventDefault();
