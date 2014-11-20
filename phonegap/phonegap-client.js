6b173fa41023ac36e9642b074f2250674b74cd9d
kamal <kkjoshi.it@gmail.com>
2014-11-20 11:41:17 +0530

    memorize regex

--- phonegap/phonegap-client.js
+++ phonegap/phonegap-client.js
@@ -2,14 +2,18 @@
 /* global libsb, $ */
 var config = require('../client-config.js');
 libsb.on("navigate", function(state, next) {
+	var regex;
 	if (state.phonegap && state.source === 'boot') {
+		regex = new RegExp("(^https?:" + config.server.host + ")(($)|(\/)).*");
+		console.log("test", regex.test("https://local.scrollback.io/akdshf");
 		window.openExternal = function openExternal(elem) {
 			window.open(elem.href, "_system");
 			return false;
 		};
 		$(document).on('click', "a[href]", function(e) {
 			// config.host e.g- //local.scrollback.io
-			if (!(new RegExp("(^https?:" + config.host + ")(($)|(\/)).*")).test(this.href)) {
+
+			if (!(regex).test(this.href)) {
 				e.preventDefault();
 				window.openExternal(this);
 			}
