d85f8f1aaaef09ae193119eb6dc3036430341340
Satyajit Sahoo <satyajit.happy@gmail.com>
2014-11-29 15:44:22 +0530

    Use an event based approach

--- dialogs/dialogs-client.js
+++ dialogs/dialogs-client.js
@@ -1,8 +1,157 @@
 /* jshint browser: true */
 /* global $, libsb */
 
-var getDialogTemplates = require("./dialogs-templates.js"),
+var validate = require("../lib/validate.js"),
+	afterSignUp,
 	signingUp;
+
+function showError(error, entry) {
+	var $entry = $(entry),
+		$errorMsg = $entry.data("errorMsg") || $();
+
+	if (!error) {
+		$entry.removeClass("error");
+		$errorMsg.popover("dismiss");
+
+		return;
+	}
+
+	$entry.addClass("error");
+
+	$errorMsg = $("<div>").addClass("error").append(
+		$("<div>").addClass("popover-content").text(error)
+	).popover({
+		origin: $entry
+	});
+
+	$entry.data("errorMsg", $errorMsg);
+
+	$(document).off("modalDismissed.errorentry").on("modalDismissed.errorentry", function() {
+		$errorMsg.popover("dismiss");
+	});
+
+	$entry.off("change.errorentry input.errorentry paste.errorentry").on("change.errorentry input.errorentry paste.errorentry", function() {
+		$errorMsg.popover("dismiss");
+
+		$(this).removeClass("error");
+	});
+
+	$entry.focus();
+}
+
+function checkExisting(name, callback) {
+	libsb.emit("getRooms", {
+		ref: name
+	}, function(err, res) {
+		if (res && res.results && res.results.length) {
+			return callback(true);
+		}
+
+		libsb.emit("getUsers", {
+			ref: name
+		}, function(err, res) {
+			if (res && res.results && res.results.length) {
+				return callback(true);
+			}
+
+			return callback(false);
+		});
+	});
+
+}
+
+function validateName(entry, button, type, callback) {
+	var $entry = $(entry),
+		$button = $(button),
+		name = $entry.val(),
+		validation;
+
+	name = (typeof name === "string") ? name.toLowerCase().trim() : "";
+
+	validation = validate(name);
+
+	if (!validation.isValid) {
+		return showError(type + " " + validation.error, entry);
+	}
+
+	$button.addClass("working");
+
+	checkExisting(name, function(isTaken) {
+		$button.removeClass("working");
+
+		if (isTaken) {
+			showError(name + " is not available. May be try another?", entry);
+		} else {
+			showError(false);
+
+			if (typeof callback === "function") {
+				callback(name);
+			}
+		}
+	});
+}
+
+function createRoom(entry, button, callback) {
+	validateName(entry, button, "Room", function(name) {
+		var errormessage = "We could not create the room. Please refresh the page and try again.";
+
+		if (!name) {
+			return showError(errormessage, entry);
+		}
+
+		libsb.emit("room-up", {
+			to: name,
+			room: {
+				id: name,
+				description: "",
+				params: {},
+				guides: {}
+			}
+		}, function(err) {
+			if (err) {
+				return showError(errormessage, entry);
+			}
+
+			if (typeof callback === "function") {
+				callback();
+			}
+
+			libsb.emit("navigate", { dialog: null });
+		});
+	});
+}
+
+function createUser(entry, button, callback) {
+	validateName(entry, button, "User", function(name) {
+		var errormessage = "We could not create your account. Please refresh the page and try again.";
+
+		if (!name || !libsb.user || !libsb.user.identities) {
+			return showError(errormessage, entry);
+		}
+
+		libsb.emit("user-up", {
+			user: {
+				id: name,
+				identities: libsb.user.identities,
+				picture: libsb.user.picture,
+				params: {
+					pictures: libsb.user.params.pictures
+				},
+				guides: {}
+			}
+		}, function(err) {
+			if (err) {
+				return showError(errormessage, entry);
+			}
+
+			if (typeof callback === "function") {
+				afterSignUp = callback;
+			}
+
+			libsb.emit("navigate", { dialog: null });
+		});
+	});
+}
 
 function showDialog(eventname, type, template) {
 	libsb.emit(eventname + "-dialog", template, function(err, dialog) {
@@ -62,52 +211,6 @@ function showDialog(eventname, type, template) {
 	});
 }
 
-function emitDialog(type) {
-	var defaults, template, eventname,
-		dialogTemplates;
-
-	if (!type) {
-		return $.modal("dismiss");
-	}
-
-	// Default dialog template
-	defaults = {
-		title: "", // Modal title
-		description: "", // A description to be displayed under title
-		buttons: {}, // List of objects, e.g. - google: { text: "Google+", action: function() {} }
-		content: [], // Additional content to be displayed under buttons
-		action: null, // Action button, e.g. - { text: "Create account", action: function() {} }
-		dismiss: true // Dialog is dismissable or not
-	};
-
-	dialogTemplates = getDialogTemplates({
-		roomName: (window.currentState.mode === "noroom") ? window.currentState.roomName : ""
-	});
-
-	template = (type in dialogTemplates) ? $.extend({}, defaults, dialogTemplates[type]) : $.extend({}, defaults);
-
-	eventname = type;
-
-	switch (type) {
-		case "createroom":
-			if (!libsb.user) {
-				return;
-			}
-
-			if (signingUp) {
-				template = $.extend({}, defaults, dialogTemplates["signup-createroom"]);
-				signingUp = false;
-			} else if ((/^guest-/).test(libsb.user.id)) {
-				template = $.extend({}, defaults, dialogTemplates["auth-createroom"]);
-				eventname = "auth";
-			}
-
-			break;
-	}
-
-	showDialog(eventname, type, template);
-}
-
 // When modal is dismissed, reset the dialog property to null
 $(document).on("modalDismissed", function() {
 	libsb.emit("navigate", {
@@ -118,8 +221,33 @@ $(document).on("modalDismissed", function() {
 
 // Emit a dialog event when navigate is called
 libsb.on("navigate", function(state, next) {
+	var eventname;
+
 	if (state.source === "dialog" || ("dialog" in state.changes && state.source !== "modal-dismiss")) {
-		emitDialog(state.dialog);
+		if (!state.dialog) {
+			return $.modal("dismiss");
+		}
+
+		eventname = state.dialog;
+
+		if (state.dialog === "createroom") {
+			if (!libsb.user) {
+				return;
+			}
+
+			if (!signingUp && (/^guest-/).test(libsb.user.id)) {
+				eventname = "auth";
+			}
+		}
+
+		showDialog(eventname, state.dialog, {
+			title: null, // Modal title
+			description: null, // A description to be displayed under title
+			buttons: {}, // List of objects, e.g. - google: { text: "Google+", action: function() {} }
+			content: [], // Additional content to be displayed under buttons
+			action: null, // Action button, e.g. - { text: "Create account", action: function() {} }
+			dismiss: true // Dialog is dismissable or not
+		});
 	}
 
 	next();
@@ -145,10 +273,85 @@ libsb.on("init-dn", function(init, next) {
 	next();
 }, 500);
 
+libsb.on("auth-dialog", function(dialog, next) {
+	var roomName = (window.currentState.mode === "noroom") ? window.currentState.roomName : "";
+
+	if (window.currentState.dialog === "createroom" && (/^guest-/).test(libsb.user.id)) {
+		dialog.title = "Create a new room";
+		dialog.description = "You must sign in first";
+		dialog.content = [
+			"<p>Choose a room name for your community</p>",
+			"<input type='text' id='createroom-dialog-room' value='" + roomName + "' disabled>"
+		];
+	} else {
+		dialog.title = "Sign in to Scrollback";
+	}
+
+	next();
+}, 100);
+
 libsb.on("createroom-dialog", function(dialog, next) {
+	var roomName = (window.currentState.mode === "noroom") ? window.currentState.roomName : "";
+
+	if (signingUp) {
+		dialog.title = "Create a new room";
+		dialog.content = [
+			"<p>Choose a username</p>",
+			"<input type='text' id='createroom-dialog-user' autofocus>",
+			"<p>Choose a room name for your community</p>",
+			"<input type='text' id='createroom-dialog-room' value='" + roomName + "' autofocus>"
+		];
+		dialog.action = {
+			text: "Sign up & create room",
+			action: function() {
+				var $userEntry = $("#createroom-dialog-user"),
+					$roomEntry = $("#createroom-dialog-room"),
+					username = $userEntry.val(),
+					roomname = $roomEntry.val(),
+					self = this;
+
+				username = (typeof username === "string") ? username.toLowerCase().trim() : "";
+				roomname = (typeof roomname === "string") ? roomname.toLowerCase().trim() : "";
+
+				if (username === roomname) {
+					return showError("User and room names cannot be the same", $userEntry);
+				}
+
+				validateName($roomEntry, self, "Room", function() {
+					createUser($userEntry, self, function() {
+						createRoom($roomEntry, self);
+					});
+				});
+			}
+		};
+	} else {
+		dialog.title = "Create a new room";
+		dialog.description = "Choose a room name for your community";
+		dialog.content = [ "<input type='text' id='createroom-dialog-room' value='" + roomName + "' autofocus>" ];
+		dialog.action = {
+			text: "Create room",
+			action: function() {
+				createRoom("#createroom-dialog-room", this);
+			}
+		};
+	}
+
 	next();
 }, 100);
 
 libsb.on("signup-dialog", function(dialog, next) {
+	dialog.title = "Finish sign up";
+	dialog.description = "Choose a username";
+	dialog.content = [
+		"<input type='text' id='signup-dialog-user' autofocus>",
+		"<p>Be creative. People in Scrollback will know you by this name.</p>"
+	];
+	dialog.action = {
+		text: "Create account",
+		action: function() {
+			createUser("#signup-dialog-user", this);
+		}
+	};
+
 	next();
 }, 100);
