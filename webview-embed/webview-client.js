0644847560b0ab57ae6d9920d89bc40e7e444a48
Amal Antony <amalantony06@gmail.com>
2014-12-03 17:56:43 +0530

    Css changes to hide sign-in and room-area on webview.

--- webview-embed/webview-client.js
+++ webview-embed/webview-client.js
@@ -1,11 +1,12 @@
 /* jshint node:true, browser:true */
-/* global libsb, currentState */
+/* global libsb, currentState, $ */
 
 var changedNick = false;
 
 libsb.on("navigate", function(state, next) {
     if (!changedNick && state.hasOwnProperty('webview') && currentState.connectionStatus === "online") {
         try {
+            $('body').addClass('webview-true webview');
             var webview = JSON.parse(decodeURIComponent(state.webview));
             if (webview.hasOwnProperty('nick')) {
                 libsb.emit('init-up', {
