825edc3d62cb72c10fa7dedad115126b5ae24d06
Amal Antony <amalantony06@gmail.com>
2014-12-12 11:17:42 +0530

    Login with facebook & google replaced with polyfill

--- google/google-client.js
+++ google/google-client.js
@@ -1,39 +1,10 @@
 /* jshint browser:true, node:true */
-/* global libsb, $, currentState*/
+/* global libsb, $ */
+
 var config = require("../client-config.js");
 
-function getParameterByName(name, url) {
-	name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-	var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-		results = regex.exec(url);
-	return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
-
 function loginWithGoogle() {
-	var googleRef = window.open("https:" + config.server.host + "/r/google/login", "_blank", "location=no");
-	if (currentState.phonegap) {
-		var interval = setInterval(function() {
-			googleRef.executeScript({
-				code: "window.location.href;"
-			}, function(ret) {
-				var url = ret[0];
-				var code = getParameterByName('code', url);
-				if (code !== null) {
-					var auth = {
-						command: "signin",
-						auth: {
-							google: {
-								code: code
-							}
-						}
-					};
-					$(window).trigger("phonegapmsg", [auth]);
-					clearInterval(interval);
-					googleRef.close();
-				}
-			});
-		}, 100);
-	}
+	window.open("https:" + config.server.host + "/r/google/login", "_blank", "location=no");
 }
 
 $('.js-phonegap-google-login').click(loginWithGoogle);
