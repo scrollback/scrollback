3be7c874f973b67f1e438e43494b6ab9fc1657bc
kamal <kkjoshi.it@gmail.com>
2015-01-02 17:32:55 +0530

    add new user, edit text, edit labels

--- storage/action-transform.js
+++ storage/action-transform.js
@@ -1,7 +1,10 @@
 var storageUtils = require('./storage-utils.js'),
 	log = require('../lib/logger.js');
-function makePut(type) {
-	return { source: null, filters: [], type: type };
+function makePut(type, source) {
+	if (!type || !source) {
+		return log.d("No source or type");
+	}
+	return { source: source, filters: [], type: type };
 }
 
 /*
@@ -16,18 +19,15 @@ exports.text = function (text) {
 	var puts = [], put;
 	
 	/* Start Compatibility Block */
-	text.thread = text.threads[0].id.substr(0,text.threads[0].id.length-1);
+	text.thread = text.threads[0].id.substr(0, text.threads[0].id.length-1);
 	text.title = text.threads[0].title;
-	text.labels = text.labels || {};
 	text.labels['color' + text.threads[0].id.substr(text.threads[0].id.length-1)] = 1;
-	text.tags = [];
-	for(var i in text.labels) if(text.labels[i] > 0.5) text.tags.push(i);
+	addTags(text);
 	/* End Compatibility Block */
 	log.d("Text:", text);
 	// insert text
-	if(text.text) {
-		put = makePut("insert");
-		put.source = 'texts';
+	if (text.text) {
+		put = makePut("insert", "texts");
 		put.insert = {
 			id: text.id, from: text.from, to: text.to,
 			text: text.text, time: storageUtils.timetoString(text.time), 
@@ -39,11 +39,10 @@ exports.text = function (text) {
 	
 	// insert thread
 	if (text.thread) {
-		put = makePut(text.id == text.thread ? "insert" : "update");
-		put.source = 'threads';
+		put = makePut(text.id == text.thread ? "insert" : "update", 'threads');
 		put.filters.push(['id', 'eq', text.thread]);
 
-		if(text.id == text.thread) {
+		if (text.id == text.thread) {
 			/* This is a new thread */
 			put.insert = {
 				id: text.thread, from: text.from, to: text.to,
@@ -67,45 +66,48 @@ exports.text = function (text) {
 
 
 exports.edit = function (edit) {
-	var puts = [], put = makePut();
-	put.source = 'texts';
+	var puts = [], put = makePut("update", 'texts');
 	put.filters.push(['id', 'eq', edit.ref]);
 	put.update = [];
-	
-	if(edit.text) {
+	addTags(edit);
+	if (edit.text) {
 		put.update.push(['text', 'set', edit.text]);
 	}
+	/*
+	if (edit.title) {
+		
+	}*/
 	
-	if(edit.title) {
+	if (edit.tags) {
+		put.update.push(['tags', 'set', edit.tags]); 
 	}
 	
+	puts.push(put);
 	return puts;
 };
 
 exports.room = exports.user = function (action) {
-	var entity = action[action.type], put;
 	
-	put.source = 'entities';
-	if(entity.identities.length) {
-		entity.identities = entity.identities.map(function(ident) {
-			return ident.split(':', 2);
-		});
-	}
-	
-	put.insert = {
-		id: entity.id, type: action.type, description: entity.description,
-		picture: entity.picture, createTime: entity.old.createTime,
-		identities: entity.identities.map(function(ident) { 
-			return ident.split(':', 2); 
-		}),
-		timezone: entity.timezone, locale: entity.locale,
-		params: entity.params
-	};
-	
-	put.filters.push(['id', 'eq', entity.id]);
-	
-	if(action.type == 'room') {
-		put.insert.guides = entity.guides;
+	var entity = action[action.type],
+		isNewEntity = !(entity.old && entity.old.id),
+		put = makePut(isNewEntity ? 'insert' : 'update', 'entities');
+	if (isNewEntity) { // new room or user.
+		put.insert = {
+			id: entity.id, type: action.type, 
+			description: entity.description,
+			picture: entity.picture, 
+			createtime: entity.createTime,
+			identities: entity.identities.map(function(ident) { 
+				return ident.split(':', 2); 
+			}),
+			timezone: entity.timezone, locale: entity.locale,
+			params: entity.params,
+			guides: entity.guides,
+			deletetime: entity.deleteTime
+		};
+	} else { // update
+		put.filters.push(['id', 'eq', entity.id]);
+		put.update.push(['']);
 	}
 	
 	return [put];
@@ -114,3 +116,10 @@ exports.room = exports.user = function (action) {
 exports.join = exports.part = exports.admit = exports.expel = function (/*action*/) {
 	
 };
+
+
+
+function addTags(action) {
+	if (!action.tags) action.tags = [];
+	for (var i in action.labels) if (action.labels[i] > 0.5) action.tags.push(i);
+}
--- storage/storage-test.js
+++ storage/storage-test.js
@@ -1,5 +1,6 @@
 /* jshint mocha: true */
 var assert = require('assert'),
+	crypto = require('crypto'),
 	core =new (require('ebus'))(),
 	generate = require("../lib/generate.js"),
 	storageUtils = require('./storage-utils.js'),
@@ -41,6 +42,29 @@ describe("Storage Test.", function() {
 		});
 	});
 	
+	it("Insert new text message. (Labels and tags)", function(done) {
+		var msg = getNewTextMessage();
+		msg.labels.abusive = 1;
+		msg.labels.hidden = 1;
+		msg.tags = ['abc'];
+		core.emit("text", msg, function() {
+			log("inserted message");
+			pg.connect(connString, function(err, client, cb) {
+				storageUtils.runQueries(client, 
+										[{query: "SELECT * from texts where id=$1", values: [msg.id]}], 
+										function(err, results) {
+					log.d("Arguments:", arguments);
+					results.forEach(function(result) {
+						result.rows[0].tags.sort();
+						assert.deepEqual(result.rows[0].tags, ['abc', 'color3', 'abusive', 'hidden'].sort(), "tags / labels not saved");
+					});
+					cb();
+					done();
+				});
+			});
+		});
+	});
+	
 	it("Update Thread", function(done) {
 		var m1 = getNewTextMessage();
 		core.emit("text", m1, function() {
@@ -63,6 +87,76 @@ describe("Storage Test.", function() {
 			});
 		});
 	});	
+	
+	it("Edit (Edit text)", function(done) {
+		var m1 = getNewTextMessage();
+		core.emit("text", m1, function() {
+			var text = generate.sentence(11);
+			var edit = {
+				ref: m1.id,
+				text: text
+			};
+			core.emit("edit", edit, function() {
+				pg.connect(connString, function(err, client, cb) {
+					storageUtils.runQueries(client, 
+											[{query: "SELECT * from texts where id=$1", values: [m1.id]}], 
+											function(err, results) {
+						log.d("Arguments:", arguments);
+						results.forEach(function(result) {
+							assert.equal(result.rows[0].text, text, "Updating text failed");
+						});
+						cb();
+						done();
+					});
+				});
+			});
+		});
+	});	
+	
+	it("Edit (labels text)", function(done) {
+		var m1 = getNewTextMessage();
+		m1.labels.abusive = 1;
+		m1.labels.hidden = 1;
+		core.emit("text", m1, function() {
+			var edit = {
+				ref: m1.id,
+				labels: {abusive: 1, color3: 1}
+			};
+			core.emit("edit", edit, function() {
+				pg.connect(connString, function(err, client, cb) {
+					storageUtils.runQueries(client, 
+											[{query: "SELECT * from texts where id=$1", values: [m1.id]}], 
+											function(err, results) {
+						log.d("Arguments:", arguments);
+						results.forEach(function(result) {
+							assert.deepEqual(result.rows[0].tags, ['abusive', 'color3'], "Updating text failed");
+						});
+						cb();
+						done();
+					});
+				});
+			});
+		});
+	});	
+	
+	it("storing new user.", function(done) {
+		var user = getNewUser();
+		core.emit("user", user, function(){
+			pg.connect(connString, function(err, client, cb) {
+				storageUtils.runQueries(client, 
+										[{query: "SELECT * from entities where id=$1", values: [user.user.id]}], 
+										function(err, results) {
+					log.d("Arguments:", arguments);
+					results.forEach(function(result) {
+						assert.deepEqual(result.rows[0].id, user.user.id, "Adding new user failed");
+					});
+					cb();
+					done();
+				});
+			});
+		});
+	});
+	
 });
 /*
 
@@ -78,7 +172,30 @@ function getNewTextMessage() {
 		from: generate.names(6),
 		to: generate.names(10),
 		text: generate.sentence(10),
+		labels: {},
 		threads: [{id: id + (3), title: generate.sentence(15)}],
 		time: new Date().getTime()
 	};
+}
+
+function getNewUser() {
+	var email = generate.names(15) + "@" + generate.names(6) +"." + generate.names(2);
+	return {
+		id: generate.uid(),
+		type:"user",
+		user: {
+			id:generate.names(8),
+			description:"this is me?",
+			type:"user",
+			timezone: 0,
+			picture: generatePick(email),
+			identities:["mailto:"+email], 
+			params:{},
+			guides: {}
+		}
+	};
+}
+
+function generatePick(id) {
+	return 'https://gravatar.com/avatar/' + crypto.createHash('md5').update(id).digest('hex') + '/?d=identicon&s=48';
 }
