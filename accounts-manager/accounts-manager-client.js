9e512148ed46b29133c15ed172ef79617d0d52f1
Amal Antony <amalantony06@gmail.com>
2015-01-15 16:49:09 +0530

    Changed file names for importing js files in account-manager-client.

--- accounts-manager/accounts-manager-client.js
+++ accounts-manager/accounts-manager-client.js
@@ -1,8 +1,8 @@
 /* jshint browser:true */
 /* global $ */
 
-var loginWithGoogle = require('./google-am.js');
-var loginWithFacebook = require('./facebook-am.js');
+var loginWithGoogle = require('./google-am-client.js');
+var loginWithFacebook = require('./facebook-am-client.js');
 
 $('.js-phonegap-google-login').click(loginWithGoogle);
 $('.js-phonegap-fb-login').click(loginWithFacebook);
