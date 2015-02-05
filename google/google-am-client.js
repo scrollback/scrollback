1124ad89da91a3bd2aecdb7ba759db48544bffcf
Amal Antony <amalantony06@gmail.com>
2015-02-05 16:10:30 +0530

    Removed the initial logout from google client.

--- google/google-am-client.js
+++ google/google-am-client.js
@@ -1,6 +1,7 @@
 /* jshint browser:true */
 /* global libsb, $ */
 var $googlebutton = $('.js-cordova-google-login');
+
 function sendInit(token) {
 	/* sends init with the auth token */
 	if (!token) {
@@ -31,55 +32,52 @@ var interval_id; // required since the successCallback is not reliably fired in 
 
 function loginWithGoogle() {
 	var logged_in = false;
-		
+
 	var isGuest = libsb && libsb.user && (/^guest-/).test(libsb.user.id);
 	if (window.plugins && window.plugins.googleplus) {
 		$googlebutton.addClass('working');
-		window.plugins.googleplus.logout(function(m) {
-			console.log("Logged out", m);
-			window.plugins.googleplus.login({},
-				function(obj) {
-					//successCallback
-					logged_in = true;
-					$googlebutton.removeClass('working');
-					console.log("Login with Google+ successfull", obj);
-					sendInit(obj.oauthToken);
-				},
-				function(msg) {
-					//errorCallback
-					console.log("Login with Google+ failed", msg);
-					$googlebutton.removeClass('working');
-				});
-			if (isGuest && !logged_in) {
-				if (interval_id) {
-					clearInterval(interval_id);
-					interval_id = null;
-				}
-				interval_id = setInterval(function() {
-					console.log("Trying silent login");
-					window.plugins.googleplus.trySilentLogin({},
-						function(obj) {
-							logged_in = true;
-							console.log("Silent lgn success", JSON.stringify(obj));
-							sendInit(obj.oauthToken);
-							clearInterval(interval_id);
-							interval_id = null;
-						},
-						function(msg) {
-							console.log('Silent login for google+ failed: ' + msg);
-						}
-					);
-				}, 1000);
-				libsb.on('init-dn', function(i, n) {
-					// clear all intervals used for polling.
-					// multiple intervals get added if user clicks the login button multiple times.
-					clearInterval(interval_id);
-					interval_id = null;
-					$googlebutton.removeClass('working');
-					n();
-				}, 500);
+		window.plugins.googleplus.login({},
+			function(obj) {
+				//successCallback
+				logged_in = true;
+				$googlebutton.removeClass('working');
+				console.log("Login with Google+ successfull", obj);
+				sendInit(obj.oauthToken);
+			},
+			function(msg) {
+				//errorCallback
+				console.log("Login with Google+ failed", msg);
+				$googlebutton.removeClass('working');
+			});
+		if (isGuest && !logged_in) {
+			if (interval_id) {
+				clearInterval(interval_id);
+				interval_id = null;
 			}
-		});
+			interval_id = setInterval(function() {
+				console.log("Trying silent login");
+				window.plugins.googleplus.trySilentLogin({},
+					function(obj) {
+						logged_in = true;
+						console.log("Silent lgn success", JSON.stringify(obj));
+						sendInit(obj.oauthToken);
+						clearInterval(interval_id);
+						interval_id = null;
+					},
+					function(msg) {
+						console.log('Silent login for google+ failed: ' + msg);
+					}
+				);
+			}, 1000);
+			libsb.on('init-dn', function(i, n) {
+				// clear all intervals used for polling.
+				// multiple intervals get added if user clicks the login button multiple times.
+				clearInterval(interval_id);
+				interval_id = null;
+				$googlebutton.removeClass('working');
+				n();
+			}, 500);
+		}
 	}
 }
 
