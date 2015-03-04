639c20da195bf8f5fb2800376469cb622fc8484c
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-03-04 16:39:51 +0530

    Added text menu

--- ui/components/chat-item.jsx
+++ ui/components/chat-item.jsx
@@ -1,12 +1,25 @@
 /* jshint browser: true */
 
-var format = require("../../lib/format.js");
+var format = require("../../lib/format.js"),
+	showMenu = require("../utils/show-menu.js");
 
 module.exports = function(core, config, store) {
 	var React = require("react"),
 		ChatItem;
 
 	ChatItem = React.createClass({
+		showChatMenu: function(e) {
+			core.emit("text-menu", {
+				origin: e.currentTarget,
+				buttons: {},
+				items: {},
+				arrow: true,
+				textObj: this.props.text
+			}, function(err, menu) {
+				showMenu("text-menu", menu);
+			});
+		},
+
 		render: function() {
 			var nav =  store.getNav(),
 				text = format.formatTextToMD(this.props.text.text),
@@ -22,7 +35,7 @@ module.exports = function(core, config, store) {
 			}
 
 			return (
-				<div className={classNames} key={"chat-item-" + nav.room + "-" + nav.thread + "-" + this.props.text.id}>
+				<div className={classNames} key={"chat-item-" + nav.room + "-" + nav.thread + "-" + this.props.text.id} onClick={this.showChatMenu}>
 			 		<div className="chat-item-nick">{this.props.text.from}</div>
 					<div className="chat-item-message markdown-text" dangerouslySetInnerHTML={{__html: text}}></div>
 					<time className="chat-item-timestamp" dateTime={new Date(this.props.text.time).toISOString()}>{time}</time>
