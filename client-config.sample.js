83a534429637f8a0644e081f684c7c7fed1f2421
aravind <aravindet@gmail.com>
2015-03-05 13:25:10 +0530

    Changed default configs for easier setup

--- client-config.sample.js
+++ client-config.sample.js
@@ -1,2 +1,15 @@
 module.exports = {
+    server: {
+        protocol: "http:",
+        host: "//localhost:7528"
+    },
+    analytics: {
+        "id": "UA-XXXXXXXX-1"
+    },
+    localStorage: {
+        version: 1.0
+    },
+    errorception: {
+        id: "XXXXXXXXXXXXXXXXX"//https://errorception.com/ project id
+    }
 };
