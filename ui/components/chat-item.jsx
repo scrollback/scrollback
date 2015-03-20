872458572132a16b1cf65b44ccdb13f0cace5669
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-03-20 12:44:48 +0530

    Revert "Don't show 'Tweet message' menu"
    This reverts commit 2a02c1624814dae7681793cefb702f8a6008aff0.

--- ui/components/chat-item.jsx
+++ ui/components/chat-item.jsx
@@ -1,17 +1,34 @@
 /* jshint browser: true */
 
-var format = require("../../lib/format.js");
+var format = require("../../lib/format.js"),
+	showMenu = require("../utils/show-menu.js");
 
 module.exports = function(core, config, store) {
 	var React = require("react"),
 		ChatItem;
 
 	ChatItem = React.createClass({
+		showChatMenu: function(e) {
+			if (e.target.tagName === "A") {
+				return;
+			}
+
+			core.emit("text-menu", {
+				origin: e.currentTarget,
+				buttons: {},
+				items: {},
+				arrow: true,
+				textObj: this.props.text
+			}, function(err, menu) {
+				showMenu("text-menu", menu);
+			});
+		},
+
 		render: function() {
 			var nav =  store.get("nav"),
 				text = format.formatTextToMD(this.props.text.text),
 				time = format.friendlyTime(this.props.text.time, new Date().getTime()),
-				timeStamp, nick, classNames = "chat-item";
+				timeStamp, nick, classNames = "chat-item", pref = "";
 
 			if (this.props.text.labels) {
 				for (var label in this.props.text.labels) {
@@ -24,19 +41,19 @@ module.exports = function(core, config, store) {
 			if (this.props.showtime) {
 				timeStamp = <time className="chat-item-timestamp" dateTime={new Date(this.props.text.time).toISOString()}>{time}</time>;
 			}
-
-			if (this.props.continues) {
+			
+			if(this.props.continues) {
 				classNames += " chat-item-continues";
 			}
-
-			if (this.props.continuation) {
+			
+			if(this.props.continuation) {
 				classNames += " chat-item-continuation";
 			} else {
 				nick = <div className="chat-item-nick">{format.username(this.props.text.from)}</div>;
 			}
 
 			return (
-				<div className={classNames} key={"chat-item-" + nav.room + "-" + (nav.thread || "all") + "-" + this.props.text.id}>
+				<div className={classNames} key={"chat-item-" + nav.room + "-" + (nav.thread || "all") + "-" + this.props.text.id} onClick={this.showChatMenu}>
 					{nick}
 					<div className="chat-item-message markdown-text" dangerouslySetInnerHTML={{__html: text}}></div>
 					{timeStamp}
