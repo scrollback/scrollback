f508e299f26d48b3ea4fb514235187dc819f34ca
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-03-22 14:18:32 +0530

    Show persona button only in web and embed

--- browserid-auth/browserid-auth-client.js
+++ browserid-auth/browserid-auth-client.js
@@ -1,38 +1,32 @@
 /* jshint browser: true */
 
-module.exports = function(core) {
-	var env = "";
-	core.on("boot", function(state, next) {
-		env = state.context.env || "web";
+module.exports = function(core, config, store) {
+	core.on("auth", function(auth, next) {
+		if (/(web|embed)/.test(store.get("context", "env"))) {
+			auth.buttons.persona = {
+				text: "Email",
+				prio: 200,
+				action: function() {
+					navigator.id.watch({
+						onlogin: function(assertion) {
+							var action = {};
 
-		if (state.context.env != "android") {
-			core.on('auth', function(auth, next) {
-				auth.buttons.persona = {
-					text: "Email",
-					prio: 200,
-					action: function() {
-						navigator.id.watch({
-							onlogin: function(assertion) {
-								var action = {};
+							action.auth = {
+								browserid: assertion
+							};
 
-								action.auth = {
-									browserid: assertion
-								};
+							core.emit("init-up", action, function() {});
+						},
+						onlogout: function() {
+							// will get there soon enough.
+						}
+					});
 
-								core.emit("init-up", action, function() {});
-							},
-							onlogout: function() {
-								// will get there soon enough.
-							}
-						});
+					navigator.id.request();
+				}
+			};
 
-						navigator.id.request();
-					}
-				};
-
-				next();
-			}, 500);
+			next();
 		}
-		next();
-	}, 100);
+	}, 500);
 };
