f8e5ff9987054a603811a75952549436acbdd159
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-03-22 14:33:22 +0530

    Added checks before calling android methods

--- push-notification/gcm-client.js
+++ push-notification/gcm-client.js
@@ -1,22 +1,25 @@
-/* jshint browser:true */
-/* global Android */
-var core, config, store, gcmTimeValidity = 7 * 24 * 60 * 60 * 1000, updateDevice = false, userUpQueued = false;
-var appUtils = require("../lib/app-utils.js");
+/* jshint browser: true */
+
+var core, config, store, gcmTimeValidity = 7 * 24 * 60 * 60 * 1000, updateDevice = false, userUpQueued = false,
+	appUtils = require("../lib/app-utils.js");
 
 module.exports = function(c, conf, st) {
 	var LS = window.localStorage, lastGCMTime, device;
 	core = c;
 	config = conf;
 	store = st;
-	
+
 	lastGCMTime = LS.getItem("lastGCMTime");
-	lastGCMTime = lastGCMTime? parseInt(lastGCMTime) : 0;
-	
+	lastGCMTime = lastGCMTime ? parseInt(lastGCMTime) : 0;
+
 	core.on("boot", function(state, next) {
 		console.log(state.context.env, lastGCMTime, (new Date().getTime() - gcmTimeValidity), lastGCMTime < (new Date().getTime() - gcmTimeValidity));
-		if(state.context.env === "android" && lastGCMTime < (new Date().getTime() - gcmTimeValidity)) {
-			console.log("asking for device details");
-			Android.registerGCM();
+		if (state.context.env === "android" && lastGCMTime < (new Date().getTime() - gcmTimeValidity)) {
+
+			if (window.Android && typeof window.Android.registerGCM === "function") {
+				window.Android.registerGCM();
+			}
+
 			window.addEventListener("gcm_register", function(event) {
 				console.log("got device details", event);
 				device = event.detail;
@@ -25,31 +28,35 @@ module.exports = function(c, conf, st) {
 		}
 		next();
 	}, 100);
-	
-	
+
 	core.on("statechange", function(changes, next) {
 		var userObj, userId = store.get("user");
-		if(changes.user && !appUtils.isGuest(userId) && device && updateDevice) {
+
+		if (changes.user && !appUtils.isGuest(userId) && device && updateDevice) {
 			console.log("got device id.");
-			
+
 			userObj = store.getUser(userId);
-			if(userObj === "missing") {
+
+			if (userObj === "missing") {
 				userUpQueued = true;
 				next();
 				return;
 			}
+
 			updateUser(userObj);
-		}else if(userUpQueued && changes.entities && changes.entities[userId] && typeof changes.entities[userId] === "object") {
+
+		} else if (userUpQueued && changes.entities && changes.entities[userId] && typeof changes.entities[userId] === "object") {
 			updateUser(changes.entities[userId] );
 		}
 		next();
 	}, 500);
-	
-	function updateUser(userObj){
+
+	function updateUser(userObj) {
 		var uuid;
-		if(!userObj.params) userObj.params = {};
-		if(!userObj.params.pushNotifications) userObj.params.pushNotifications = {};
-		if(!userObj.params.pushNotifications.devices || userObj.params.pushNotifications.devices instanceof Array) {
+
+		if (!userObj.params) userObj.params = {};
+		if (!userObj.params.pushNotifications) userObj.params.pushNotifications = {};
+		if (!userObj.params.pushNotifications.devices || userObj.params.pushNotifications.devices instanceof Array) {
 			userObj.params.pushNotifications.devices = {};
 		}
 		device.expiryTime = new Date().getTime() + gcmTimeValidity;
@@ -64,12 +71,12 @@ module.exports = function(c, conf, st) {
 			LS.setItem("currentDevice", uuid);
 		});
 	}
-	
-	
+
 	core.on("logout", function(payload, next) {
-		if(store.getContext("env") === "android"){
-			Android.unregisterGCM();
+		if (store.get("context", "env") === "android" && window.Android && typeof window.Android.unregisterGCM === "function") {
+			window.Android.unregisterGCM();
 		}
+
 		next();
 	}, 500);
 };
