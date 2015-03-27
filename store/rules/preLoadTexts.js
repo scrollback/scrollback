fffdde5fc4ecbd275e72c7b506e27ba2250b2a11
aravind <aravindet@gmail.com>
2015-03-28 00:47:30 +0530

    Init perf improvements   Deferred text loading until nav to room mode   Aggregating setstates in preload rules

--- store/rules/preLoadTexts.js
+++ store/rules/preLoadTexts.js
@@ -1,28 +1,38 @@
 var core, config, store;
 //var entityOps = require("./../entity-ops.js");
+
+var query;
+
 module.exports = function(c, conf, s) {
 	core = c;
 	config = conf;
 	store = s;
+	
+	query = require("../bulkQuery.js")(core, store, "texts");
+	
 	core.on("setstate", function(changes, next) {
 		var room = store.getNav("room") || "",
-			current = store.get();
+			current = store.get(),
+			threadRanges;
 		
-		if (changes.threads && changes.threads[room]) {
-			changes.threads[room].forEach(function(threadRange) {
+		if(
+			(changes.nav && changes.nav.mode ||
+			current.nav && current.nav.mode) == "room" && // don’t bother except in the room mode
+			(
+				changes.threads && changes.threads[room] || // either the threads have changed, or
+				changes.nav && (room = changes.nav.room) && // we are switching rooms, whose
+				current.threads && current.threads[room] // threads were previously loaded
+			)
+		) {
+			threadRanges = [].concat(
+				changes.threads && changes.threads[room] || [],
+				current.threads && current.threads[room] || []
+			);
+			
+			threadRanges.forEach(function(threadRange) {
 				if(threadRange.items && threadRange.items.length) {
 					threadRange.items.forEach(function(threadObj) {
-						loadRecentTexts(room, threadObj.id);
-					});
-				}
-			});
-		}
-		
-		if (changes.nav && (room = changes.nav.room) && current.threads && current.threads[room]) {
-			current.threads[room].forEach(function(threadRange) {
-				if(threadRange.items && threadRange.items.length){
-					threadRange.items.forEach(function(threadObj) {
-						loadRecentTexts(room, threadObj.id);
+						query(room + '_' + threadObj.id);
 					});
 				}
 			});
@@ -31,31 +41,33 @@ module.exports = function(c, conf, s) {
 		next();
 	}, 800);
 };
-
-
-function loadRecentTexts(roomId, threadId) {
-	var key = roomId+"_"+threadId;
-	
-	if(store.get('texts', key) && store.get('texts', key).length) {
-//		console.log('texts already exist for ', threadId, 'Skipping');
-		return; // Already there.
-	}
-	
-	core.emit("getTexts", {to:roomId, thread:threadId, time: null, before: 3}, function(err, texts) {
-		var changes = { texts:{} };
-		if(err || !texts || !texts.results) return;
-		
-		changes.texts[key] = [];
-		changes.texts[key].push({
-			end : null,
-			items: texts.results
-		});
-		
-		if(texts.results.length) {
-			changes.texts[key][0].start = texts.results[0].time;
-		} else {
-			changes.texts[key][0].start = null;
-		}
-		core.emit("setstate", changes);
-	});
-}
+//
+//
+//function loadRecentTexts(roomId, threadId) {
+//	var key = roomId+"_"+threadId;
+//	
+//	if(store.get('texts', key) && store.get('texts', key).length) {
+////		console.log('texts already exist for ', threadId, 'Skipping');
+//		return; // Already there.
+//	}
+//	
+//	window.queryCount = (window.queryCount || 0)+1;
+//	
+//	core.emit("getTexts", {to:roomId, thread:threadId, time: null, before: 3}, function(err, texts) {
+//		var changes = { texts:{} };
+//		if(err || !texts || !texts.results) return;
+//		
+//		changes.texts[key] = [];
+//		changes.texts[key].push({
+//			end : null,
+//			items: texts.results
+//		});
+//		
+//		if(texts.results.length) {
+//			changes.texts[key][0].start = texts.results[0].time;
+//		} else {
+//			changes.texts[key][0].start = null;
+//		}
+//		core.emit("setstate", changes);
+//	});
+//}
--- store/rules/preLoadThreads.js
+++ store/rules/preLoadThreads.js
@@ -1,22 +1,28 @@
 var core, config, store;
 //var entityOps = require("./../entity-ops.js");
+
+var query;
+
 module.exports = function(c, conf, s) {
 	core = c;
 	config = conf;
 	store = s;
+	
+	query = require("../bulkQuery.js")(core, store, 'threads');
+	
 	core.on("setstate", function(changes, next) {
-		var user = store.get("user") || "", room = store.getNav(room) || "";
-		var regex = new RegExp("_" + user + "$");
+		var user = store.get("user") || "",
+			room = store.getNav(room) || "",
+			regex = new RegExp("_" + user + "$");
+				
 		if (changes.app && changes.app.featuredRooms) {
-			changes.app.featuredRooms.forEach(function(featuredRoomId) {
-				loadRecentThreads(featuredRoomId);
-			});
+			changes.app.featuredRooms.forEach(query);
 		}
 
 		if(changes.entities && user) {
 			Object.keys(changes.entities).forEach(function(key) {
 				if(regex.test(key) && changes.entities[key] && changes.entities[key].room) {
-					loadRecentThreads(changes.entities[key].room);
+					query(changes.entities[key].room);
 				}
 			});
 		}
@@ -25,26 +31,6 @@ module.exports = function(c, conf, s) {
 };
 
 
-function loadRecentThreads(featuredRoomId) {
-	
-	if(store.get('threads', featuredRoomId)) return;
-	
-	core.emit("getThreads", {to:featuredRoomId, time: null, before: 3}, function(err, threads) {
-		var changes = {
-			threads:{}
-		};
-		if(err || !threads || !threads.results) return;
-		changes.threads[threads.to] = [];
-		changes.threads[threads.to].push({
-			end : null,
-			items: threads.results
-		});
 
-		if(threads.results.length) {
-			changes.threads[threads.to][0].start = threads.results[0].startTime;
-		} else {
-			changes.threads[threads.to][0].start = null;
-		}
-		core.emit("setstate", changes);
-	});
-}
+
+
