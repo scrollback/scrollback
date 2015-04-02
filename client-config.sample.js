67c68d9cedc8c1abd5b453b728982a5f89bc271b
Chandrakanta Shatapathy <ja.chandrakant@gmail.com>
2015-04-02 16:57:33 +0530

    added a nginx configuration file and requirements to server and cient config sample file

--- client-config.sample.js
+++ client-config.sample.js
@@ -1,9 +1,15 @@
 module.exports = {
-	analytics: {
-		"id": "UA-XXXXXXXX-1"
-	},
-	errorception: {
-		// https://errorception.com/
-		id: "XXXXXXXXXXXXXXXXX"
-	}
+	server: {
+        protocol: "https:",
+        host: "$branch.stage.scrollback.io"
+    },
+    analytics: {
+        "id": "UA-XXXXXXXX-1"
+    },
+    localStorage: {
+        version: 1.0
+    },
+    errorception: {
+        id: "XXXXXXXXXXXXXXXXX"//https://errorception.com/ project id
+    }
 };
--- server-config.sample.js
+++ server-config.sample.js
@@ -6,5 +6,17 @@ module.exports = {
 			db: "scrollback",
 			server: "localhost"
 		}
+	},
+	http: {
+		host: "$branch.stage.scrollback.io",
+		cookieDomain: "localhost",
+		port: "$port",
+		home: "public", // the directory containing static files
+		time: 60000,
+		limit: 30,
+		index: "/me" //index URL redirect
+	},
+    "browserid-auth": {
+		audience: "https://$branch.stage.scrollback.io"
 	}
 };
