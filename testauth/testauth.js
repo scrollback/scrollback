dfb7004bde225812b9835ebdef74f808b533aba4
Chandrakanta Shatapathy <ja.chandrakant@gmail.com>
2015-04-27 15:48:35 +0530

    small fix

--- testauth/testauth.js
+++ testauth/testauth.js
@@ -1,8 +1,8 @@
 module.exports = function(core) {
 	core.on("init",function(init, next) {
-		if(init.auth && init.auth.testAuth) {
+		if(init.auth && init.auth.testauth) {
 				core.emit("getUsers", {
-				identity:init.auth.testAuth,
+				identity:init.auth.testauth,
 				session: "internal-test"
 			}, function(err, data) {
 				if (err || !data || !data.results) return next(err);
