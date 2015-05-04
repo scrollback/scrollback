c2f2d2b63b7889a447cc9b28c3a64b3e77872ced
Juhi <juhi.goel234@gmail.com>
2015-05-04 16:28:38 +0530

    Added images for no messages and no discussions

--- ui/components/thread-list.jsx
+++ ui/components/thread-list.jsx
@@ -163,7 +163,7 @@ module.exports = function(core, config, store) {
 			} else {
 				empty = (
 					<div className="thread-feed-empty">
-						{loading ? "Loading discussions..." : "There are no discussions yet :-("}
+						{loading ? "Loading discussions..." : <img src="/s/img/no-messages.png" />}
 					</div>
 				);
 			}
