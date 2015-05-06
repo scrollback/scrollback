796430166c77d608286092f691cc6c293b124399
Chandrakanta Shatapathy <ja.chandrakant@gmail.com>
2015-05-06 12:53:47 +0530

    added test for join/part

--- test/public/t/integration/joinpart-test.js
+++ test/public/t/integration/joinpart-test.js
@@ -1,58 +1,107 @@
 /*global describe*/
 /*global it*/
-/*global uid*/
 /*global assert*/
 /*global SockJS*/
 /*global scrollback*/
-/*global beforeEach*/
-/*global afterEach*/
+/*global getConnection*/
 
 describe('Testing join/part action of a user: ', function() {
 
 	var socket;
-	beforeEach(function(done) {
-		socket = new SockJS(scrollback.host + "/socket");
-		var sessionId = "web://" + uid();
-		var init = {
-			"id": sessionId,
-			"type": "init",
-			"to": "me",
-			"suggestedNick": "testinguser",
-			"session": "web://" + uid(),
-			"resource": uid(),
-			"origin": {
-				domain: "scrollback.io",
-				verified: true
-			}
-		};
-		socket.onopen = function() {
-			socket.send(JSON.stringify(init));
-			done();
-		};
-	});
+		it("join action", function(done) {
+			socket = new SockJS(scrollback.host + "/socket");
+			getConnection(socket, "testinguser");
+			socket.onmessage = function(msg) {
+				var back = {
+						from: "testinguser",
+						type: 'back',
+						to: "scrollback"
+					},
+					join = {
+						to: "scrollback",
+						type: 'join'
+					};
+				msg = JSON.parse(msg.data);
+				console.log(msg);
+				if (msg.type === 'init') {
+					//text.session = msg.session;
+					socket.send(JSON.stringify(back));
+					return;
+				}
 
-	it('guest user joining a room', function(done) {
-		socket.onmessage = function(msg) {
-			msg = JSON.parse(msg.data);
-			console.log(msg);
-			var join = {
-				type: "join",
-				to: "scrollback",
-				from: msg.from,
+				if (msg.type === 'back') {
+					join.from = msg.from;
+					socket.send(JSON.stringify(join));
+					return;
+				}
+				assert(msg.type !== 'error', "join action failed");
+				socket.close();
+				done();
 			};
-			
-			if (msg.type === 'init') {
-				socket.send(JSON.stringify(join));
-				return;
-			}
-			assert(msg.type !== 'error', "back action failed");
-			done();
-		};
+		});
 
-	});
+	it("join action from a guest", function(done) {
+			socket = new SockJS(scrollback.host + "/socket");
+			getConnection(socket, "guest");
+			socket.onmessage = function(msg) {
+				var back = {
+						from: "guest",
+						type: 'back',
+						to: "scrollback"
+					},
+					join = {
+						to: "scrollback",
+						type: 'join'
+					};
+				msg = JSON.parse(msg.data);
+				console.log(msg);
+				if (msg.type === 'init') {
+					//text.session = msg.session;
+					socket.send(JSON.stringify(back));
+					return;
+				}
 
-	afterEach(function(done) {
-		socket.close();
-		done();
-	});
+				if (msg.type === 'back') {
+					join.from = msg.from;
+					socket.send(JSON.stringify(join));
+					return;
+				}
+				assert(msg.type === 'error', "Join action happened from a guest user");
+				assert(msg.message === 'ERR_NOT_ALLOWED', "wrong error message" );
+				socket.close();
+				done();
+			};
+		});
+
+	it("part action", function(done) {
+			socket = new SockJS(scrollback.host + "/socket");
+			getConnection(socket, "testinguser");
+			socket.onmessage = function(msg) {
+				var back = {
+						from: "testinguser",
+						type: 'back',
+						to: "scrollback"
+					},
+					part = {
+						to: "scrollback",
+						type: 'part'
+					};
+				msg = JSON.parse(msg.data);
+				console.log(msg);
+				if (msg.type === 'init') {
+					//text.session = msg.session;
+					socket.send(JSON.stringify(back));
+					return;
+				}
+
+				if (msg.type === 'back') {
+					part.from = msg.from;
+					socket.send(JSON.stringify(part));
+					return;
+				}
+				assert(msg.type !== 'error', "part action failed");
+				socket.close();
+				done();
+			};
+		});
 });
