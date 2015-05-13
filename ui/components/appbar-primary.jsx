7ea6edbb32d4e607a9115368e7b2c2b00950ece5
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-05-13 17:29:50 +0530

    Include @2x versions for sharper logo on high-res screens

--- ui/components/appbar-primary.jsx
+++ ui/components/appbar-primary.jsx
@@ -100,9 +100,9 @@ module.exports = function(core, config, store) {
 			return (
 				<div ref="appbar" key="appbar-primary" className="appbar appbar-primary" onClick={this.toggleMinimize}>
 					<a data-mode="room chat" className="appbar-icon appbar-icon-back appbar-icon-left" onClick={this.goBack}></a>
-					<img data-mode="home" className="appbar-title-logotype" src="/s/assets/logo/scrollback-logo-white.png" />
+					<img data-mode="home" className="appbar-title-logotype" src="/s/assets/logo/scrollback-logo-white@2x.png" />
 					<div data-mode="room chat" className="appbar-title-container">
-						<img className="appbar-logotype appbar-logotype-primary" src="/s/assets/logo/scrollback-logo.png" />
+						<img className="appbar-logotype appbar-logotype-primary" src="/s/assets/logo/scrollback-logo@2x.png" />
 						<h1 className="appbar-title appbar-title-primary">{this.state.title}</h1>
 					</div>
 					<div className="user-area" onClick={this.showUserMenu}>
