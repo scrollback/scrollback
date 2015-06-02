01b98a7ad24f3f223682db68ccdcfff65d1f1729
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-06-02 13:13:18 +0530

    Handle permission to upload stuff

--- authorizer/rules/permissionRules.js
+++ authorizer/rules/permissionRules.js
@@ -1,30 +1,52 @@
 "use strict";
-var permissionLevels = require('../permissionWeights.js');
-var SbError = require('./../../lib/SbError.js');
-var readActions = ["away", "back", "getTexts", "getThreads"];
-var writeActions = ["text", "edit"];
+
+var permissionLevels = require("../permissionWeights.js"),
+	SbError = require("./../../lib/SbError.js"),
+	readActions = [ "away", "back", "getTexts", "getThreads"],
+	writeActions = [ "text", "edit", "http/getPolicy" ],
+	registeredUserActions = [ "http/getPolicy" ];
 
 module.exports = function() {
 	return function(action) {
-		var guides = action.room.guides, readLevel, writeLevel;
-		if (readActions.indexOf(action.type)>=0) {
-			readLevel = (guides && guides.authorizer && typeof guides.authorizer.readLevel) ? guides.authorizer.readLevel : "guest";
-			if (permissionLevels[readLevel] > permissionLevels[action.user.role]){
-				return (new SbError('ERR_NOT_ALLOWED', {
-					source: 'authorizer',
+		var guides = action.room.guides,
+			readLevel, writeLevel;
+
+		if (readActions.indexOf(action.type) > -1) {
+			readLevel = (guides && guides.authorizer && guides.authorizer.readLevel) ? guides.authorizer.readLevel : "guest";
+
+			if (permissionLevels[readLevel] > permissionLevels[action.user.role]) {
+				return (new SbError("ERR_NOT_ALLOWED", {
+					source: "authorizer",
 					action: action.type,
 					requiredRole: readLevel,
 					currentRole: action.user.role
 				}));
 			}
-		} else if (writeActions.indexOf(action.type)>=0) {
-			writeLevel = (guides && guides.authorizer && typeof guides.authorizer.writeLevel) ? guides.authorizer.writeLevel : "guest";
-			console.log(writeLevel, action.user.role, permissionLevels[writeLevel] > permissionLevels[action.user.role]);
-			if (permissionLevels[writeLevel] > permissionLevels[action.user.role]){
-				
-				console.log("throwing error.............................");
-				return (new SbError('ERR_NOT_ALLOWED', {
-					source: 'authorizer',
+		}
+
+		if (writeActions.indexOf(action.type) > -1) {
+			writeLevel = (guides && guides.authorizer && guides.authorizer.writeLevel) ? guides.authorizer.writeLevel : "guest";
+
+			if (permissionLevels[writeLevel] > permissionLevels[action.user.role]) {
+				return (new SbError("ERR_NOT_ALLOWED", {
+					source: "authorizer",
+					action: action.type,
+					requiredRole: writeLevel,
+					currentRole: action.user.role
+				}));
+			}
+		}
+
+		if (registeredUserActions.indexOf(action.type) > -1) {
+			if (guides && guides.authorizer && guides.authorizer.writeLevel) {
+				writeLevel = guides.authorizer.writeLevel;
+			}
+
+			writeLevel = (permissionLevels[writeLevel] && permissionLevels[writeLevel] > permissionLevels.guest) ? writeLevel : "registered";
+
+			if (permissionLevels[writeLevel] > permissionLevels[action.user.role]) {
+				return (new SbError("ERR_NOT_ALLOWED", {
+					source: "authorizer",
 					action: action.type,
 					requiredRole: writeLevel,
 					currentRole: action.user.role
