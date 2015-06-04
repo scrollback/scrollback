ac42a53441bd565db1c3cf10c631b129a79af613
Chandrakanta Shatapathy <ja.chandrakant@gmail.com>
2015-06-04 18:49:20 +0530

    removed unnecessary codes

--- test/test-script.sh
+++ test/test-script.sh
@@ -3,14 +3,9 @@
 currdir=`cd $(dirname "${BASH_SOURCE[0]}") && pwd`
 basedir="${currdir%/*}"
 logfile="${basedir}/logs/test-$(date +%y%m%d%H).log"
-backup="${basedir}/leveldb-storage/backup-$(date +%y%m%d%H)"
-data="${basedir}/leveldb-storage/data"
-testing_state="${basedir}/leveldb-storage/testing-state"
-leveldb_storage="${basedir}/leveldb-storage"
 
 # Create logfile
 touch "$logfile"
-
 exec &>> "$logfile"
 
 # Show error messages
@@ -36,10 +31,6 @@ sudo stop scrollback
 # clear redis
 redis-cli SELECT 11
 redis-cli FLUSHDB
-#backing up previous data
-mv "$data" "$backup"
-#recovering data
-cp -a "$testing_state/data" "$leveldb_storage/"
 
 # Setup
 npm install
