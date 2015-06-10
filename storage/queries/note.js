cf1a1a8c105a777fd7ba80d35547e0a4e104de70
harish <harry.softer@gmail.com>
2015-06-10 16:39:19 +0530

    Changing the sql query for notes.

--- storage/queries/note.js
+++ storage/queries/note.js
@@ -14,56 +14,20 @@
 module.exports = [
 	function (query) { // Stage 1, group and get notifications
 		return {
-			$:	"SELECT \"action\", \"notetype\", \"group\", " +
-				"MAX(\"score\") as \"score\", MAX(\"time\") AS \"time\", " +
-				"COUNT(*) AS \"count\", " +
-				"CASE WHEN MAX(COALESCE(readtime, '2050-01-01')) = '2050-01-01'" +
-				"THEN NULL ELSE MAX(readtime) END AS readtime " +
-				"FROM notes WHERE \"user\"=${user} AND dismissTime IS NULL " +
-				"GROUP BY \"action\", \"notetype\", \"group\"",
-			
+			$: 	"SELECT \"ref\", \"notetype\", \"user\", \"score\", \"notedata\", \"group\", \"time\", \"count\"" +
+				" FROM (" +
+    			"SELECT *," +
+        		"COUNT(*) OVER (PARTITION BY \"user\", \"notetype\", \"group\" ORDER BY \"time\" DESC) \"count\"," +
+        		"RANK() OVER (PARTITION BY \"user\", \"notetype\", \"group\" ORDER BY \"time\" DESC) timeRank " +
+    			"FROM notes " +
+    			"WHERE \"user\" = ${user}"+
+				") t"+
+				" WHERE \"count\" <= 3 OR timeRank = 1;",
 			user: query.user.id
 		};
 	},
-	
-	function (query, results) { // Stage 2, expand small groups
-		var smallGroups = [];
-		
-		query.results = [];
-		results.forEach(function (row) {
-			if(row.count < 3) {
-				smallGroups.push(row.group);
-			} else {
-				query.results.push({
-					user: query.user.id,
-					action: row.action,
-					noteType: row.notetype,
-					group: row.group,
-					score: row.score,
-					time: row.time.getTime(),
-					readTime: row.readtime && row.readtime.getTime(),
-					count: row.count
-				});
-			}
-		});
-		
-		if (smallGroups.length) {
-			return {
-				$:	"SELECT action, notetype, ref, \"group\", score, " +
-					"time, readtime, notedata " +
-					"FROM notes WHERE \"user\"=${user} AND dismissTime IS NULL " +
-					"AND \"group\" IN ($(groups))",
-
-				user: query.user.id,
-				groups: smallGroups
-			};
-		} else {
-			return null;
-		}
-	},
-	
-	function (query, results) { // Stage 3, result transform
-		query.results = query.results.concat(results.map(function (row) {
+	function (query, results) { // Stage 2, result transform
+		query.results = (query.results?query.results:[]).concat(results.map(function (row) {
 			return {
 				user: query.user.id,
 				action: row.action,
