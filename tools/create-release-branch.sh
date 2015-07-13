d0408228ff4f55fd109041df7c152cbfc00ddfcb
Chandrakanta Shatapathy <ja.chandrakant@gmail.com>
2015-07-13 19:34:41 +0530

    add full month no to release branch

--- tools/create-release-branch.sh
+++ tools/create-release-branch.sh
@@ -2,12 +2,9 @@
 
 cd $HOME/scrollback
 
-year=$(date +"%y") 
-month=$(date +"%m")
-d=$(date +"%d")
-y=${year:1}
-m=${month:1}
-release_branch='r'$y"."$m"."$d
+d=$(date +"%y.%-m.%d")
+release_branch='r'${d:1}
+
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 if [ $current_branch != "master" ]; then
 	echo "ERR_NOT_MASTER"
@@ -19,7 +16,7 @@ git checkout -b $release_branch > /dev/null
 git add npm-shrinkwrap.json > /dev/null
 git commit -m "shrinkwrap added" > /dev/null
 sleep 2
-git push --set-upstream origin $release_branch > /dev/null 
+git push --set-upstream origin $release_branch > /dev/null
 sleep 2
 
 echo $release_branch
