93210a1f551a8d335e9bc25c805873fe76de9271
Chandrakanta Shatapathy <ja.chandrakant@gmail.com>
2015-07-16 14:06:38 +0530

    updating deploy script

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
-echo deploying
-echo $1
-echo 'cd scrollback; git fetch origin; git checkout '$1'; npm install; bower install; gulp; sudo restart scrollback; tail -f logs/now.log' | ssh scrollback@sock.scrollback.io
+if [[ "$1" == "" ]]; then
+	echo "No branch name provided"
+	exit 1
+fi
+
+echo "Deploying branch $1"
+
+echo 'cd scrollback && git fetch origin && git checkout '$1' && export NODE_ENV="development" && echo $NODE_ENV && npm install && export NODE_ENV="production" && bower install && gulp && sudo restart scrollback && tail -f logs/now.log' | ssh scrollback@sock.scrollback.io
