002e1119401ea3055f639c69c927e3d8cfa0bf90
Chandrakanta Shatapathy <ja.chandrakant@gmail.com>
2015-07-28 18:59:54 +0530

    renamed template files & added jws key

--- server-config-template.js
+++ server-config-template.js
@@ -11,7 +11,12 @@ module.exports = {
 		host: "$branch.stage.scrollback.io",
 		port: "$port"
 	},
-    "browserid-auth": {
+	"browserid-auth": {
 		audience: "https://$branch.stage.scrollback.io"
+	},
+	jws: {
+		keys: {
+			"$branch.stage.scrollback.io": ["9gEBqp19abpuRXVqu4xjDDRASdoCPhIhVRxHGgj6mqB7Y0KOcYuY65TznL1fxVaxa0Q8eiECxpTWVbadDtb2xoWf2cWTCSu2VfmvpGKxeliz+SkSjYHGrtVbRD63WpcuIjMXB9Cfgf5H3SFpeTSD6ENtpCBLr2KfKI9ITMKE8nI="]
+		}
 	}
 };
