3f7386b2e62e4ae35a6f5ebe03160de9f62df409
harish <harry.softer@gmail.com>
2015-07-30 12:15:40 +0530

    deferring the getUsers queries until the user navigates to the room.

--- store/rules/loadRelatedUsers.js
+++ store/rules/loadRelatedUsers.js
@@ -2,23 +2,17 @@
 
 var relationsProps = require("./../property-list.js").relations;
 var core, config, store;
-module.exports = function(c, conf, s) {
-	core = c;
-	conf = config;
-	store = s;
-	core.on("back-dn", function(backDn, next) {
-		if (store.get("user") === backDn.from) loadUsersList(backDn.to);
-		next();
-	}, 1000);
-};
+
 
 function constructEntitiesFromUserList(list, entities, roomId) {
 	list.forEach(function(e) {
 		var relation;
 
-		if (entities[roomId + "_" + e.id]) relation = entities[roomId + "_" + e.id];
-		else relation = {};
-
+		if (entities[roomId + "_" + e.id]) {
+			relation = entities[roomId + "_" + e.id];
+		} else {
+			relation = {};
+		}
 		relation.room = roomId;
 		relation.user = e.id;
 		relation.status = "offline";
@@ -40,7 +34,9 @@ function loadUsersList(roomId) {
 
 	listeningRooms = Array.isArray(listeningRooms) ? listeningRooms.slice(0) : [];
 
-	if (listeningRooms.indexOf(roomId) < 0) listeningRooms.push(roomId);
+	if (listeningRooms.indexOf(roomId) < 0) {
+		listeningRooms.push(roomId);
+	}
 
 	function emitSetState() {
 		constructEntitiesFromUserList(memberList, entities, roomId);
@@ -48,11 +44,14 @@ function loadUsersList(roomId) {
 		occupantList.forEach(function(e) {
 			entities[roomId + "_" + e.id].status = "online";
 		});
+		console.log( entities);
 		core.emit("setstate", {
 			entities: entities,
 			app: {
 				listeningRooms: listeningRooms
 			}
+		}, function(){
+			console.log(arguments);
 		});
 	}
 
@@ -61,15 +60,52 @@ function loadUsersList(roomId) {
 		memberOf: roomId
 	}, function(err, data) {
 		memberList = data.results || [];
-		if (!done) done = true;
-		else emitSetState();
+		if (!done) {
+			done = true;
+		} else {
+			emitSetState();
+		}
 	});
 	core.emit("getUsers", {
 		type: "getUsers",
 		occupantOf: roomId
 	}, function(err, data) {
 		occupantList = data.results || [];
-		if (!done) done = true;
-		else emitSetState();
+		if (!done) {
+			done = true;
+		} else {
+			emitSetState();
+		}
 	});
 }
+
+
+
+module.exports = function(c, conf, s) {
+	core = c;
+	conf = config;
+	store = s;
+
+	core.on("setstate", function(changes) {
+		var future = store.with(changes),
+			roomId = future.get("nav", "room"),
+			oldRoomId = store.get("nav", "room"),
+			oldRelations = store.getRelatedUsers(roomId);
+
+
+		if(!changes.entities) {
+			changes.entities = {};
+		}
+		
+
+		if (roomId && roomId !== oldRoomId) {
+			oldRelations.forEach(function(rel) {
+				changes.entities[roomId + "_" + rel.id] = null;
+			});
+			console.log("clear",changes);
+			loadUsersList(roomId);
+		}
+		
+		
+	}, 850);
+};
