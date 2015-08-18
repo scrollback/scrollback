c669acb18b94b5f39ab0ae429088eedec8358921
Chandrakanta Shatapathy <chandra@scrollback.io>
2015-08-18 13:05:26 +0530

    indentation fix

--- ui/modules/settings-user.js
+++ ui/modules/settings-user.js
@@ -32,13 +32,13 @@ module.exports = function(core, config, store) {
 	});
 
 	core.on("user-dn", function(user, next) {
-			if (!userUtils.isGuest(user.user.id) && !userUtils.isGuest(user.old.id)) {
-				$("<div>").html("Your account settings were successfully saved.").
-				alertbar({
-					type: "info",
-					timeout: 1500
-				});
-			}		
+		if (!userUtils.isGuest(user.user.id) && !userUtils.isGuest(user.old.id)) {
+			$("<div>").html("Your account settings were successfully saved.").
+			alertbar({
+				type: "info",
+				timeout: 1500
+			});
+		}
 		next();
 	}, 500);
 
