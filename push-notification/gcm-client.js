9b0c43ea3c346a12a1f30258f91d67de37fa6620
Aravind <aravindet@gmail.com>
2015-09-02 18:34:18 +0530

    Update gcm-client.js

--- push-notification/gcm-client.js
+++ push-notification/gcm-client.js
@@ -3,101 +3,76 @@
 "use strict";
 
 var gcmTimeValidity = 12 * 60 * 60 * 1000,
-	updateDevice = false,
 	objUtils = require("../lib/obj-utils.js"),
 	userUtils = require("../lib/user-utils.js");
 
 module.exports = function(core, config, store) {
 	var LS = window.localStorage,
-		lastGCMTime, device, initObject, initCallback, userActionID, uuid, key = "",
-		defaultPackageName;
-
-	defaultPackageName = config.pushNotification.defaultPackageName || "";
-
-	lastGCMTime = LS.getItem("lastGCMTime");
-	lastGCMTime = lastGCMTime ? parseInt(lastGCMTime, 10) : 0;
+		device, initObject, initCallback, gcmRegisterSkipped = false;
 
 	function addGcmData() {
-		if (!device || !initCallback || !initObject) {
-			if (typeof initCallback === "function") {
-				initCallback();
-			}
+		var userObj, params, key = "",
+			defaultPackageName = config.pushNotification.defaultPackageName || "";
+		
+		if (!device || typeof initCallback !== "function" || !initObject) {
+			initCallback();
 			return;
 		}
-		var userObj = store.getUser();
+		
+		userObj = store.getUser();
 		if (!userObj.id || userUtils.isGuest(userObj.id)) {
 			initCallback();
 			return;
 		}
 
-
-		var params = userObj.params ? objUtils.clone(userObj.params) : {};
-		initObject.params = params;
+		params = userObj.params ? objUtils.clone(userObj.params) : {};
+		initObject.params = initObject.params || {};
 		initObject.params.pushNotifications = params.pushNotifications || {};
 		initObject.params.pushNotifications.devices = params.pushNotifications.devices || {};
-		device = objUtils.clone(device);
 		device.expiryTime = new Date().getTime() + gcmTimeValidity;
-		uuid = device.uuid;
 
 		device.platform = "android";
+		device.enabled = true;
 		key = device.uuid + (device.packageName && device.packageName !== defaultPackageName ? ("_" + device.packageName) : "");
 
 		initObject.params.pushNotifications.devices[key] = device;
-		device.enabled = true;
 		userActionID = userObj.id;
 		updateDevice = false;
 		initCallback();
+		
+		LS.setItem("lastGCMTime", new Date().getTime());
+		LS.setItem("currentDevice", device.uuid);
 	}
 
 	core.on("boot", function(state, next) {
-		if (state.context.env === "android") {
-			if (lastGCMTime < (new Date().getTime() - gcmTimeValidity)) {
+		var lastGCMTime = LS.getItem("lastGCMTime");
+		lastGCMTime = lastGCMTime ? parseInt(lastGCMTime, 10) : 0;
 
-				if (window.Android && typeof window.Android.registerGCM === "function") {
-					window.Android.registerGCM();
-				}
-
-				window.addEventListener("gcm_register", function(event) {
-					device = event.detail;
-
-					if (window.Android && typeof window.Android.getPackageName === "function") {
-						device.packageName = window.Android.getPackageName();
-					}
-
-					updateDevice = true;
-//					if (store.get("app", "connectionStatus") === "online") {
-//						addGcmData();
-//					}
-				});
-			} /* else if (typeof initCallback === "function") {
-				initCallback();
-			} */
+		if (
+			state.context.env === "android" &&
+			lastGCMTime < (new Date().getTime() - gcmTimeValidity) &&
+			window.Android && typeof window.Android.registerGCM === "function"
+		) {
+			window.Android.registerGCM();
+		} else {
+			gcmRegisterSkipped = true;
 		}
 		next();
 	}, 100);
 
-	core.on("init-user-up", function(payload, next) {
-		if (store.get("context").env === "web") {
-			return next();
+	window.addEventListener("gcm_register", function(event) {
+		device = objUtils.clone(event.detail);
+		if (window.Android && typeof window.Android.getPackageName === "function") {
+			device.packageName = window.Android.getPackageName();
 		}
-		initObject = payload;
-		initCallback = next;
 		addGcmData();
 	});
 
-	core.on("statechange", function(changes, next) {
-		if (changes.user && device && updateDevice) {
-			addGcmData();
-		}
-		next();
-	}, 500);
-
-	core.on("user-dn", function(action) {
-		if (action.id === userActionID) {
-			LS.setItem("lastGCMTime", new Date().getTime());
-			LS.setItem("currentDevice", uuid);
-			updateDevice = false;
-		}
+	core.on("init-user-up", function(payload, next) {
+		if (gcmRegisterSkipped) return next();
+		initObject = payload;
+		initCallback = next;
+		addGcmData();
 	});
 
 	core.on("logout", function(payload, next) {
